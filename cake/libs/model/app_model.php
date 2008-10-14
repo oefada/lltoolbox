3ba354ca249cdf8e26aae421445cd8416651d217
vgarcia <vgarcia@bd3a7434-8174-4811-bcf6-7b9fed35a952>
2008-10-14 17:42:09 +0000

    Luxury link custom app_model
    git-svn-id: http://repos.luxurylink.com/svn/repos/toolbox/trunk@57 bd3a7434-8174-4811-bcf6-7b9fed35a952

--- cake/libs/model/app_model.php
+++ cake/libs/model/app_model.php
@@ -38,5 +38,31 @@
  * @subpackage	cake.cake.libs.model
  */
 class AppModel extends Model {
+	/**
+	* Constructor just sets some defaults for Luxury Link and calls the parent constructor.
+	*/
+	function __construct($id = false, $table = null, $ds = null) {
+		if ($this->name === null) {
+			$this->name = get_class($this);
+		}
+
+		//only do this on models other than AppModel
+		if($this->name != 'AppModel'):
+			//Use the model name as the table name (forget about the cake plurals)
+			if ($this->useTable !== false) {
+				if ($this->useTable === null) {
+					//set the table as modelName
+					$this->useTable = Inflector::variable($this->name);
+				}
+			}
+
+			//Use modelNameID as the ID key (overrite using 'id' for cake)
+			if ($this->primaryKey === null) {
+				$this->primaryKey = Inflector::variable($this->name).'Id';
+			}
+		endif;
+
+		parent::__construct($id, $table, $ds);
+	}
 }
 ?>
