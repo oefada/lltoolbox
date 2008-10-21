ea2fa7cf7bae7e70a162e27a1c4e7b8ddad25838
alee <alee@bd3a7434-8174-4811-bcf6-7b9fed35a952>
2008-10-21 18:25:34 +0000

    deleted worksheet - changed worksheet to ticket
    git-svn-id: http://repos.luxurylink.com/svn/repos/toolbox/trunk@109 bd3a7434-8174-4811-bcf6-7b9fed35a952

--- app/models/worksheet_status.php
+++
@@ -1,9 +0,0 @@
-<?php
-class WorksheetStatus extends AppModel {
-
-	var $name = 'WorksheetStatus';
-	var $useTable = 'worksheetStatus';
-	var $primaryKey = 'worksheetStatusId';
-	var $displayField = 'worksheetStatusName';
-}
-?>
