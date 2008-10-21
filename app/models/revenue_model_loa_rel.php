8b4bb54a432947934ffac9dd5844aa751edb44bd
alee <alee@bd3a7434-8174-4811-bcf6-7b9fed35a952>
2008-10-21 22:22:04 +0000

    git-svn-id: http://repos.luxurylink.com/svn/repos/toolbox/trunk@113 bd3a7434-8174-4811-bcf6-7b9fed35a952

--- app/models/revenue_model_loa_rel.php
+++ app/models/revenue_model_loa_rel.php
@@ -10,5 +10,81 @@ class RevenueModelLoaRel extends AppModel {
 						  );
 						  
 	var $hasMany = array('RevenueModelLoaRelDetail' => array('foreignKey' => 'revenueModelLoaRelId'));
+	
+	var $validate = array (
+						'loaId' => array(
+								'rule' => 'numeric',
+								'message' => 'The Loa Id must be numeric.'
+								),
+						'keepPercentage' => array(
+								'rule' => array('checkPresets'),
+								'message' => 'The Keep Percentage field is required for the "Revenue Split" revenue model.  This value must be a number from 0 to 100.'
+								),
+						'x' => array(
+								'rule' => array('checkPresets'),
+								'message' => 'The X field is required for any "X for Y" revenue models.  This value must be an integer.'
+								),
+						'y' => array(
+								'rule' => array('checkPresets'),
+								'message' => 'The Y field is required for any "X for Y" revenue models.  This value must be an integer'
+								), 
+						'expMaxOffers' => array(
+								'rule' => array('checkExp'),
+								'message' => 'This field is required for the expiration criteria you have selected.'
+								), 
+						'expDate' => array(
+								'rule' => array('checkExp'),
+								'message' => 'This field is required for the expiration criteria you have selected.'
+								), 
+						'expFee' => array(
+								'rule' => array('checkExp'),
+								'message' => 'This field is required for the expiration criteria you have selected.'
+								)
+						);
+						
+						
+	function checkExp($data) {
+		$validPresets = true;
+		$expCriteriaId = $this->data['RevenueModelLoaRel']['expirationCriteriaId'];
+		if (isset($data['expMaxOffers']) && $expCriteriaId == 2) {
+			$value = $data['expMaxOffers'];
+			if (!$value || !is_int($value)) {
+				$validPresets = false;						
+			}			
+		} elseif (isset($data['expDate']) && $expCriteriaId == 3) {
+			$value = $data['expDate'];
+			if (!$value) {
+				$validPresets = false;						
+			}			
+		} elseif (isset($data['expFee']) && $expCriteriaId == 1) {
+			$value = $data['expFee'];
+			if (!$value || !is_numeric($value)) {
+				$validPresets = false;						
+			}			
+		}
+		return $validPresets;
+	}					
+	
+	function checkPresets($data) {
+		$validPresets = true;
+		$revModelId = $this->data['RevenueModelLoaRel']['revenueModelId'];
+		if (isset($data['keepPercentage']) && $revModelId == 1) {
+			$value = $data['keepPercentage'];
+			if (!$value || !is_numeric($value) || !($value >=0 && $value <= 100)) {
+				$validPresets = false;	
+			}
+		} elseif (isset($data['x']) && ($revModelId == 2 || $revModelId == 3)) {
+			$value = $data['x'];
+			if (!$value || !is_int($value)) {
+				$validPresets = false;						
+			}
+		} elseif (isset($data['y']) && ($revModelId == 2 || $revModelId == 3)) {
+			$value = $data['y'];
+			if (!$value || !is_int($value)) {
+				$validPresets = false;						
+			}			
+		}
+		return $validPresets;
+	}
 }
 ?>
