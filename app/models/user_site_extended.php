4becf7a8ed08b3bcec0f01cd9c0d09f57cbc4f87
vgarcia <vgarcia@bd3a7434-8174-4811-bcf6-7b9fed35a952>
2008-10-29 21:15:34 +0000

    New beforeSave method makes sure to hash the password before it gets saved to the database.
    git-svn-id: http://repos.luxurylink.com/svn/repos/toolbox/trunk@149 bd3a7434-8174-4811-bcf6-7b9fed35a952

--- app/models/user_site_extended.php
+++ app/models/user_site_extended.php
@@ -6,5 +6,27 @@ class UserSiteExtended extends AppModel {
 	var $name = 'UserSiteExtended';
 	var $useTable = 'userSiteExtended';
 	var $primaryKey = 'userSiteExtendedId';
+	
+	var $belongsTo = array('User' => array('foreignKey' => 'userId'));
+	
+	/**
+	 * This method runs before a user account is created or saved. For now it just creates a hash from the password field if it is found
+	 */
+	function beforeSave() {
+		$hasher = new PasswordHash(10, false);
+		
+		//we'll take a new password for the password field, hash it and put it in the passwordHash field
+		if (!empty($this->data['UserSiteExtended']['password']) &&
+			strpos($this->data['UserSiteExtended']['password'], '$2a$') !== 0 //only do this if the password doesn't already look like a hash
+			) {
+			$this->data['UserSiteExtended']['passwordHash'] = $hasher->HashPassword($this->data['UserSiteExtended']['password']);
+			
+		//if the new password is in the passwordHash variable, we do the same, but the password variable above wins
+		} else if(!empty($this->data['UserSiteExtended']['passwordHash']) && strpos($this->data['UserSiteExtended']['passwordHash'], '$2a$') !== 0) {
+			$this->data['UserSiteExtended']['passwordHash'] = $hasher->HashPassword($this->data['UserSiteExtended']['passwordHash']);
+		}
+		
+		return true;
+	}
 }
 ?>
