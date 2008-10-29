85205ac8d3b40fd721f5332d07cd71c95addfeec
vgarcia <vgarcia@bd3a7434-8174-4811-bcf6-7b9fed35a952>
2008-10-29 21:14:12 +0000

    Added validation for dates.
    git-svn-id: http://repos.luxurylink.com/svn/repos/toolbox/trunk@147 bd3a7434-8174-4811-bcf6-7b9fed35a952

--- app/models/loa.php
+++ app/models/loa.php
@@ -8,10 +8,29 @@ class Loa extends AppModel {
 	var $belongsTo = array('LoaCustomerApprovalStatus' => array('foreignKey' => 'customerApprovalStatusId'),
 						   'Client' => array('foreignKey' => 'clientId')
 						  );
+	var $validate = array('startDate' => array('rule' => array('validateEndStartDate'), 'message' => 'Start date must be less than end date'),
+							'endDate' => array('rule' => array('validateEndStartDate'), 'message' => 'Start date must be less than end date')
+							);
+							
+	var $order = array("Loa.startDate DESC");
 
 	var $hasMany = array('LoaItem' => array('foreignKey' => 'loaId'), 
 						 'ClientLoaPackageRel' => array('foreignKey' => 'loaId'),
 						 'RevenueModelLoaRel' => array('foreignKey' => 'loaId')
 						);
+						
+	function validateEndStartDate()
+	{
+		if(isset($data)):
+			$startDate = ife($data['Loa']['startDate'], $data['Loa']['startDate'], $this->data['Loa']['startDate']);
+			$endDate = ife($data['Loa']['endDate'], $data['Loa']['endDate'], $this->data['Loa']['endDate']);
+			
+			if($startDate >= $endDate) {
+				return false;
+			}
+		endif;
+			
+		return true;
+	}
 }
 ?>
--- app/models/loa_item_rate_period.php
+++ app/models/loa_item_rate_period.php
@@ -6,5 +6,20 @@ class LoaItemRatePeriod extends AppModel {
 	var $primaryKey = 'loaItemRatePeriodId';
 	
 	var $belongsTo = array('LoaItem' => array('foreignKey' => 'loaItemId'));
+	
+	var $validate = array('startDate' => array('rule' => array('validateEndStartDate'), 'message' => 'Start date must be less than end date'),
+							'endDate' => array('rule' => array('validateEndStartDate'), 'message' => 'Start date must be less than end date')
+							);
+
+	function validateEndStartDate()
+	{
+		$startDate = $this->data[$this->name]['startDate'];
+		$endDate = $this->data[$this->name]['endDate'];
+		
+		if($startDate >= $endDate) {
+			return false;
+		}
+		return true;
+	}
 }
 ?>
