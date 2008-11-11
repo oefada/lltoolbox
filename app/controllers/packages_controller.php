d671c9678fb4c8cd5b11260ccec1bae3baa3ca31
vgarcia <vgarcia@bd3a7434-8174-4811-bcf6-7b9fed35a952>
2008-11-11 19:31:42 +0000

    New carve rate periods method
    git-svn-id: http://repos.luxurylink.com/svn/repos/toolbox/trunk@236 bd3a7434-8174-4811-bcf6-7b9fed35a952

--- app/controllers/packages_controller.php
+++ app/controllers/packages_controller.php
@@ -38,122 +38,14 @@ class PackagesController extends AppController {
 	}
 	
 	function carveRatePeriods($clientId = null, $id = null) {
-		// set recursive to 2 so we can access all the package loa item relations also
 		$this->Package->recursive = 2;
 		
-		$packageData = $this->Package->read(null, $id);
-
-		// the first record has the ids we need to handle all the rate periods and rate period item relations
-		if(!isset($packageData['PackageLoaItemRel'][0])) {
-			return;
-		}
-		$ratePeriodItemTemp = $packageData['PackageLoaItemRel'][0]['PackageRatePeriodItemRel'];
-
-		// let's delete ALL the package loa item relations and and package rate periods related to THIS package id
-		foreach ($ratePeriodItemTemp as $rel) {
-			$this->Package->PackageLoaItemRel->PackageRatePeriodItemRel->deleteAll(array('PackageRatePeriodItemRel.packageRatePeriodId' => $rel['packageRatePeriodId']));
-			$this->Package->PackageLoaItemRel->PackageRatePeriodItemRel->PackageRatePeriod->deleteAll(array('PackageRatePeriod.packageRatePeriodId' => $rel['packageRatePeriodId']));
-		}
+		$packageStartDate = $this->data['Package']['validityStartDate']['year'] . '-' . $this->data['Package']['validityStartDate']['month'] . '-' . $this->data['Package']['validityStartDate']['day'];
+		$packageEndDate = $this->data['Package']['validityEndDate']['year'] . '-' . $this->data['Package']['validityEndDate']['month'] . '-' . $this->data['Package']['validityEndDate']['day'];
 		
-		// retrieve all loa items related to this package id
-		$data = $this->Package->PackageLoaItemRel->LoaItem->find('all', array('conditions' => array('PackageLoaItemRel.packageId' => $id)));
+		$carvedRatePeriods = $this->Package->PackageLoaItemRel->LoaItem->carveRatePeriods($this->data['Package']['CheckedLoaItems'], $this->data['PackageLoaItemRel'], $packageStartDate, $packageEndDate);
 
-		// populate with loa items and their rate periods
-		$itemRatePeriods = array(); 
-		$packageLoaItemRel = array();
-		
-		/*  ======= CARVING ==============
-		 *  Include the package start and end dates
-		 *  Include all unique loa item rate periods
-		 *  ==============================
-		 */
-		 
-		$packageDates = array();
-		$packageDates[] = $packageStartDate = substr($packageData['Package']['startDate'], 0, 10);
-		$packageDates[] = $packageEndDate = substr($packageData['Package']['endDate'], 0, 10);
-								
-		// go through every loa item rate period
-		foreach ($data as $k => $v) {
-			foreach ($v['LoaItemRatePeriod'] as $a => $b) {
-				if (($b['startDate'] >= $packageStartDate) && ($b['startDate'] <= $packageEndDate) && !in_array($b['startDate'], $packageDates)) {
-					$packageDates[] = $b['startDate'];
-				}
-				if (($b['endDate'] >= $packageStartDate) && ($b['endDate'] <= $packageEndDate) && !in_array($b['endDate'], $packageDates)) { 
-					$packageDates[] = $b['endDate'];
-				}
-				// add the package loa item relation id so we can use as lookup later below
-				$b['packageLoaItemRelId'] = $v['PackageLoaItemRel']['packageLoaItemRelId'];
-				
-				// add the item base price 
-				$b['itemBasePrice'] = $v['LoaItem']['itemBasePrice'];
-				
-				// finally add essential data we need to the overall loa item array
-				$itemRatePeriods[] = $b;
-			}
-			// load this array with overall package to item relation
-			$packageLoaItemRel[$v['PackageLoaItemRel']['packageLoaItemRelId']] = $v['PackageLoaItemRel'];
-		}
-
-		// we now have package rate period dates so sort the dates
-		sort($packageDates);
-	
-		$one_day = 24 * 60 * 60;
-		$count = count($packageDates) - 1;
-
-		// cycle through each item rate period using $packageDates start-end pairs.
-		// for each start-end pair, use $packageLoaItemRelId to obtain item prices and ids.
-
-		for ($i=0; $i < $count; $i++) {
-			$packageLoaItemRelId = array();
-			$rangeStart = strtotime($packageDates[$i]);
-			$rangeEnd = strtotime($packageDates[($i + 1)]) - $one_day;
-			
-			foreach ($itemRatePeriods as $v) {
-				// if the item rate period falls within the start-end pair, then this rate period has a valid item and use the approved retail price, other use base price
-				$ratePeriodItemPrice = (($rangeStart >= strtotime($v['startDate'])) && ($rangeEnd <= strtotime($v['endDate']))) ? $v['approvedRetailPrice'] : $v['itemBasePrice'];		
-
-				// if there is an price override, use it.  if there is a no charge, then the price is zeroed out
-				if ($packageLoaItemRel[$v['packageLoaItemRelId']]['priceOverride']) {
-					$ratePeriodItemPrice = $packageLoaItemRel[$v['packageLoaItemRelId']]['priceOverride'];
-				}
-				if ($packageLoaItemRel[$v['packageLoaItemRelId']]['noCharge']) {
-					$ratePeriodItemPrice = 0;	
-				}
-				$packageLoaItemRelId[$v['packageLoaItemRelId']] = $ratePeriodItemPrice * $packageLoaItemRel[$v['packageLoaItemRelId']]['quantity'];
-				
-			}
-
-			// create package rate period for this start-end pair
-			/*
-			$insertSql = "INSERT INTO packageRatePeriod SET packageRatePeriodName = 'PACKAGE RATE PERIOD', ";
-			$insertSql.= "startDate = '" . date('Y-m-d', $rangeStart) . "', endDate = '" . date('Y-m-d', $rangeEnd) . "', ";
-			$insertSql.= "approvedRetailPrice = " . array_sum($packageLoaItemRelId) . ", approved = 0, approvedBy = 'AUTO';";
-			*/
-			$packageRatePeriod = array('PackageRatePeriod' => array('packageRatePeriodName' => 'PACKAGE RATE PERIOD',
-																	'startDate' => date('Y-m-d', $rangeStart),
-																	'endDate' => date('Y-m-d', $rangeEnd),
-																	'approvedRetailPrice' => array_sum($packageLoaItemRelId),
-																	'approved' => 0,
-																	'approvedBy' => 'AUTO'));
-			// now we can populate the packageRatePeriodItemRel because we have a package rate period
-			$this->PackageRatePeriod->create();
-			$this->PackageRatePeriod->set($packageRatePeriod);
-			if ($this->PackageRatePeriod->save()) {
-				$packageRatePeriodId = $this->PackageRatePeriod->id;
-
-				foreach ($packageLoaItemRelId as $id => $price) {
-					$packageRatePeriodItemRel = array(
-										'PackageRatePeriodItemRel' => array(
-																'packageRatePeriodId' => $packageRatePeriodId,
-																'packageLoaItemRelId' => $id,
-																'ratePeriodPrice' => $price
-																	)
-														);
-					$this->PackageRatePeriodItemRel->create();
-					$this->PackageRatePeriodItemRel->save($packageRatePeriodItemRel);
-				}	
-			}
-		}
+		$this->data['PackageRatePeriod'] = $carvedRatePeriods['PackageRatePeriod'];
 	}
 	
 	function carveRatePeriodsForDisplay() {
@@ -166,8 +58,8 @@ class PackagesController extends AppController {
 		$currencyCodes = $this->Package->Currency->find('list', array('fields' => 'currencyCode'));
 		$this->set('currencyCodes', $currencyCodes);
 
-		$packageStartDate = $this->data['Package']['startDate']['year'] . '-' . $this->data['Package']['startDate']['month'] . '-' . $this->data['Package']['startDate']['day'];
-		$packageEndDate = $this->data['Package']['endDate']['year'] . '-' . $this->data['Package']['endDate']['month'] . '-' . $this->data['Package']['endDate']['day'];
+		$packageStartDate = $this->data['Package']['validityStartDate']['year'] . '-' . $this->data['Package']['validityStartDate']['month'] . '-' . $this->data['Package']['validityStartDate']['day'];
+		$packageEndDate = $this->data['Package']['validityEndDate']['year'] . '-' . $this->data['Package']['validityEndDate']['month'] . '-' . $this->data['Package']['validityEndDate']['day'];
 		
 		$carvedRatePeriods = $this->Package->PackageLoaItemRel->LoaItem->carveRatePeriods($this->data['Package']['CheckedLoaItems'], $this->data['PackageLoaItemRel'], $packageStartDate, $packageEndDate);
 
@@ -187,11 +79,12 @@ class PackagesController extends AppController {
 			
 			//the first saveAll saves packages and all associated date
 			//the second save saves the HABTM format relationships
+			$this->carveRatePeriods($clientId, $packageId);
 
 			if ($this->Package->saveAll($this->data) && $this->Package->save($this->data)) {
 				$packageId = $this->Package->getLastInsertID();
 				$this->addPackageOfferTypeDefFieldRel($packageId);
-				$this->carveRatePeriods($clientId, $packageId);
+				
 				$this->Session->setFlash(__('The Package has been saved', true), 'default', array(), 'success');
 				$this->redirect("/clients/$clientId/packages");
 			} else {
@@ -390,9 +283,10 @@ class PackagesController extends AppController {
 			$this->redirect(array('controller' => 'clients', 'action'=>'index'));
 		}
 		if (!empty($this->data)) {
+			$this->carveRatePeriods($clientId);
+			debug($this->data);
 			if ($this->updatePackageLoaItems() && $this->Package->saveAll($this->data) && $this->Package->save($this->data)) {
 				$this->addPackageOfferTypeDefFieldRel($id);
-				$this->carveRatePeriods($clientId, $id);
 				$this->Session->setFlash(__('The Package has been saved', true), 'default', array(), 'success');
 				$this->redirect("/clients/$clientId/packages/edit/$id");
 			} else {
