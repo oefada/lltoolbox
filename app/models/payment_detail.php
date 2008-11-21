b5145e416a7aea86bc91a5205535facf7f259628
alee <alee@bd3a7434-8174-4811-bcf6-7b9fed35a952>
2008-11-21 17:48:27 +0000

    ticketing updates and payment detail (cc processing)
    git-svn-id: http://repos.luxurylink.com/svn/repos/toolbox/trunk@317 bd3a7434-8174-4811-bcf6-7b9fed35a952

--- app/models/payment_detail.php
+++ app/models/payment_detail.php
@@ -10,5 +10,29 @@ class PaymentDetail extends AppModel {
 						   'PaymentProcessor' => array('foreignKey' => 'paymentProcessorId')
 						  );
 
+
+	var $validate = array(
+					    'initials' => array(
+					        'rule' => 'alphaNumeric', 
+					        'required' => true,
+					        'allowEmpty' => false,
+					        'on' => 'create',
+					        'message' => 'Initials are required before submitting a payment.'
+					    ),
+					     'userPaymentSettingId' => array(
+					        'rule' => 'numeric', 
+					        'required' => true,
+					        'allowEmpty' => false,
+					        'on' => 'create',
+					        'message' => 'You must choose one of the User Payment Settings above.'
+					    ),
+					     'paymentAmount' => array(
+					        'rule' => 'numeric', 
+					        'required' => true,
+					        'allowEmpty' => false,
+					        'on' => 'create',
+					        'message' => 'Payment amount must be in integer and greater than 0.'
+					    )
+					);
 }
 ?>
