e84ce5c8faf07e9d456cdd1076a0e11e63a996f6
vgarcia <vgarcia@bd3a7434-8174-4811-bcf6-7b9fed35a952>
2008-12-06 11:12:17 +0000

    Missing sessions controller. [#13]
    git-svn-id: http://repos.luxurylink.com/svn/repos/toolbox/trunk@389 bd3a7434-8174-4811-bcf6-7b9fed35a952

---
+++ app/controllers/sessions_controller.php
@@ -0,0 +1,32 @@
+<?php
+class SessionsController extends AppController {
+
+	var $name = 'Sessions';
+	var $uses = array('LdapUser');
+	var $components = array('Auth');
+	var $layout = 'login';
+	
+	function login() {
+	    $this->Auth->logout();
+	    Configure::write('debug', '0');
+	    if (!empty($this->data)) {
+	        $user = $this->LdapUser->auth($this->data['LdapUser']['username'], $this->data['LdapUser']['password']); 
+            $this->Auth->userModel = 'LdapUser';
+	        if ($this->Auth->login($user)) {
+				if ($this->Auth->autoRedirect) {
+					$this->redirect($this->Auth->redirect(), null, true);
+				}
+			} else {
+				$this->Session->setFlash($this->Auth->loginError, 'default', array(), 'auth');
+				$this->data[$this->Auth->userModel]['password'] = null;
+			}
+	    }
+	}
+	
+	function logout(){
+        $this->Session->setFlash('Logout');
+	    $this->redirect($this->Auth->logout());
+    }
+}
+
+?>
