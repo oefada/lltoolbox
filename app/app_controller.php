61ea7769aee1c772c5e9647d1c248728b32988c0
alee <alee@bd3a7434-8174-4811-bcf6-7b9fed35a952>
2008-12-08 17:49:59 +0000

    testing webservices on toolboxdev -- auth/ldap disabled for now
    git-svn-id: http://repos.luxurylink.com/svn/repos/toolbox/trunk@399 bd3a7434-8174-4811-bcf6-7b9fed35a952

--- app/app_controller.php
+++ app/app_controller.php
@@ -39,16 +39,18 @@
 uses('sanitize');
 class AppController extends Controller {
 	var $helpers = array('Html2', 'Form', 'Text', 'Pagination', 'Layout', 'Ajax', 'StrictAutocomplete', 'Number', 'DatePicker', 'Prototip', 'Session');
-	var $components = array('RequestHandler', 'Auth');
+	var $components = array('RequestHandler');
 	var $Sanitize;
 	
 	function beforeFilter() {
+		/*
         $this->Auth->loginAction = array('controller' => 'sessions', 'action' => 'login');  
         $this->Auth->loginRedirect = array('controller' => 'pages', 'action' => 'display', 'home');
         $this->Auth->allow('none');
         $this->Auth->loginError = "Could not log you in, please try again.";
         $this->Auth->authError = "Insufficient access rights.<br />Must be logged in, or logged in with elevated access.";
         $this->Auth->authorize = 'controller';
+        */
         
 		$this->Sanitize = new Sanitize();
 		
