258f164773577d8001c18872b99cdf0e9b76fbf8
alee <alee@bd3a7434-8174-4811-bcf6-7b9fed35a952>
2008-12-22 17:48:10 +0000

    git-svn-id: http://repos.luxurylink.com/svn/repos/toolbox/trunk@525 bd3a7434-8174-4811-bcf6-7b9fed35a952

--- app/controllers/web_service_tickets_controller.php
+++ app/controllers/web_service_tickets_controller.php
@@ -386,6 +386,7 @@ class WebServiceTicketsController extends WebServicesController
 	}
 
 	function updateTrackPending($ticketId) {
+		mail('alee@luxurylink.com', 'test',$ticketId);
 		if (!$ticketId) {
 			return false;	
 		}
@@ -402,6 +403,7 @@ class WebServiceTicketsController extends WebServicesController
 		} else {
 					
 		}
+		mail('alee@luxurylink.com','track', print_r($revenueModelLoaRel, true));
 	}
 	
 	function updateTrackDetail($in0) {
