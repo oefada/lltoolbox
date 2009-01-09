45e09ae346de588c06f9b029a6bdd3ea714eaa25
alee <alee@bd3a7434-8174-4811-bcf6-7b9fed35a952>
2009-01-09 02:17:14 +0000

    git-svn-id: http://repos.luxurylink.com/svn/repos/toolbox/trunk@603 bd3a7434-8174-4811-bcf6-7b9fed35a952

--- app/controllers/web_service_new_clients_controller.php
+++ app/controllers/web_service_new_clients_controller.php
@@ -76,8 +76,6 @@ class WebServiceNewClientsController extends WebServicesController
 			
 			// get new client id and send back to Sugar
 			$decoded_request['client']['client_id'] = $this->Client->getLastInsertID();
-			$tmp = print_r($decoded_request, true) . print_r($client_data_save, true);
-			mail('alee@luxurylink.com','testing client 3', $tmp);
 		}
 		
 		//$decoded_request['client']['client_desc'];        
@@ -95,25 +93,6 @@ class WebServiceNewClientsController extends WebServicesController
         //$decoded_request['client']['client_cell1'];
         //$decoded_request['client']['client_cell2'];
         //$decoded_request['client']['client_cell3'];
-		
-		/*
-		// ======== THIS IS OLD WAY
-	    // this SPROC inserts or updates and checks for client active / expiration dates
-	    //$result = ll_execute_sproc('llsp_upd_client_mstr_from_sugar', $decoded_request['client']);
-	
-	    // DB error
-	    if (!$result || !mssql_num_rows($result)) {
-	        $response_value = '-2';
-	    } else {
-	        // result set with updated fields after the sproc
-	        $sm_sproc_response = mssql_fetch_array($result);
-	
-	        foreach ($decoded_request['client'] as $k => $v) {
-	            $decoded_request['client'][$k] = $sm_sproc_response[$k];
-	        }
-	    }
-	
-		*/
 	
 	    $decoded_request['request']['response'] = $response_value;
 	    $decoded_request['request']['response_time'] = time();
@@ -121,7 +100,7 @@ class WebServiceNewClientsController extends WebServicesController
 	    $encoded_response = json_encode($decoded_request);
 	
 	    // sugar has to update appropiate site manger id.  send to esb client id and sugar id
-	    $client = new nusoap_client('http://192.168.100.22:8888/services2/ClientReceiver2?wsdl',true);
+	    $client = new nusoap_client('http://sugardev.luxurylink.com:8888/services2/ClientReceiver2?wsdl',true);
 	    $response_esb = $client->call('soap_call', array('args' => $encoded_response));
 	
 	    // this tests to see if we were getting correct response from the request
