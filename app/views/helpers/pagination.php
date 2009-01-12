b6a2b45e0f628912f960a4a4f860cdd3214cd8af
vgarcia <vgarcia@bd3a7434-8174-4811-bcf6-7b9fed35a952>
2009-01-12 23:25:00 +0000

    git-svn-id: http://repos.luxurylink.com/svn/repos/toolbox/trunk@607 bd3a7434-8174-4811-bcf6-7b9fed35a952

--- app/views/helpers/pagination.php
+++ app/views/helpers/pagination.php
@@ -19,7 +19,7 @@ class PaginationHelper extends AppHelper
         // Init
         if ( $show < 1 ) $show = 1;                        // make sure you're showing at least 1
         $show_mid = ceil( $show / 2 );                    // find the midpoint of the shown page numbers / skips
-        $skip = '<span class="skip">'.$skip.'</span>';    // add spans around skip text
+        $skip = '<span class="current">'.$skip.'</span>';    // add spans around skip text
         $out = "\n";
 
         // Figure out start point for In-between numbers
@@ -29,14 +29,14 @@ class PaginationHelper extends AppHelper
 
         // Previous link
         $out .= ( ($page-1) > 0 )
-                    ? $this->{$obj}->link( 'Prev', $link.($page-1), array_merge(array('title'=>'View the previous index page', 'class'=>'nextprev'), $options) ) 
-                    : '<span class="nextprev">Prev</span>';
+                    ? $this->{$obj}->link( '&lt;&lt; Prev', $link.($page-1), array_merge(array('title'=>'View the previous index page', 'class'=>'nextprev'), $options), array(), false, false ) 
+                    : '<div class="disabled">&lt;&lt; Prev</div>';
         $out .= "\n";
 
         // First number
         $out .= ( $page == 1 )
                     ? '<span class="current">1</span>'
-                    : $this->{$obj}->link( '1', $link.'1', array_merge(array('title'=>'View index page 1'), $options) );
+                    : '<span>'.$this->{$obj}->link( '1', $link.'1', array_merge(array('title'=>'View index page 1'), $options) ).'</span>';
         $out .= "\n";
 
         // In-between numbers
@@ -48,7 +48,7 @@ class PaginationHelper extends AppHelper
                 $out .= ( $start == 2 ) 
                             ? ( $page == 2 )
                                 ? '<span class="current">2</span>'
-                                : $this->{$obj}->link( '2', $link.'2', array_merge(array('title'=>'View index page 2'), $options) )
+                                : '<span>'.$this->{$obj}->link( '2', $link.'2', array_merge(array('title'=>'View index page 2'), $options) ).'</span>'
                             : $skip;
             }
 
@@ -58,7 +58,7 @@ class PaginationHelper extends AppHelper
                 $out .= ( $start >= ($total-$show) ) 
                             ? ( $page == ($total-1) )
                                 ? '<span class="current">'.($total-1).'</span>'
-                                : $this->{$obj}->link( ($total-1), $link.($total-1), array_merge(array('title'=>'View index page '.($total-1)), $options) )
+                                : '<span>'.$this->{$obj}->link( ($total-1), $link.($total-1), array_merge(array('title'=>'View index page '.($total-1)), $options) ).'</span>'
                             : $skip;
             }
 
@@ -67,7 +67,7 @@ class PaginationHelper extends AppHelper
             {
                 $out .= ( $page == ($start+$i) )
                             ? '<span class="current">'.($start+$i).'</span>'
-                            : $this->{$obj}->link( ($start+$i), $link.($start+$i), array_merge(array('title'=>'View index page '.($start+$i)), $options) );
+                            : '<span>'.$this->{$obj}->link( ($start+$i), $link.($start+$i), array_merge(array('title'=>'View index page '.($start+$i)), $options) ).'</span>';
             }
 
             $out .= "\n";
@@ -78,18 +78,18 @@ class PaginationHelper extends AppHelper
         {
             $out .= ( $page == $total )
                         ? '<span class="current">'.$total.'</span>'
-                        : $this->{$obj}->link( $total, $link.$total, array_merge(array('title'=>'View index page '.$total), $options) );
+                        : '<span>'.$this->{$obj}->link( $total, $link.$total, array_merge(array('title'=>'View index page '.$total), $options) ).'</span>';
             $out .= "\n";
         }
 
         // Next link
         $out .= ( ($page+1) <= $total )
-                    ? $this->{$obj}->link( 'Next', $link.($page+1), array_merge(array('title'=>'View the next index page', 'class'=>'nextprev'), $options) )
-                    : '<span class="nextprev">Next</span>';
+                    ? $this->{$obj}->link( 'Next &gt;&gt;', $link.($page+1), array_merge(array('title'=>'View the next index page', 'class'=>'nextprev'), $options), array(), false, false )
+                    : '<div class="disabled">Next &gt;&gt;</div>';
         $out .= "\n";
 
         // Return
-        return '<div class="pagination">'.$out.'</div>';
+        return '<div class="paging-container"><div class="paging">'.$out.'</div></div>';
     }
 
 }
