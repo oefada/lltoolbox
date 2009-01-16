5875c340bbd5059271ef84d6c9dbfdb02b2b8c6b
vgarcia <vgarcia@bd3a7434-8174-4811-bcf6-7b9fed35a952>
2009-01-16 01:04:09 +0000

    Filtering membership fee and balance > 0 [#86]
    git-svn-id: http://repos.luxurylink.com/svn/repos/toolbox/trunk@654 bd3a7434-8174-4811-bcf6-7b9fed35a952

--- app/controllers/reports_controller.php
+++ app/controllers/reports_controller.php
@@ -502,6 +502,7 @@ class ReportsController extends AppController {
                                 FROM client AS Client
                                 INNER JOIN loa AS Loa ON (Loa.clientId = Client.clientId)
             					WHERE Loa.startDate BETWEEN DATE_ADD(NOW(), INTERVAL -90 DAY) AND NOW()
+            					        AND Loa.membershipBalance > 0 AND Loa.membershipFee > 0
                                 GROUP BY Client.clientId, Loa.loaId
                                 ORDER BY Loa.startDate DESC";
 
@@ -515,6 +516,7 @@ class ReportsController extends AppController {
                                 FROM client AS Client
                                 INNER JOIN loa AS Loa ON (Loa.clientId = Client.clientId)
             					WHERE Loa.startDate BETWEEN DATE_ADD(NOW(), INTERVAL -180 DAY) AND DATE_ADD(NOW(), INTERVAL -90 DAY)
+            					    AND Loa.membershipBalance > 0 AND Loa.membershipFee > 0
                                 GROUP BY Client.clientId, Loa.loaId
                                 ORDER BY Loa.startDate DESC";
 
@@ -528,6 +530,7 @@ class ReportsController extends AppController {
                                 FROM client AS Client
                                 INNER JOIN loa AS Loa ON (Loa.clientId = Client.clientId)
             					WHERE Loa.startDate BETWEEN DATE_ADD(NOW(), INTERVAL -270 DAY) AND DATE_ADD(NOW(), INTERVAL -180 DAY)
+            					    AND Loa.membershipBalance > 0 AND Loa.membershipFee > 0
                                 GROUP BY Client.clientId, Loa.loaId
                                 ORDER BY Loa.startDate DESC";
 
@@ -541,6 +544,7 @@ class ReportsController extends AppController {
                                 FROM client AS Client
                                 INNER JOIN loa AS Loa ON (Loa.clientId = Client.clientId)
             					WHERE Loa.startDate BETWEEN DATE_ADD(NOW(), INTERVAL -1 YEAR) AND DATE_ADD(NOW(), INTERVAL -270 DAY)
+            					    AND Loa.membershipBalance > 0 AND Loa.membershipFee > 0
                                 GROUP BY Client.clientId, Loa.loaId
                                 ORDER BY Loa.startDate DESC";
 
