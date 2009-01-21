247a36d6bc5868adaf2b42a9c2124017a965f18c
alee <alee@bd3a7434-8174-4811-bcf6-7b9fed35a952>
2009-01-21 00:02:11 +0000

    git-svn-id: http://repos.luxurylink.com/svn/repos/toolbox/trunk@668 bd3a7434-8174-4811-bcf6-7b9fed35a952

--- app/controllers/web_service_new_clients_controller.php
+++ app/controllers/web_service_new_clients_controller.php
@@ -108,11 +108,9 @@ class WebServiceNewClientsController extends WebServicesController
 	    $encoded_response = json_encode($decoded_request);
 	
 	    // sugar has to update appropiate site manger id.  send to esb client id and sugar id
-	    $webservice_live_url = 'http://sugardev.luxurylink.com:8888/services2/ClientReceiver2?wsdl';
-		
-	    $soap_client = new soap_client($webservice_live_url, true);
-	    $response_esb = $soap_client->call('soap_call', array('args' => $encoded_response));
-	    unset($soap_client);
+	    $soap_client_sugar = new soap_client('http://sugardev.luxurylink.com:8888/services2/ClientReceiver2?wsdl', true);
+	    $response_esb = $soap_client_sugar->call('soap_call', array('args' => $encoded_response));
+	    unset($soap_client_sugar);
 		mail('alee@luxurylink.com','test 4', print_r($response_esb, true));
 	
 	    // this tests to see if we were getting correct response from the request
