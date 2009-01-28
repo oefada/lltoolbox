1d0a3e09111ef6f4a01206598b026f8d9f3ba058
vgarcia <vgarcia@bd3a7434-8174-4811-bcf6-7b9fed35a952>
2009-01-28 21:33:12 +0000

    More cake updates
    git-svn-id: http://repos.luxurylink.com/svn/repos/toolbox/trunk@726 bd3a7434-8174-4811-bcf6-7b9fed35a952

--- app/config/bootstrap.php
+++ app/config/bootstrap.php
@@ -7,24 +7,22 @@
  *
  * PHP versions 4 and 5
  *
- * CakePHP(tm) :  Rapid Development Framework <http://www.cakephp.org/>
- * Copyright 2005-2008, Cake Software Foundation, Inc.
- *								1785 E. Sahara Avenue, Suite 490-204
- *								Las Vegas, Nevada 89104
+ * CakePHP(tm) :  Rapid Development Framework (http://www.cakephp.org)
+ * Copyright 2005-2008, Cake Software Foundation, Inc. (http://www.cakefoundation.org)
  *
  * Licensed under The MIT License
  * Redistributions of files must retain the above copyright notice.
  *
  * @filesource
- * @copyright		Copyright 2005-2008, Cake Software Foundation, Inc.
- * @link				http://www.cakefoundation.org/projects/info/cakephp CakePHP(tm) Project
- * @package			cake
- * @subpackage		cake.app.config
- * @since			CakePHP(tm) v 0.10.8.2117
- * @version			$Revision$
- * @modifiedby		$LastChangedBy$
- * @lastmodified	$Date$
- * @license			http://www.opensource.org/licenses/mit-license.php The MIT License
+ * @copyright     Copyright 2005-2008, Cake Software Foundation, Inc. (http://www.cakefoundation.org)
+ * @link          http://www.cakefoundation.org/projects/info/cakephp CakePHP(tm) Project
+ * @package       cake
+ * @subpackage    cake.app.config
+ * @since         CakePHP(tm) v 0.10.8.2117
+ * @version       $Revision$
+ * @modifiedby    $LastChangedBy$
+ * @lastmodified  $Date$
+ * @license       http://www.opensource.org/licenses/mit-license.php The MIT License
  */
 /**
  *
@@ -42,21 +40,21 @@
  * $controllerPaths = array('this path to controllers', 'second full path to controllers', 'etc...');
  *
  */
-if (!defined('REVISION')) {
-    if (file_exists(APP . '.svn' . DS . 'entries')) {
-        $svn = file(APP . '.svn' . DS . 'entries');
-        if (is_numeric(trim($svn[3]))) {
-            $version = $svn[3];
-        } else { // pre 1.4 svn used xml for this file
-            $version = explode('"', $svn[4]);
-            $version = $version[1];    
-        }
-        define ('REVISION', trim($version));
-        unset ($svn);
-        unset ($version);
-    } else {
-        define ('REVISION', 0); // default if no svn data avilable
-    }
-}
+ if (!defined('REVISION')) {
+     if (file_exists(APP . '.svn' . DS . 'entries')) {
+         $svn = file(APP . '.svn' . DS . 'entries');
+         if (is_numeric(trim($svn[3]))) {
+             $version = $svn[3];
+         } else { // pre 1.4 svn used xml for this file
+             $version = explode('"', $svn[4]);
+             $version = $version[1];    
+         }
+         define ('REVISION', trim($version));
+         unset ($svn);
+         unset ($version);
+     } else {
+         define ('REVISION', 0); // default if no svn data avilable
+     }
+ }
 //EOF
 ?>
