e0bdcca915d0e7041bf959f564c4d8e42680546b
vgarcia <vgarcia@bd3a7434-8174-4811-bcf6-7b9fed35a952>
2009-01-29 10:04:44 +0000

    Validation was causing this to break on our test server.
    git-svn-id: http://repos.luxurylink.com/svn/repos/toolbox/trunk@741 bd3a7434-8174-4811-bcf6-7b9fed35a952

--- app/models/admin_user.php
+++ app/models/admin_user.php
@@ -3,12 +3,6 @@ class AdminUser extends AppModel {
     var $name = 'AdminUser';
     var $actsAs = array('Acl');
     
-    var $validate = array(
-        'email' => array('email'),
-        'password' => array('alphaNumeric'),
-        'active' => array('numeric')
-    );
-    
     function parentNode() {
     }
 }
