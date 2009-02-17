a3ec0b0b2b2bc4ec8b1436a107148ed1c9c101f5
vgarcia <vgarcia@bd3a7434-8174-4811-bcf6-7b9fed35a952>
2009-02-17 00:59:10 +0000

    Bug fix.
    git-svn-id: http://repos.luxurylink.com/svn/repos/toolbox/trunk@975 bd3a7434-8174-4811-bcf6-7b9fed35a952

--- app/app_controller.php
+++ app/app_controller.php
@@ -72,7 +72,7 @@ class AppController extends Controller {
 		    Configure::write('debug', '0');
 		}
 
-		if (is_object($this->{$this->modelClass}) && $this->{$this->modelClass}->Behaviors->attached('Logable')) {
+		if (isset($this->{$this->modelClass}) && is_object($this->{$this->modelClass}) && $this->{$this->modelClass}->Behaviors->attached('Logable')) {
      	    $this->{$this->modelClass}->setUserData($user);
      	    $this->{$this->modelClass}->setUserIp($this->_userIp());
      	 }
