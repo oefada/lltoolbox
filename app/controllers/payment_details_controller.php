9017790c0aaa7125cba76e802d5eca054c2aa357
alee <alee@bd3a7434-8174-4811-bcf6-7b9fed35a952>
2009-02-23 17:59:50 +0000

    git-svn-id: http://repos.luxurylink.com/svn/repos/toolbox/trunk@1040 bd3a7434-8174-4811-bcf6-7b9fed35a952

--- app/controllers/payment_details_controller.php
+++ app/controllers/payment_details_controller.php
@@ -1,4 +1,7 @@
 <?php
+
+App::import('Vendor', 'aes.php');
+
 class PaymentDetailsController extends AppController {
 
 	var $name = 'PaymentDetails';
@@ -18,22 +21,111 @@ class PaymentDetailsController extends AppController {
 
 	function add() {
 		if (!empty($this->data)) {
-			$this->PaymentDetail->create();
-			if ($this->PaymentDetail->save($this->data)) {
-				$this->Session->setFlash(__('Payment was successfully charged.', true), 'default', array(), 'success');
-				//$this->redirect(array('controller' => 'tickets', 'action'=>'view', 'id' => $this->data['PaymentDetail']['ticketId']));
-				$this->redirect(array('controller' => 'tickets', 'action'=>'updateTrackDetail', 'id' => $this->data['PaymentDetail']['ticketId']));
-				die();
-			} else {
-				$this->Session->setFlash(__('The payment cannot be processed yet -- please correct the errors below first.', true), 'default', array(), 'error');
+			
+			$usingNewCard = 0;
+			$saveNewCard = 0;
+			
+			if (isset($this->data['UserPaymentSetting']['useNewCard'])) {
+				unset($this->data['UserPaymentSetting']['useNewCard']);
+				$usingNewCard = 1;
+			} 
+			if (isset($this->data['UserPaymentSetting']['save'])) {
+				unset($this->data['UserPaymentSetting']['save']);	
+				$saveNewCard = 1;	
 			}
+			
+			$badPaymentRequest = false;	
+			
+			$data = array();
+	        $data['userId']                 = $this->data['PaymentDetail']['userId'];
+	        $data['ticketId']               = $this->data['PaymentDetail']['ticketId'];
+	        $data['paymentProcessorId']     = $this->data['PaymentDetail']['paymentProcessorId'];
+	        $data['paymentAmount']          = $this->data['PaymentDetail']['billingPrice'];
+	        $data['initials']               = $this->data['PaymentDetail']['initials'];
+	        $data['autoCharge']             = 0;
+	        $data['saveUps']                = $saveNewCard;
+	        $data['zAuthHashKey']           = md5('L33T_KEY_LL' . $data['userId'] . $data['ticketId'] . $data['paymentProcessorId'] . $data['paymentAmount'] . $data['initials']);
+	        
+	        if ($usingNewCard) {
+	        	$data['userPaymentSetting'] = $this->data['UserPaymentSetting'];
+	        	$data['userPaymentSetting']['ccNumber'] 		= aesEncrypt($data['userPaymentSetting']['ccNumber']);
+	        } elseif ($this->data['PaymentDetail']['userPaymentSettingId']) {
+	        	$data['userPaymentSettingId'] = $this->data['PaymentDetail']['userPaymentSettingId'];
+	        } else {
+		        $badPaymentRequest = true;
+		    }
+	        
+	        if (!$badPaymentRequest) {
+	        	$paymentResponse = $this->sendToPayment(json_encode($data));
+	        	if (trim($paymentResponse) == 'CHARGE_SUCCESS') {
+	        		$this->Session->setFlash(__('Payment was successfully charged.', true), 'default', array(), 'success');
+	        		$this->redirect(array('controller' => 'tickets', 'action'=>'view', 'id' => $this->data['PaymentDetail']['ticketId']));
+	        	} else {
+	        		$this->Session->setFlash(__('Payment Not Processed -- Error: ' . $paymentResponse, true), 'default', array(), 'error');
+	        	}
+	        } else {
+	        	$this->Session->setFlash(__('No payment account selected.', true), 'default', array(), 'error');
+	        }
 		}
 		$this->PaymentDetail->Ticket->recursive = 2;
 		$ticket = $this->PaymentDetail->Ticket->read(null, $this->params['ticketId']);
+		$ticket['Ticket']['totalBillingAmount'] = in_array($ticket['Ticket']['offerTypeId'], array(1,2,6)) ? 30 : 40;
+		$ticket['Ticket']['totalBillingAmount'] += $ticket['Ticket']['billingPrice'];
 		$this->set('ticket', $ticket);
 		$this->set('userPaymentSetting', $ticket['User']['UserPaymentSetting']);
 		$this->set('paymentTypeIds', $this->PaymentDetail->PaymentType->find('list'));
 		$this->set('paymentProcessorIds', $this->PaymentDetail->PaymentProcessor->find('list'));		
+	}
+	
+	function sendToPayment($data) {
+		ini_set("soap.wsdl_cache_enabled", "0"); // disabling WSDL cache
+		$client = new SoapClient('http://toolbox.luxurylink.com/web_service_payments/?wsdl'); 
+		try {
+			$response = $client->processPayment($data);
+			return $response;
+		} catch (SoapFault $exception) {
+			@mail('geeks@luxurylink.com', 'WEB SERVICE ERROR:  paymentDetail LL manual charge error', $exception);
+			return false;
+		}
+	}
+
+	function test() {
+		$data['ppResponseDate'] 			= '2009-02-21 10:13:43';
+	    $data['ppTransactionId'] 			= '15FE8E86B-DDD9-F891-7432-D3C25AF1F6BF';
+	    $data['ppApprovalText'] 			= 'APPROVAL';
+	    $data['ppApprovalCode'] 			= 0;
+	    $data['ppAvsCode'] 					= 'Y';
+	    $data['ppResponseText'] 			= '';
+	    $data['ppResponseSubCode'] 			= '';
+	    $data['ppReasonCode'] 				= '';
+	    $data['isSuccessfulCharge'] 		= 1;
+	    $data['paymentTypeId'] 				= 1;
+	    $data['paymentAmount'] 				= 1385;
+	    $data['ticketId'] 					= 132654;
+	    $data['userId'] 					= 1197768;
+	    $data['userPaymentSettingId'] 		= '';
+	    $data['paymentProcessorId'] 		= 1;
+	    $data['ppFirstName'] 				= 'Wofford';
+	    $data['ppLastName'] 				= 'III';
+	    $data['ppBillingAddress1'] 			= '5421 Princess St';
+	    $data['ppBillingCity'] 				= 'Charlotte';
+	    $data['ppBillingState'] 			= 'NC';
+	    $data['ppBillingZip'] 				= 28269;
+		$data['ppBillingCountry'] 			= 'US';
+	    $data['ppCardNumLastFour'] 			= 5796;
+	    $data['ppExpMonth'] 				= 10;
+	    $data['ppExpYear'] 					= 2012;
+	    $data['ppBillingAmount'] 			= 1415;
+	    $data['autoProcessed'] 				= 0;
+	    $data['initials'] 					= 'USERCHECKOUT';
+		
+		$this->PaymentDetail->create();
+		if (!$this->PaymentDetail->save($data)) {
+			echo '1111111111';	
+		} else {
+			echo '22222222222';	
+		}
+		die('asdfs');	
 	}
 
 	// ----------------------------------------------------------
