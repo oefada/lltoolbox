b7bf5040b9683d5916515ecc6d838b6d1839f985
alee <alee@bd3a7434-8174-4811-bcf6-7b9fed35a952>
2009-02-26 17:18:25 +0000

    git-svn-id: http://repos.luxurylink.com/svn/repos/toolbox/trunk@1112 bd3a7434-8174-4811-bcf6-7b9fed35a952

--- app/controllers/web_service_new_clients_controller.php
+++ app/controllers/web_service_new_clients_controller.php
@@ -94,9 +94,13 @@ class WebServiceNewClientsController extends WebServicesController
 	function sendToSugar($data) {
 		// had to use this custom native soap class and functions because couldn't run both cakephp nusoap server and client
 		// this soap call to made to sugar in order to give Sugar the new clientId from toolbox so it's recorded in Sugar
+		mail('devmail@luxurylink.com','testing sugar postback 1', $data);
 		$client = new SoapClient('http://sugarprod.luxurylink.com:8888/services2/ClientReceiver2?wsdl'); 
+		mail('devmail@luxurylink.com','testing sugar postback 2', $data);
 		try {
+			mail('devmail@luxurylink.com','testing sugar postback 3', $data);
 			$client->soap_call($data);
+			mail('devmail@luxurylink.com','testing sugar postback 4', $data);
 		} catch (SoapFault $exception) {
 			@mail('devmail@luxurylink.com', 'WEB SERVICE UPDATE CLIENT FROM SUGAR : Could not send postback to Sugar', $exception);
 		}
