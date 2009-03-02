43533dcee108d28ac3c0c38b467b0574835fa345
alee <alee@bd3a7434-8174-4811-bcf6-7b9fed35a952>
2009-03-02 23:42:03 +0000

    git-svn-id: http://repos.luxurylink.com/svn/repos/toolbox/trunk@1156 bd3a7434-8174-4811-bcf6-7b9fed35a952

--- app/models/track_detail.php
+++ app/models/track_detail.php
@@ -4,5 +4,7 @@ class TrackDetail extends AppModel {
 	var $name = 'TrackDetail';
 	var $useTable = 'trackDetail';
 	var $primaryKey = 'trackDetailId';
+	
+	var $belongsTo = array('Track' => array('foreignKey' => 'trackId'));
 }
 ?>
