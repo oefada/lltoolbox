6b282b186fc176f398d1f008d7a59b7135029ac8
vgarcia <vgarcia@bd3a7434-8174-4811-bcf6-7b9fed35a952>
2009-03-03 23:27:44 +0000

    Bid report improvements
    git-svn-id: http://repos.luxurylink.com/svn/repos/toolbox/trunk@1160 bd3a7434-8174-4811-bcf6-7b9fed35a952

--- app/controllers/reports_controller.php
+++ app/controllers/reports_controller.php
@@ -263,21 +263,22 @@ class ReportsController extends AppController {
             				(SELECT Country.countryName FROM country AS Country WHERE Country.countryId = Client.countryId) AS country,
             				(SELECT State.stateName FROM state AS State WHERE State.stateId = Client.stateId) AS state,
                         	(SELECT City.cityName FROM city AS City WHERE City.cityId = Client.cityId) AS city,
-                            (SchedulingMaster.openingBid / Package.approvedRetailPrice * 100) AS percentMinBid,
-                            (Ticket.billingPrice / Package.approvedRetailPrice * 100) AS percentClose,
-                        	Package.approvedRetailPrice,
-                        	Package.numNights,
+                            (SchedulingMaster.openingBid / OfferLive.retailValue * 100) AS percentMinBid,
+                            (Ticket.billingPrice / OfferLive.retailValue * 100) AS percentClose,
+                        	OfferLive.retailValue,
+                        	OfferLive.roomNights,
                         	SchedulingInstance.endDate,
                         	COUNT(Bid.bidId) AS numBids,
                         	COUNT(DISTINCT Bid.userId) AS uniqueBids,
-                        	COUNT(DISTINCT Ticket2.ticketId) AS numTickets,
-                        	(SELECT SUM(Ticket2.billingPrice) FROM ticket AS Ticket2 WHERE Ticket2.offerId = Offer.offerId) as moneyPotential,
-                        	COUNT(DISTINCT Ticket.ticketId) AS numTicketsCollected,
-                        	(SELECT SUM(Ticket3.billingPrice) FROM ticket AS Ticket3 WHERE Ticket3.offerId = Offer.offerId AND Ticket3.ticketStatusId = 6) as moneyCollected
+                        	COUNT(DISTINCT Ticket.ticketId) AS numTickets,
+                        	(SELECT SUM(Ticket3.billingPrice) FROM ticket AS Ticket3 WHERE Ticket3.offerId = Offer.offerId) as moneyPotential,
+                        	COUNT(DISTINCT Ticket2.ticketId) AS numTicketsCollected,
+                        	(SELECT SUM(Ticket4.billingPrice) FROM ticket AS Ticket4 WHERE Ticket4.offerId = Offer.offerId AND Ticket4.ticketStatusId IN(5,6)) as moneyCollected
                     FROM offer AS Offer
                     LEFT JOIN ticket AS Ticket ON (Ticket.offerId = Offer.offerId)
-                    LEFT JOIN ticket AS Ticket2 ON (Ticket2.offerId = Offer.offerId AND Ticket2.ticketStatusId = 6)
+                    LEFT JOIN ticket AS Ticket2 ON (Ticket2.offerId = Offer.offerId AND Ticket2.ticketStatusId IN(5,6))
                     LEFT JOIN bid AS Bid ON (Bid.offerId = Offer.offerId)
+                    INNER JOIN offerLive AS OfferLive ON (OfferLive.offerId = Offer.offerId)
                     INNER JOIN schedulingInstance AS SchedulingInstance ON (SchedulingInstance.schedulingInstanceId = Offer.schedulingInstanceId)
                     INNER JOIN schedulingMaster AS SchedulingMaster ON (SchedulingMaster.schedulingMasterId = SchedulingInstance.schedulingMasterId)
                     INNER JOIN offerType as OfferType ON (OfferType.offerTypeId = SchedulingMaster.offerTypeId)
