caa905df7cb2baa237111f2c450112271ec5babc
vgarcia <vgarcia@bd3a7434-8174-4811-bcf6-7b9fed35a952>
2009-03-04 22:43:13 +0000

    Order of loa item types
    git-svn-id: http://repos.luxurylink.com/svn/repos/toolbox/trunk@1184 bd3a7434-8174-4811-bcf6-7b9fed35a952

--- app/models/loa_item_type.php
+++ app/models/loa_item_type.php
@@ -1,9 +1,10 @@
 <?php
 class LoaItemType extends AppModel {
 
-	var $name = 'LoaItemType';
-	var $useTable = 'loaItemType';
-	var $primaryKey = 'loaItemTypeId';
-	var $displayField = 'loaItemTypeName';
+	var $name           = 'LoaItemType';
+	var $useTable       = 'loaItemType';
+	var $primaryKey     = 'loaItemTypeId';
+	var $displayField   = 'loaItemTypeName';
+	var $order          = 'loaItemTypeName';
 }
 ?>
