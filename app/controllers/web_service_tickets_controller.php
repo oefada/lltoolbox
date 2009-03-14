44e054e5b870f822c98a02fb3f7f67fe26efefc7
alee <alee@bd3a7434-8174-4811-bcf6-7b9fed35a952>
2009-03-14 01:17:30 +0000

    git-svn-id: http://repos.luxurylink.com/svn/repos/toolbox/trunk@1264 bd3a7434-8174-4811-bcf6-7b9fed35a952

--- app/controllers/web_service_tickets_controller.php
+++ app/controllers/web_service_tickets_controller.php
@@ -357,6 +357,8 @@ class WebServiceTicketsController extends WebServicesController
 		// retrieve data to fill out the email templates
 		// -------------------------------------------------------------------------------
 			
+		mail('devmail@luxurylink.com', 'testing', print_r($params, true));
+			
 		$this->Ticket->recursive = 0;
 		$this->Address->recursive = -1;
 		$this->ClientLoaPackageRel->recursive = 0;
