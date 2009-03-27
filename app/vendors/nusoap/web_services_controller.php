332dd9773cf68e03ad8b592577ee198c21a1b449
alee <alee@bd3a7434-8174-4811-bcf6-7b9fed35a952>
2009-03-27 22:28:50 +0000

    git-svn-id: http://repos.luxurylink.com/svn/repos/toolbox/trunk@1331 bd3a7434-8174-4811-bcf6-7b9fed35a952

--- app/vendors/nusoap/web_services_controller.php
+++ app/vendors/nusoap/web_services_controller.php
@@ -74,7 +74,6 @@ class WebServicesController extends AppController
 	{
 		App::import('Vendor','nusoap/nusoap');		
 		$cacheName="soapserver/{$this->name}.soapserver";		
-		mail('devmail@luxurylink.com','TESTING CON', print_r($_SERVER, true));
 		if ($this->server_cache && false)
 		{
 			//reads the instance from cache
