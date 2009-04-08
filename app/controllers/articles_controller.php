ca0eb6669154284e4c5417b3177650a4afe86408
root <root@bd3a7434-8174-4811-bcf6-7b9fed35a952>
2009-04-08 18:59:51 +0000

    git-svn-id: http://repos.luxurylink.com/svn/repos/toolbox/trunk@1387 bd3a7434-8174-4811-bcf6-7b9fed35a952

---
+++ app/controllers/articles_controller.php
@@ -0,0 +1,66 @@
+<?php
+class ArticlesController extends AppController {
+
+	var $name = 'Articles';
+	var $helpers = array('Html', 'Form');
+
+	function index() {
+		$this->Article->recursive = 0;
+		$this->set('articles', $this->paginate());
+	}
+
+	function view($id = null) {
+		if (!$id) {
+			$this->Session->setFlash(__('Invalid Article.', true));
+			$this->redirect(array('action'=>'index'));
+		}
+		$this->set('article', $this->Article->read(null, $id));
+	}
+
+	function add() {
+		if (!empty($this->data)) {
+			$this->Article->create();
+			if ($this->Article->save($this->data)) {
+				$this->Session->setFlash(__('The Article has been saved', true));
+				$this->redirect(array('action'=>'index'));
+			} else {
+				$this->Session->setFlash(__('The Article could not be saved. Please, try again.', true));
+			}
+		}
+		$destinations = $this->Article->Destination->find('list', array('order' => 'destinationName'));
+		$this->set('destinationIds', $destinations);
+	}
+
+	function edit($id = null) {
+		if (!$id && empty($this->data)) {
+			$this->Session->setFlash(__('Invalid Article', true));
+			$this->redirect(array('action'=>'index'));
+		}
+		if (!empty($this->data)) {
+			if ($this->Article->save($this->data)) {
+				$this->Session->setFlash(__('The Article has been saved', true));
+				$this->redirect(array('action'=>'index'));
+			} else {
+				$this->Session->setFlash(__('The Article could not be saved. Please, try again.', true));
+			}
+		}
+		if (empty($this->data)) {
+			$this->data = $this->Article->read(null, $id);
+		}
+		$destinations = $this->Article->Destination->find('list', array('order' => 'destinationName'));
+		$this->set('destinationIds', $destinations);
+	}
+
+	function delete($id = null) {
+		if (!$id) {
+			$this->Session->setFlash(__('Invalid id for Article', true));
+			$this->redirect(array('action'=>'index'));
+		}
+		if ($this->Article->del($id)) {
+			$this->Session->setFlash(__('Article deleted', true));
+			$this->redirect(array('action'=>'index'));
+		}
+	}
+
+}
+?>
