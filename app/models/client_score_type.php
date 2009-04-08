dc5e65dcf2dc66056f32c0da63dca89b7a8c8407
root <root@bd3a7434-8174-4811-bcf6-7b9fed35a952>
2009-04-08 18:59:27 +0000

    git-svn-id: http://repos.luxurylink.com/svn/repos/toolbox/trunk@1386 bd3a7434-8174-4811-bcf6-7b9fed35a952

---
+++ app/models/client_score_type.php
@@ -0,0 +1,9 @@
+<?php
+class ClientScoreType extends AppModel {
+
+	var $name = 'ClientScoreType';
+	var $useTable = 'clientScoreType';
+	var $primaryKey = 'clientScoreTypeId';
+	var $displayField = 'clientScoreTypeName';
+}
+?>
