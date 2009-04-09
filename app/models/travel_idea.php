e484e9d338e767b14b9e90ce81c064270414dcca
root <root@bd3a7434-8174-4811-bcf6-7b9fed35a952>
2009-04-09 18:09:50 +0000

    git-svn-id: http://repos.luxurylink.com/svn/repos/toolbox/trunk@1412 bd3a7434-8174-4811-bcf6-7b9fed35a952

---
+++ app/models/travel_idea.php
@@ -0,0 +1,14 @@
+<?php
+class TravelIdea extends AppModel {
+
+	var $name = 'TravelIdea';
+	var $useTable = 'travelIdea';
+	var $primaryKey = 'travelIdeaId';
+	var $displayField = 'travelIdeaHeader';
+	var $order = array('TravelIdea.travelIdeaHeader');
+	
+	var $hasMany = array('TravelIdeaItem' => array('foreignKey' => 'travelIdeaId'));
+	var $belongsTo = array('LandingPage' =>array('foreignKey' => 'landingPageId'));
+
+}
+?>
