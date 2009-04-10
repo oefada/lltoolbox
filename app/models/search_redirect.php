cadda0057348cf09b5a5b4a95231f687626ba994
vgarcia <vgarcia@bd3a7434-8174-4811-bcf6-7b9fed35a952>
2009-04-10 23:40:17 +0000

    git-svn-id: http://repos.luxurylink.com/svn/repos/toolbox/trunk@1442 bd3a7434-8174-4811-bcf6-7b9fed35a952

--- app/models/search_redirect.php
+++ app/models/search_redirect.php
@@ -5,9 +5,21 @@ class SearchRedirect extends AppModel {
 	var $useTable = 'searchRedirect';
 	var $primaryKey = 'searchRedirectId';
 	
-	var $validate = array('keyword' => array('alphaNumeric' => array('rule' => array('alphaNumeric'), 'message' => 'Only letters and numbers are allowed'),
+	var $validate = array('keyword' => array('regex' => array('rule' => array('validateKeyword'), 'message' => 'Only lower-cased letters and numbers are allowed'),
 	                                        'isUnique' => array('rule' => array('isUnique'), 'message' => 'This keyword already exists')
-	                                        )
+	                                        ),
+	                      'redirectUrl' => array('notEmpty')
 	                    );
+
+	/* This method basically duplicates Cake's Built-In alphaNumeric core rule.
+	 * However, there is a problem with PHP 5.1.6 when using the built-in rule,
+	 * PHP 5.1.6 is bugged and always returns 0
+	 */                
+	function validateKeyword() {
+	   if (!preg_match("/^[a-z0-9]+$/", $this->data['SearchRedirect']['keyword'])) {
+	       return false;
+	   }
+	   return true;
+	}
 }
 ?>
