80683350a4fd6bf810aa9fcc6b508f7d04fda9ea
root <root@bd3a7434-8174-4811-bcf6-7b9fed35a952>
2009-05-15 23:43:47 +0000

    [alee] fixed balances going under zero after ticket loa allocation for keep
    git-svn-id: http://repos.luxurylink.com/svn/repos/toolbox/trunk@1569 bd3a7434-8174-4811-bcf6-7b9fed35a952

--- app/models/track_detail.php
+++ app/models/track_detail.php
@@ -147,6 +147,26 @@ class TrackDetail extends AppModel {
 				// some debug email will go out here
 				break;
 		}
+
+		// if membership balance met for keep -- disperse allocation to remit and keep
+		if ($track['expirationCriteriaId'] == 1) {
+			$loa_result = $this->query("SELECT membershipBalance FROM loa WHERE loaId = $track[loaId] LIMIT 1");
+			if (!empty($loa_result)) {
+				$loa_membership_balance = $loa_result[0]['loa']['membershipBalance'];
+				if (($loa_membership_balance - $new_track_detail['amountKept']) < 0) {
+					if ($loa_membership_balance > 0) {
+						$new_track_detail['amountRemitted'] = $new_track_detail['amountRemitted'] + abs($loa_membership_balance - $new_track_detail['amountKept']);
+						$new_track_detail['amountKept'] = $loa_membership_balance;
+					} else {
+						$new_track_detail['amountRemitted'] = $new_track_detail['amountRemitted'] + $new_track_detail['amountKept'];
+						$new_track_detail['amountKept'] = 0;
+					}
+				}
+			} else {
+				mail('devmail@luxurylink.com', 'TICKET ALLOCATION: Could not find LOA', print_r($track, true));
+			}
+		}
+
 		return $new_track_detail;
 	}
 	
