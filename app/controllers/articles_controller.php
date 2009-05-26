11871734a26729869487c6827af6bc1ba287a4d4
root <root@bd3a7434-8174-4811-bcf6-7b9fed35a952>
2009-05-26 23:57:09 +0000

    [alee] articles url display and seo name
    git-svn-id: http://repos.luxurylink.com/svn/repos/toolbox/trunk@1583 bd3a7434-8174-4811-bcf6-7b9fed35a952

--- app/controllers/articles_controller.php
+++ app/controllers/articles_controller.php
@@ -24,6 +24,8 @@ class ArticlesController extends AppController {
 
 	function add() {
 		if (!empty($this->data)) {
+			$this->data['Article']['articleSeoName'] = $this->convertToSeoName($this->data['Article']['articleTitle']);
+			$this->data['Article']['articleUrlDisplay'] = $this->getArticleUrlDisplay($this->data['Article']['primaryStypeId']);
 			$this->Article->create();
 			if ($this->Article->save($this->data)) {
 				$this->Session->setFlash(__('The Article has been saved', true));
@@ -42,6 +44,9 @@ class ArticlesController extends AppController {
 			$this->redirect(array('action'=>'index'));
 		}
 		if (!empty($this->data)) {
+			$this->getArticleUrlDisplay(91);
+			$this->data['Article']['articleUrlDisplay'] = $this->getArticleUrlDisplay($this->data['Article']['primaryStypeId']);
+			$this->data['Article']['articleSeoName'] = $this->convertToSeoName($this->data['Article']['articleTitle']);
 			if ($this->Article->save($this->data)) {
 				$this->Session->setFlash(__('The Article has been saved', true));
 				$this->redirect(array('action'=>'index'));
@@ -98,6 +103,52 @@ class ArticlesController extends AppController {
 			$this->Session->setFlash(__('Article deleted', true));
 			$this->redirect(array('action'=>'index'));
 		}
+	}
+	
+	function getArticleUrlDisplay($primaryStyleId) {
+		$this->LandingPage->recursive = -1;
+		$landingPage = $this->LandingPage->read(null, $primaryStyleId);
+		$styleName = $this->convertToSeoName($landingPage['LandingPage']['landingPageName']);
+		switch ($landingPage['LandingPage']['landingPageTypeId']) {
+			case 1:
+				$articleUrlDisplay = "/destinations/$styleName/a/";
+				break;
+			case 2: 
+				$articleUrlDisplay = "/styles-interests/$styleName/a/";
+				break;
+		}
+		return $articleUrlDisplay;
+	}
+
+	function convertToSeoName($str) {
+	    $str = strtolower(html_entity_decode($str, ENT_QUOTES, "ISO-8859-1"));  // convert everything to lower string
+	    $search_accent = explode(",","ç,æ,~\,á,é,í,ó,ú,à,è,ì,ò,ù,ä,ë,ï,ö,ü,ÿ,â,ê,î,ô,û,å,e,i,ø,u,ñ");
+	    $replace_accent = explode(",","c,ae,oe,a,e,i,o,u,a,e,i,o,u,a,e,i,o,u,y,a,e,i,o,u,a,e,i,o,u,n");
+	    $search_accent[] = '&';
+	    $replace_accent[] = ' and ';
+	    $str = str_replace($search_accent, $replace_accent, $str);
+	    $str = preg_replace("/<([^<>]*)>/", ' ', $str);                     // remove html tags
+	    $str_array = preg_split("/[^a-zA-Z0-9]+/", $str);                   // remove non-alphanumeric
+	    $count_a = count($str_array);
+	    if ($count_a) {
+	        if ($str_array[0] == 'the') {
+	            array_shift($str_array);
+	        }
+	        if (isset($str_array[($count_a - 1)]) && (($str_array[($count_a - 1)] == 'the') || !$str_array[($count_a - 1)])) {
+	            array_pop($str_array);
+	        }
+	        for ($i=0; $i<$count_a; $i++) {
+	            if ($str_array[$i]=='s' && strlen($str_array[($i - 1)])>1) {
+	                $str_array[($i - 1)] = $str_array[($i - 1)] . 's';
+	                unset($str_array[$i]);
+	            } elseif ($str_array[$i]=='' || !$str_array[$i]) {
+	                unset($str_array[$i]);
+	            }
+	        }
+	        return (substr(implode('-', $str_array), 0, 499));
+	    }else {
+	        return '';
+	    }
 	}
 
 }
