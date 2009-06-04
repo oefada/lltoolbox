d33d834592e0558d8621237d665fb3c9341c57c6
root <root@bd3a7434-8174-4811-bcf6-7b9fed35a952>
2009-06-04 22:39:47 +0000

    [alee] NEW PROMO ENGINE
    git-svn-id: http://repos.luxurylink.com/svn/repos/toolbox/trunk@1595 bd3a7434-8174-4811-bcf6-7b9fed35a952

--- app/models/payment_detail.php
+++ app/models/payment_detail.php
@@ -1,4 +1,7 @@
 <?php
+App::Import('Model', 'GiftCertBalance'); 
+App::Import('Model', 'CreditTracking'); 
+
 class PaymentDetail extends AppModel {
 
 	var $name = 'PaymentDetail';
@@ -10,5 +13,50 @@ class PaymentDetail extends AppModel {
 						   'PaymentType' => array('foreignKey' => 'paymentTypeId'),
 						   'PaymentProcessor' => array('foreignKey' => 'paymentProcessorId')
 						  );
+
+	function saveGiftCert($ticketId, $gcData, $userId, $auto = 0, $initials = 'NA') {
+		$paymentDetail = array();
+		$paymentDetail['paymentTypeId'] 		= 2;
+		$paymentDetail['paymentAmount'] 		= $gcData['totalAmountOff'];
+		$paymentDetail['ticketId']				= $ticketId;
+		$paymentDetail['userId']				= $userId;
+		$paymentDetail['autoProcessed']			= $auto;
+		$paymentDetail['isSuccessfulCharge']	= 1;
+		$paymentDetail['initials']				= $initials;
+		$paymentDetail['ppResponseDate']		= date('Y-m-d H:i:s');
+
+		$this->create();
+		if ($this->save($paymentDetail)) {
+			$GiftCertBalance = new GiftCertBalance();
+			$data = array();
+			$data['GiftCertBalance']['promoCodeId'] = $gcData['promoCodeId'];
+			$data['GiftCertBalance']['amount']	= -$gcData['totalAmountOff'];
+			$GiftCertBalance->create();
+			$GiftCertBalance->save($data);
+		}
+	}
+
+	function saveCof($ticketId, $cofData, $userId, $auto = 0, $initials = 'NA') {
+		$paymentDetail = array();
+		$paymentDetail['paymentTypeId'] 		= 3;
+		$paymentDetail['paymentAmount'] 		= $cofData['totalAmountOff'];
+		$paymentDetail['ticketId']				= $ticketId;
+		$paymentDetail['userId']				= $userId;
+		$paymentDetail['autoProcessed']			= $auto;
+		$paymentDetail['isSuccessfulCharge']	= 1;
+		$paymentDetail['initials']				= $initials;
+		$paymentDetail['ppResponseDate']		= date('Y-m-d H:i:s');
+		
+		$this->create();
+		if ($this->save($paymentDetail)) {
+			$CreditTracking = new CreditTracking();
+			$data = array();
+			$data['CreditTracking']['userId'] = $cofData['userId'];
+			$data['CreditTracking']['amount']	= -$cofData['totalAmountOff'];
+			$data['CreditTracking']['creditTrackingTypeId'] = $cofData['creditTrackingTypeId'];
+			$CreditTracking->create();
+			$CreditTracking->save($data);
+		}
+	}
 }
 ?>
