c866bfac1affbc5ff420950b85c81802b6611de2
root <root@bd3a7434-8174-4811-bcf6-7b9fed35a952>
2009-06-25 21:16:42 +0000

    [alee] added RAF functions
    git-svn-id: http://repos.luxurylink.com/svn/repos/toolbox/trunk@1673 bd3a7434-8174-4811-bcf6-7b9fed35a952

--- app/models/promo.php
+++ app/models/promo.php
@@ -6,6 +6,8 @@ class Promo extends AppModel {
 	var $primaryKey = 'promoId';
 	var $displayField = 'promoName';
 
+	var $rafPromoId = 60;
+
 	var $hasAndBelongsToMany = array(
 		'PromoCode' =>
 		   array('className'    => 'PromoCode',
@@ -15,5 +17,32 @@ class Promo extends AppModel {
 				 'unique'       => true,
 		   )
 	);
+
+	function checkInsertRaf($promoCodeId) {
+		if (!$promoCodeId) {
+			return false;
+		}
+
+		$sql = "SELECT pco.userId FROM promoCodeOwner pco ";
+		$sql.= "INNER JOIN promoCodeRel pcr ON pco.promoCodeId = pcr.promoCodeId AND pcr.promoId = " . $this->rafPromoId;
+		$sql.= "	WHERE pco.promoCodeId = $promoCodeId";
+		$result = $this->query($sql);
+
+		return (!empty($result)) ? $result[0]['pco']['userId'] : false;
+	}
+
+	function getRafData($promoCodeId) {
+		if (!$promoCodeId) {
+			return false;
+		}
+	
+		$sql = "SELECT User.firstname, User.email, Charity.charityName FROM promoCodeOwner as PromoCodeOwner ";
+		$sql.= "INNER JOIN user as User USING (userId) ";
+		$sql.= "INNER JOIN charty as Charity USING (charityId) ";
+		$sql.= "WHERE PromoCodeOwner.promoCodeId = $promoCodeId";
+		$result = $this->query($sql);
+		
+		return (!empty($result)) ? $result[0] : false;
+	}
 }
 ?>
