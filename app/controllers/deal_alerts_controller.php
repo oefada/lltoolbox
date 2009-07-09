3c518019756dfef1103f94d380ac181323b54614
vgarcia <vgarcia@bd3a7434-8174-4811-bcf6-7b9fed35a952>
2009-07-09 22:47:23 +0000

    git-svn-id: http://repos.luxurylink.com/svn/repos/toolbox/trunk@1744 bd3a7434-8174-4811-bcf6-7b9fed35a952

--- app/controllers/deal_alerts_controller.php
+++ app/controllers/deal_alerts_controller.php
@@ -11,60 +11,9 @@ class DealAlertsController extends AppController {
 		
 		$emailsToSend = array();
 		$this->autoRender = false;
-
-		//TAKE CARE OF THE ORIGINAL SUBSCRIBERS
-		//the last action date is the same for all of these, so it's easy to find the new packages for all clients
-		//one global date to search for for each client, so we can calculate one global list of new packages per client
-		$subs = $this->DealAlert->query("SELECT DealAlert.*, User.userId, User.email, User.firstName, User.lastName FROM dealAlert AS DealAlert
-										INNER JOIN user AS User USING(userId)
-										WHERE lastAction = 'EMAIL'");
-
-		foreach ($subs as $sub) {
-			$clientId = $sub['DealAlert']['clientId'];
-			
-			//if this client doesn't have a list of new packages, create it now
-			if (!isset($newPackages[$clientId])):
-				$tmpNew = $this->DealAlert->query("SELECT DISTINCT(OfferLive.packageId), Client.name, OfferLive.shortBlurb,
-														Client.clientId,
-														oldProductId,
-														seoName,
-														Client.locationDisplay
-														FROM offerLive AS OfferLive 
-														INNER JOIN clientLoaPackageRel cl USING(packageId)
-														INNER JOIN client AS Client ON(Client.clientId = $clientId)
-														LEFT JOIN offerLive AS OfferLivePrev ON (OfferLivePrev.startDate <= ('{$sub['DealAlert']['lastActionDate']}' - INTERVAL 5 MINUTE) AND OfferLivePrev.packageId = OfferLive.packageId)
-														WHERE cl.clientId = $clientId
-														AND OfferLivePrev.offerId IS NULL AND OfferLive.startDate BETWEEN ('{$sub['DealAlert']['lastActionDate']}' - INTERVAL 5 MINUTE) AND '{$sub['DealAlert']['lastActionDate']}'
-														GROUP BY OfferLive.packageId");
-
-				$tmp = array();
-				foreach ($tmpNew as $pkg) {
-					$tmp[] = array('packageId' => $pkg['OfferLive']['packageId'],
-									'clientName' => $pkg['Client']['name'],
-									'clientUrl' => "http://www.luxurylink.com/luxury-hotels/".$pkg['Client']['seoName']."?clid=".$pkg['Client']['clientId'],
-									'shortBlurb' => $pkg['OfferLive']['shortBlurb'],
-									'oldProductId' => $pkg['Client']['oldProductId'],
-									'seoName' => $pkg['Client']['seoName'],
-									'clientId' => $pkg['Client']['clientId'],
-									'locationDisplay' => $pkg['Client']['locationDisplay']);
-				}
-								
-				$newPackages[$clientId] = $tmp;
-			endif;
-			if ($newPackages[$clientId]) {
-				@$emailsToSend[$sub['DealAlert']['userId']] = array('email' => $sub['User']['email'],
-																	'firstName' => $sub['User']['firstName'],
-																	'lastName' => $sub['User']['lastName'],
-																	'userId' => $sub['User']['userId'],
-																	'packages' => array_merge((array)$emailsToSend[$sub['DealAlert']['userId']]['packages'], (array)$newPackages[$clientId]));
-			}
-		}
 		
-		//take care of all new subscribers
-		//the dates can be different for all, so must calculate new packages per subscriber
 		$subs = $this->DealAlert->query("SELECT DealAlert.*, User.email FROM dealAlert AS DealAlert
-										INNER JOIN user AS User USING(userId)
-										WHERE lastAction = 'SUBSCRIBE'");		
+										INNER JOIN user AS User USING(userId)");		
 		foreach ($subs as $sub) {
 			$clientId = $sub['DealAlert']['clientId'];
 			
@@ -76,7 +25,7 @@ class DealAlertsController extends AppController {
 														FROM offerLive AS OfferLive 
 														INNER JOIN clientLoaPackageRel cl USING(packageId)
 														INNER JOIN client AS Client ON(Client.clientId = $clientId)
-													LEFT JOIN offerLive AS OfferLivePrev ON (OfferLivePrev.startDate <= ('{$sub['DealAlert']['lastActionDate']}' - INTERVAL 5 MINUTE) AND OfferLivePrev.packageId = OfferLive.packageId)
+													LEFT JOIN offerLive AS OfferLivePrev ON (OfferLivePrev.startDate <= ('{$sub['DealAlert']['lastActionDate']}' - INTERVAL 5 MINUTE) AND OfferLivePrev.endDate >= ('{$sub['DealAlert']['lastActionDate']}' - INTERVAL 30 DAY) AND OfferLivePrev.endDate >= '{$sub['DealAlert']['subscribeDate']}' AND OfferLivePrev.packageId = OfferLive.packageId)
 													WHERE cl.clientId = $clientId
 													AND OfferLivePrev.offerId IS NULL AND OfferLive.startDate BETWEEN ('{$sub['DealAlert']['lastActionDate']}' - INTERVAL 5 MINUTE) AND '{$sub['DealAlert']['lastActionDate']}'
 													GROUP BY OfferLive.packageId");
