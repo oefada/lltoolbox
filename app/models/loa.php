f54db4135af09c6cf7db5f0a5cec0da43e8a3557
ltum <ltum@bd3a7434-8174-4811-bcf6-7b9fed35a952>
2009-07-28 17:40:30 +0000

    include membershipType
    git-svn-id: http://repos.luxurylink.com/svn/repos/toolbox/trunk@1801 bd3a7434-8174-4811-bcf6-7b9fed35a952

--- app/models/loa.php
+++ app/models/loa.php
@@ -8,7 +8,8 @@ class Loa extends AppModel {
 	var $belongsTo = array('LoaCustomerApprovalStatus' => array('foreignKey' => 'customerApprovalStatusId'),
 						   'Client' => array('foreignKey' => 'clientId'),
 						   'Currency' => array('foreignKey' => 'currencyId'),
-						   'LoaLevel' => array('foreignKey' => 'loaLevelId')
+						   'LoaLevel' => array('foreignKey' => 'loaLevelId'),
+						   'LoaMembershipType' => array('foreignKey' => 'loaMembershipTypeId')
 						  );
 	var $validate = array('startDate' => array('rule' => array('validateEndStartDate'), 'message' => 'Start date must be less than end date'),
 							'endDate' => array('rule' => array('validateEndStartDate'), 'message' => 'Start date must be less than end date')
@@ -35,12 +36,13 @@ class Loa extends AppModel {
 	}
 	
 	function beforeSave($options) {
-	    $orig = $this->find('Loa.loaId = '.$this->data['Loa']['loaId'], array('customerApprovalStatusId'));
-	    
-	    if (@$orig['Loa']['customerApprovalStatusId'] != 2 && $this->data['Loa']['customerApprovalStatusId'] == 2) {
-	        $this->data['Loa']['customerApprovalDate'] = date('Y-m-d H:i:s');
-	    }
-	    
+		if ($this->data['Loa']['loaId']) {
+		    $orig = $this->find('Loa.loaId = '.$this->data['Loa']['loaId'], array('customerApprovalStatusId'));
+		    
+		    if (@$orig['Loa']['customerApprovalStatusId'] != 2 && $this->data['Loa']['customerApprovalStatusId'] == 2) {
+		        $this->data['Loa']['customerApprovalDate'] = date('Y-m-d H:i:s');
+		    }			
+		}
 	    return true;
 	}
 	
