94611fa76ad670de8decc6324e6ba7d7e51797a6
alee <alee@bd3a7434-8174-4811-bcf6-7b9fed35a952>
2009-11-09 22:03:09 +0000

    [alee] for new offer luxury link instead of offer live
    git-svn-id: http://repos.luxurylink.com/svn/repos/toolbox/trunk@2102 bd3a7434-8174-4811-bcf6-7b9fed35a952

---
+++ app/controllers/offer_luxury_links_controller.php
@@ -0,0 +1,36 @@
+<?php
+class OfferLuxuryLinksController extends AppController {
+
+	var $name = 'OfferLuxuryLink';
+
+	function index() {
+		$this->OfferLuxuryLink->recursive = 0;
+		$this->set('offers', $this->paginate());
+	}
+
+	function edit($id = null) {
+		if (!empty($this->data)) {
+			if ($this->OfferLuxuryLink->save($this->data)) {
+				$this->Session->setFlash(__('The Offer has been modified', true), 'default', array(), 'success');
+			} else {
+				$this->Session->setFlash(__('The Offer could not be modified', true), 'default', array(), 'error');
+			}
+		}
+		if (empty($this->data)) {
+			$this->data = $this->OfferLuxuryLink->read(null, $id);
+		}
+	}
+	
+	//Limit access to this controller
+	function isAuthorized() {
+	    $authorized_groups = array('Merchandising', 'Geeks');
+	    
+	    if (in_array('Geeks', $this->user['LdapUser']['groups']) ||
+	        in_array('Merchandising', $this->user['LdapUser']['groups'])) {
+	        return true;
+	    }
+	    
+	    return false;
+	}
+}
+?>
