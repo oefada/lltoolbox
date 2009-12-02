9e766fd09108a4cc06409ed00987d08a7ec2aa31
acarney <acarney@bd3a7434-8174-4811-bcf6-7b9fed35a952>
2009-12-02 02:13:46 +0000

    fix for saving tracking links/enable multisite for tracking
    git-svn-id: http://repos.luxurylink.com/svn/repos/toolbox/trunk@2154 bd3a7434-8174-4811-bcf6-7b9fed35a952

--- app/models/client_tracking.php
+++ app/models/client_tracking.php
@@ -5,5 +5,20 @@ class ClientTracking extends AppModel {
 	var $useTable = 'clientTracking';
 	var $primaryKey = 'clientTrackingId';
 	var $displayField = 'clientTrackingId';
+	
+	var $belongsTo = array('Client' => array('className' => 'Client', 'foreignKey' => 'clientId'));
+	
+	var $actsAs = array('Multisite');
+	
+	function beforeSave() {
+		if (empty($this->data['ClientTracking']['sites'])) {
+				$client = $this->Client->find('first', array('conditions' => array('Client.clientId' => $this->data['ClientTracking']['clientId'])));
+				if (!empty($client)) {
+						$this->data['ClientTracking']['sites'] = $client['Client']['sites'];
+				}
+		}
+		return true;
+	}
+	
 }
 ?>
