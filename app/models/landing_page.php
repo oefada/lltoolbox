c5f7cbfa932142571f777d7f5332a3a66a040eca
acarney <acarney@bd3a7434-8174-4811-bcf6-7b9fed35a952>
2009-12-08 22:04:48 +0000

    make landing page tool site-aware
    git-svn-id: http://repos.luxurylink.com/svn/repos/toolbox/trunk@2182 bd3a7434-8174-4811-bcf6-7b9fed35a952

--- app/models/landing_page.php
+++ app/models/landing_page.php
@@ -8,15 +8,29 @@ class LandingPage extends AppModel {
 	var $order = array('LandingPage.landingPageName');
 	var $belongsTo = array('LandingPageType' => array('foreignKey' => 'landingPageTypeId'));
 
-	var $hasAndBelongsToMany = array(
-						'Menu' => array(
-										'with' => 'menuLandingPageRel',
-										'foreignKey' => 'landingPageId',
-										'associationForeignKey' => 'menuId'
-										)
-								);
+	var $actsAs = array('Containable');
 	
+	function beforeSave() {
+	  $lp = $this->findByLandingPageId($this->id);
+	  if (!empty($lp)) {
+			if ($lp['LandingPage']['siteId'] != $this->data['LandingPage']['siteId']) {
+				  $this->useDbConfig = AppModel::getDbName($lp['LandingPage']['siteId']);
+				  $this->query("DELETE FROM landingPage WHERE landingPageId = {$lp['LandingPage']['landingPageId']}");
+				  $this->useDbConfig = 'default';
+			}
+	  }
+	  return true;
+	}
 	
-	var $actsAs = array('Containable', 'Multisite');
+	function afterSave() {
+	  if(!empty($this->data['LandingPage']['siteId'])) {
+			$lp = $this->findByLandingPageId($this->data['LandingPage']['landingPageId']);
+			$siteId = $this->data['LandingPage']['siteId'];
+			$this->useDbConfig = AppModel::getDbName($siteId);
+			unset($lp['LandingPage']['siteId']);
+			$this->saveAll($lp, array('callbacks' => false));
+	  }
+	}
+	
 }
 ?>
