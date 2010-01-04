aff9e7331316380113f9f5bf600c93949adc2aed
acarney <acarney@bd3a7434-8174-4811-bcf6-7b9fed35a952>
2010-01-04 22:40:10 +0000

    multisite
    git-svn-id: http://repos.luxurylink.com/svn/repos/toolbox/trunk@2236 bd3a7434-8174-4811-bcf6-7b9fed35a952

--- app/app_model.php
+++ app/app_model.php
@@ -39,6 +39,10 @@
  * @subpackage	cake.app
  */
 class AppModel extends Model{
+    
+    var $sites = array(1 => 'luxurylink',
+                       2 => 'family');
+    
 	/**
 	* Constructor just sets some defaults for Luxury Link and calls the parent constructor.
 	*/
@@ -65,11 +69,133 @@ class AppModel extends Model{
 
 		parent::__construct($id, $table, $ds);
 	}
+    
+    function afterFind($results) {
+        if (Model::hasField('sites') && $this->isMultisite()) {
+            foreach($results as &$result) {
+                if (isset($result[$this->name]['sites'])) {
+                    if (in_array('sites', array_keys($result[$this->name])) && !is_array($result[$this->name]['sites'])) {
+                        $result[$this->name]['sites'] = explode(',', $result[$this->name]['sites']);
+                    }
+                }
+            }
+        }
+        return $results;
+    }
+    
+    function beforeSave() {
+        if (Model::hasField('sites') && $this->isMultisite()) {
+            if (!empty($this->data[$this->name]['sites']) && is_array($this->data[$this->name]['sites'])) {
+                $this->data[$this->name]['sites'] = implode(',', $this->data[$this->name]['sites']);
+            }
+        }
+        return true;
+    }
+    
+    function afterSave($created) {
+        if (Model::hasField(array('sites', 'siteId')) && $this->isMultisite()) {
+            if (isset($this->containModels)) {
+                $this->contain($this->containModels);
+            }
+            $modelData = $this->find('first', array('conditions' => array($this->name.'.'.$this->primaryKey => $this->id)));
+            $siteField = (Model::hasField('sites')) ? 'sites' : 'siteId';
+            if (is_numeric($modelData[$this->name][$siteField])) {
+                $modelSites = array($this->sites[$modelData[$this->name][$siteField]]);
+            }
+            elseif (is_array($modelData[$this->name][$siteField])) {
+                $modelSites = $modelData[$this->name][$siteField];
+            }
+            else {
+                return;
+            }
+            unset($modelData[$this->name][$siteField]);
+            foreach ($this->sites as $site) {
+                $this->saveToFrontEndDb($modelData, $site, $modelSites, $created);
+            }
+            $this->useDbConfig = 'default';
+        }
+    }
+    
+   function afterDelete() {
+        if (Model::hasField(array('sites', 'siteId')) && $this->isMultisite()) {
+            if (isset($this->containModels)) {
+                $this->contain($this->containModels);
+            }
+            foreach($this->sites as $site) {
+                $this->useDbConfig = $site;
+                $result = $this->find('first', array('fields' => $this->primaryKey,
+                                      'conditions' => array($this->primaryKey => $this->id)));
+                if (!empty($result)) {
+                    $this->deleteFromFrontEnd($result, $site);
+                }
+            } 
+        }
+   }
+    
+    
+    function saveToFrontEndDb($modelData, $site, $modelSites, $created) {
+        $this->useDbConfig = $site;
+        if (!$created) {
+            $exists = $this->find('first', array('fields' => $this->primaryKey,
+                                  'conditions' => array($this->primaryKey => $modelData[$this->name][$this->primaryKey])));
+            if (!empty($exists)) {
+                if (!in_array($site, $modelSites)) {
+                    $this->deleteFromFrontEnd($modelData, $site);
+                    return;
+                }
+            }
+        }
+        if (in_array($site, $modelSites)) {
+            $fields = $this->getFields($site);
+            $this->create();
+            $this->save($modelData[$this->name], array('callbacks' => false, 'fieldList' => $fields));
+            if (isset($this->containModels)) {
+                $this->saveAssocModels($modelData, $site);
+            }
+        }
+    }
+    
+    function getFields($site) {
+        $query = "SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE table_name = '{$this->useTable}' AND table_schema = '{$site}'";
+        $fields = $this->query($query);
+        $f = array();
+        foreach($fields as $field) {
+            $f[] = $field['COLUMNS']['COLUMN_NAME'];
+        }
+        return $f;
+    }
+    
+    function saveAssocModels($modelData, $site) {
+        foreach($this->containModels as $assocModel) {
+            $this->$assocModel->useDbConfig = $site;
+            $this->$assocModel->create();
+            $this->$assocModel->saveAll($modelData[$assocModel], array('callbacks' => false));
+            $this->$assocModel->useDbConfig = 'default';
+        }
+    }
+    
+    function deleteFromFrontEnd($modelData, $site) {
+        $this->useDbConfig = $site;
+        $this->deleteAll($modelData[$this->name][$this->primaryKey], array('callbacks' => false));
+        if (isset($this->containModels)) {
+            $this->deleteAssocModels($modelData, $site);
+        }
+    }
+    
+    function deleteAssocModels($modelData, $site) {
+        foreach($this->containModels as $assocModel) {
+            $this->$assocModel->useDbConfig = $site;
+            $this->$assocModel->deleteAll($modelData[$this->$assocModel][$this->$assocModel->primaryKey], array('callbacks' => false));
+            $this->$assocModel->useDbConfig = 'default';
+        }
+    }
 	
-	function getDbName($siteId) {
-		$dbMap = array(1 => 'luxurylink',
-					   2 => 'family');
-		return $dbMap[$siteId];
-	}
+    function getDbName($siteId) {
+	    return $this->sites[$siteId];
+    }
+    
+    function isMultisite() {
+        return (isset($this->multisite));
+    }
 }
 ?>
