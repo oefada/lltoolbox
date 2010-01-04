294cbfb37105aabc69aee53d37fe33790000a68a
acarney <acarney@bd3a7434-8174-4811-bcf6-7b9fed35a952>
2010-01-04 22:37:34 +0000

    photo tool
    git-svn-id: http://repos.luxurylink.com/svn/repos/toolbox/trunk@2232 bd3a7434-8174-4811-bcf6-7b9fed35a952

--- app/models/image_client.php
+++ app/models/image_client.php
@@ -1,32 +1,65 @@
 <?php
 class ImageClient extends AppModel {
-			
-			var $name = 'ImageClient';
-			var $useTable = 'imageClient';
-			var $primaryKey = 'clientImageId';
-			
-			var $actsAs = array('Containable');
-			
-			var $belongsTo = array('Client' => array('className' => 'Client', 'foreignKey' => 'clientId'),
-																										'Image' => array('className' => 'Image', 'foreignKey' => 'imageId'),
-																										'ImageType' => array('className' => 'ImageType', 'foreignKey' => 'imageTypeId')
-																										);
-			
-			function saveOrganizedImages($data) {
-						$i = 1;
-						foreach($data as $imageClientId => $image) {
-												$this->create();
-												$this->data['ImageClient']['clientImageId'] = $imageClientId;
-												$this->data['ImageClient']['inactive'] = $image['inactive'];
-												if (isset($image['imageTypeId']) && $image['imageTypeId'] == 1 && !$image['inactive']) {
-															$this->data['ImageClient']['sortOrder'] = $i;
-															$i++;
-												}
-												else {
-															$this->data['ImageClient']['sortOrder'] = null;
-												}												
-												$this->save($this->data);
-						}
+   
+   var $name = 'ImageClient';
+   var $useTable = 'imageClient';
+   var $primaryKey = 'clientImageId';
+   
+   var $actsAs = array('Containable');
+   
+   var $multisite = true;
+   var $containModels = array('Image');
+   
+   var $belongsTo = array('Client' => array('className' => 'Client', 'foreignKey' => 'clientId'),
+						   'Image' => array('className' => 'Image', 'foreignKey' => 'imageId'),
+						   'ImageType' => array('className' => 'ImageType', 'foreignKey' => 'imageTypeId')
+						   );
+    
+   function saveOrganizedImages($data, $clientId, $sitesMap) {
+	  $i = 1;
+	  if (!empty($data['duplicateTo'])) {
+		 $sitesToDuplicateTo = array_keys($data['duplicateTo']);
+	  }
+	  foreach($data['ImageClient'] as $imageClientId => $image) {
+		 $imageClient = $image;
+		 $imageClient['clientImageId'] = $imageClientId;
+		 $imageClient['clientId'] = $clientId;
+		 if(!isset($image['inactive'])) {
+			$imageClient['inactive'] = 1;
+		 }
+		 elseif($image['inactive'] == 'on') {
+			$imageClient['inactive'] = 0;
+		 }
+		 if (isset($image['imageTypeId']) && $image['imageTypeId'] == 1 && !$image['inactive']) {
+		   $imageClient['sortOrder'] = $i;
+		   $i++;
+		 }
+		 else {
+			$imageClient['sortOrder'] = null;
+		 }
+		 $this->create();
+		 $this->data['ImageClient'] = $imageClient;
+		 $this->save($this->data);
+		 if (!empty($sitesToDuplicateTo)) {
+			foreach($sitesToDuplicateTo as $site) {
+			   unset($imageClient['clientImageId']);
+			   $dupeImage = $this->find('first', array('conditions' => array('ImageClient.imageId' => $imageClient['imageId'],
+																			 'ImageClient.clientId' => $clientId,
+																			 'ImageClient.siteId' => array_search($site, $sitesMap)
+																			 ),
+													   'fields' => array('ImageClient.clientImageId')
+													   )
+										);
+			   if (!empty($dupeImage)) {
+				  $imageClient['clientImageId'] = $dupeImage['ImageClient']['clientImageId'];
+			   }
+			   $imageClient['siteId'] = array_search($site, $sitesMap);
+			   $this->create();
+			   $this->data['ImageClient'] = $imageClient;
+			   $this->save($this->data);
 			}
+		 }
+	  }
+   }
 }
 ?>
