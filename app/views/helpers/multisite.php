00a9181ad8c36cd5361dea1dc42b39e9ea782870
acarney <acarney@bd3a7434-8174-4811-bcf6-7b9fed35a952>
2010-01-04 22:34:37 +0000

    photo tool
    git-svn-id: http://repos.luxurylink.com/svn/repos/toolbox/trunk@2230 bd3a7434-8174-4811-bcf6-7b9fed35a952

---
+++ app/views/helpers/multisite.php
@@ -0,0 +1,28 @@
+<?php
+class MultisiteHelper extends AppHelper {
+    var $sites = array(1 => array('dbName' => 'luxurylink',
+                                  'displayName' => 'Luxury Link'),
+                       2 => array('dbName' => 'family',
+                                  'displayName' => 'Family')
+                       );
+    
+    function checkbox($modelName, $label='sites') {
+        $out = '<div class="input select">';
+        $out .= '<label>'.ucwords($label).'</label>';
+        foreach($this->sites as $site) {
+            if (!empty($this->data)) {
+                $checked = (in_array($site['dbName'], $this->data[$modelName]['sites'])) ? ' checked' : '';
+            }
+            else {
+                $checked = '';
+            }
+            $out .= '<div class="checkbox">';
+            $out .= '<input id="'.$modelName.'Sites'.$site['displayName'].'" type="checkbox" value="'.$site['dbName'].'" name="data['.$modelName.'][sites][]" '.$checked.' />';
+            $out .= '<label>'.$site['displayName'].'</label>';
+            $out .= '</div>';
+        }
+        $out .= '</div>';
+        return $out;
+    }    
+}
+?>
