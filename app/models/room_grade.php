29aea7b534c6c316e68c232c7d1ab021e2f248bb
acarney <acarney@bd3a7434-8174-4811-bcf6-7b9fed35a952>
2010-01-08 19:43:01 +0000

    room grades add/edit interface
    git-svn-id: http://repos.luxurylink.com/svn/repos/toolbox/trunk@2253 bd3a7434-8174-4811-bcf6-7b9fed35a952

--- app/models/room_grade.php
+++ app/models/room_grade.php
@@ -7,5 +7,7 @@ class RoomGrade extends AppModel {
     var $actsAs = array('Containable');
     
     var $hasMany = array('ImageRoomGradeRel' => array('className' => 'ImageRoomGradeRel', 'foreignKey' => 'roomGradeId'));
+    
+    var $belongsTo = array('Client' => array('className' => 'Client', 'foreignKey' => 'clientId'));
 }
 ?>
