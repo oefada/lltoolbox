905d7652054cd5a8bab7a7a15156dfffd88ce214
acarney <acarney@bd3a7434-8174-4811-bcf6-7b9fed35a952>
2010-01-08 01:24:51 +0000

    room grade image tool
    git-svn-id: http://repos.luxurylink.com/svn/repos/toolbox/trunk@2247 bd3a7434-8174-4811-bcf6-7b9fed35a952

---
+++ app/models/room_grade.php
@@ -0,0 +1,11 @@
+<?php
+class RoomGrade extends AppModel {
+    var $name = 'RoomGrade';
+    var $useTable = 'roomGrade';
+    var $primaryKey = 'roomGradeId';
+    
+    var $actsAs = array('Containable');
+    
+    var $hasMany = array('ImageRoomGradeRel' => array('className' => 'ImageRoomGradeRel', 'foreignKey' => 'roomGradeId'));
+}
+?>
