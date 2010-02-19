bbd4984178845e12a675eb7cd50bb96104562159
ltum <ltum@bd3a7434-8174-4811-bcf6-7b9fed35a952>
2010-02-19 22:39:13 +0000

    git-svn-id: http://repos.luxurylink.com/svn/repos/toolbox/trunk@2348 bd3a7434-8174-4811-bcf6-7b9fed35a952

---
+++ app/controllers/offer_families_controller.php
@@ -0,0 +1,36 @@
+<?php
+class OfferFamiliesController extends AppController {
+
+	var $name = 'OfferFamily';
+
+	function index() {
+		$this->OfferFamily->recursive = 0;
+		$this->set('offers', $this->paginate());
+	}
+
+	function edit($id = null) {
+		if (!empty($this->data)) {
+			if ($this->OfferFamily->save($this->data)) {
+				$this->Session->setFlash(__('The Offer has been modified', true), 'default', array(), 'success');
+			} else {
+				$this->Session->setFlash(__('The Offer could not be modified', true), 'default', array(), 'error');
+			}
+		}
+		if (empty($this->data)) {
+			$this->data = $this->OfferFamily->read(null, $id);
+		}
+	}
+	
+	//Limit access to this controller
+	function isAuthorized() {
+	    $authorized_groups = array('Merchandising', 'Geeks');
+	    
+	    if (in_array('Geeks', $this->user['LdapUser']['groups']) ||
+	        in_array('Merchandising', $this->user['LdapUser']['groups'])) {
+	        return true;
+	    }
+	    
+	    return false;
+	}
+}
+?>
