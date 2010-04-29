7538342b5a881c8cb4f714f76c36d069f9da7d9a
acarney <acarney@bd3a7434-8174-4811-bcf6-7b9fed35a952>
2010-04-29 00:09:58 +0000

    get parent destination for clients featured in newsletters
    git-svn-id: http://repos.luxurylink.com/svn/repos/toolbox/trunk@2408 bd3a7434-8174-4811-bcf6-7b9fed35a952

--- app/models/client_destination_rel.php
+++ app/models/client_destination_rel.php
@@ -9,6 +9,41 @@ class ClientDestinationRel extends AppModel {
 				     'Destination' => array('foreignKey' => 'destinationId'));
 	
 	var $deleteFirst = true;
+    
+    function getParentDestination($clientId, $destinationId=null) {
+        $this->contain('Destination');
+        $conditions = array('Destination.parentId IS NULL');
+        if ($destinationId) {
+            array_push($conditions, 'Destination.destinationId = '.$destinationId);
+        }
+        else {
+            array_push($conditions, 'ClientDestinationRel.clientId = '.$clientId);
+        }
+        $clientDestination = $this->find('first', array('conditions' => $conditions));
+        if (empty($clientDestination)) {
+            if ($destinationId) {
+                $conditionsAll = array('Destination.destinationId = '.$destinationId);
+            }
+            else {
+                $conditionsAll = array('ClientDestinationRel.clientId' => $clientId);
+            }
+            $clientDestinations = $this->find('all', array('conditions' => $conditionsAll));
+            if ($clientDestinations) {
+                foreach ($clientDestinations as $destination) {
+                    $parentDestination = $this->getParentDestination($clientId, $destination['Destination']['parentId']);
+                    if ($parentDestination) {
+                        return $parentDestination;
+                    }
+                    else {
+                        return false;
+                    }
+                }
+            }   
+        }
+        else {
+            return $clientDestination['Destination']['destinationId'];
+        }
+    }
 	
 }
 ?>
