8670aec8878e573b7ded0c5bd53aba64b5150348
jwoods <jwoods@bd3a7434-8174-4811-bcf6-7b9fed35a952>
2011-10-03 10:45:04 +0000

    ticket 2433 - ClientNewsletterNotifier
    git-svn-id: http://repos.luxurylink.com/svn/repos/toolbox/trunk@3536 bd3a7434-8174-4811-bcf6-7b9fed35a952

--- app/models/client_newsletter_notifier.php
+++ app/models/client_newsletter_notifier.php
@@ -4,15 +4,15 @@ class ClientNewsletterNotifier extends AppModel {
 
 	var $name = 'ClientNewsletterNotifier';
 	var $useTable = false;
-	
+
 	function prepareContactDetails() {
 		$site = $this->data[$this->name]['site'];
 	    $url = $this->data[$this->name]['url'];
-	    
+
 	    // toolbox can't communicate using www. so we need to direct the connection to one of the servers
 	    // in this case we pick web1 randomly
 	    $urlToOpen = str_replace('http://www.', 'http://www6.', $url);
-	    
+
 	    $handle = fopen($urlToOpen, "r");
         $contents = '';
         while (!feof($handle)) {
@@ -21,15 +21,16 @@ class ClientNewsletterNotifier extends AppModel {
         fclose($handle);
 
         $clients = $this->getClientsFromHtml($contents, $site);
-        
-		$this->Client = ClassRegistry::init("Client");
+
+		// $this->Client = ClassRegistry::init("Client");
+		$this->Client = new Client;
 
 	    $cl = $this->Client->find('all', array(
 	                                    'fields' => array('name', 'managerUsername'),
 	                                    'conditions' => array('Client.clientId' => $clients),
 	                                    'contain' => array('ClientContact' => array('conditions' => array('ClientContact.clientContactTypeId' => 2), 'order' => 'ClientContact.primaryContact DESC')),
 	                                    ));
-	    
+
 	    //weed out the non-sponsorship clients
 	    $sponsorshipClients = array();
 		foreach ($cl as $client) {
@@ -40,12 +41,12 @@ class ClientNewsletterNotifier extends AppModel {
 
 		return $sponsorshipClients;
 	}
-	
+
 	function getClientsFromHtml($data, $site) {
 	    //preg_match_all("/luxurylink\.com\/luxury-hotels\/.*\?clid=([0-9]+)/", $data, $clients);
         //$clientIds = array_merge(array_unique($clients[1]), array());
         $match_str = '';
-        
+
         // Right now the $match_str looks similar for both luxurylink and familygetaway (only the URL is different)
         // This may not be the case long term so they are broken out here.
         switch($site) {
@@ -56,14 +57,14 @@ class ClientNewsletterNotifier extends AppModel {
 				$match_str = '/familygetaway\.com\/vacation\/((hotels|inns|tour-packages|luxury-cruises|all-inclusive-resorts|lodges|estates-villas)\/[A-Za-z0-9-]+\/[A-Za-z0-9-\'&\+]+)/';
 				break;
 			default:
-				break; 
+				break;
         }
         preg_match_all($match_str, $data, $clients);
-		
+
 		$clients[1] = array_unique($clients[1]);
         $clientIds = array();
         $this->Client = ClassRegistry::init("Client");
-		
+
         foreach ($clients[1] as $clientUrl) {
             if ($client = $this->Client->getClientBySeoUrl($clientUrl)) {
                 if (!in_array($client['Client']['clientId'], $clientIds)) {
