2052080afd02ee04c374cf1b034daa5afbfa4f4e
sknight <sknight@bd3a7434-8174-4811-bcf6-7b9fed35a952>
2012-05-10 01:16:42 +0000

    Autoformat
    git-svn-id: http://repos.luxurylink.com/svn/repos/toolbox/trunk@4200 bd3a7434-8174-4811-bcf6-7b9fed35a952

--- app/models/image_client.php
+++ app/models/image_client.php
@@ -1,81 +1,92 @@
 <?php
-class ImageClient extends AppModel {
-   
-   var $name = 'ImageClient';
-   var $useTable = 'imageClient';
-   var $primaryKey = 'clientImageId';
-   
-   var $actsAs = array('Containable');
-   
-   var $multisite = true;
-   var $containModels = array('Image');
-   
-   var $belongsTo = array('Client' => array('className' => 'Client', 'foreignKey' => 'clientId'),
-						   'Image' => array('className' => 'Image', 'foreignKey' => 'imageId'),
-						   'ImageType' => array('className' => 'ImageType', 'foreignKey' => 'imageTypeId')
-						   );
-    
-   function saveOrganizedImages($data, $clientId, $sitesMap) {
-	  $i = 1;
-	  if (!empty($data['duplicateTo'])) {
-		 $sitesToDuplicateTo = array_keys($data['duplicateTo']);
-	  }
-	  foreach($data['ImageClient'] as $imageClientId => $image) {
-		 $imageClient = $image;
-		 $imageClient['clientImageId'] = $imageClientId;
-		 $imageClient['clientId'] = $clientId;
-		 if(!isset($image['inactive'])) {
-			$imageClient['inactive'] = 1;
-		 }
-		 elseif($image['inactive'] == 'on') {
-			$imageClient['inactive'] = 0;
-		 }
-		 if (isset($image['imageTypeId']) && $image['imageTypeId'] == 1 && $image['inactive'] == 0) {
-		   $imageClient['sortOrder'] = $i;
-		   $i++;
-		 }
-		 else {
-			$imageClient['sortOrder'] = null;
-		 }
-		 $this->create();
-		 $this->data['ImageClient'] = $imageClient;
-		 if ($this->save($this->data)) {
-			if (!empty($sitesToDuplicateTo)) {
-			   foreach($sitesToDuplicateTo as $site) {
-				  unset($imageClient['clientImageId']);
-				  $dupeImage = $this->find('first', array('conditions' => array('ImageClient.imageId' => $imageClient['imageId'],
-																				'ImageClient.clientId' => $clientId,
-                                                                                'ImageClient.imageTypeId' => $imageClient['imageTypeId'],
-																				'ImageClient.siteId' => array_search($site, $sitesMap)
-																				),
-														  'fields' => array('ImageClient.clientImageId')
-														  )
-										   );
-				  if (!empty($dupeImage)) {
-					 $imageClient['clientImageId'] = $dupeImage['ImageClient']['clientImageId'];
-				  }
-				  else {
-					 $originalImage = $this->find('first', array('conditions' => array('ImageClient.clientImageId' => $imageClientId),
-																 'fields' => array('ImageClient.caption')));
-					 $imageClient['caption'] = $originalImage['ImageClient']['caption'];
-				  }
-				  $imageClient['siteId'] = array_search($site, $sitesMap);
-				  $this->create();
-				  $this->data['ImageClient'] = $imageClient;
-				  if ($this->save($this->data)) {
-					 continue;
-				  }
-				  else {
-					 return false;
-				  }
-			   }
+class ImageClient extends AppModel
+{
+
+	var $name = 'ImageClient';
+	var $useTable = 'imageClient';
+	var $primaryKey = 'clientImageId';
+
+	var $actsAs = array('Containable');
+
+	var $multisite = true;
+	var $containModels = array('Image');
+
+	var $belongsTo = array(
+		'Client' => array(
+			'className' => 'Client',
+			'foreignKey' => 'clientId'
+		),
+		'Image' => array(
+			'className' => 'Image',
+			'foreignKey' => 'imageId'
+		),
+		'ImageType' => array(
+			'className' => 'ImageType',
+			'foreignKey' => 'imageTypeId'
+		)
+	);
+
+	function saveOrganizedImages($data, $clientId, $sitesMap)
+	{
+		$i = 1;
+		if (!empty($data['duplicateTo'])) {
+			$sitesToDuplicateTo = array_keys($data['duplicateTo']);
+		}
+		foreach ($data['ImageClient'] as $imageClientId => $image) {
+			$imageClient = $image;
+			$imageClient['clientImageId'] = $imageClientId;
+			$imageClient['clientId'] = $clientId;
+			if (!isset($image['inactive'])) {
+				$imageClient['inactive'] = 1;
+			} elseif ($image['inactive'] == 'on') {
+				$imageClient['inactive'] = 0;
 			}
-		 }
-		 else {
-			return false;
-		 }
-	  }
-	  return true;
-   }
+			if (isset($image['imageTypeId']) && $image['imageTypeId'] == 1 && $image['inactive'] == 0) {
+				$imageClient['sortOrder'] = $i;
+				$i++;
+			} else {
+				$imageClient['sortOrder'] = null;
+			}
+			$this->create();
+			$this->data['ImageClient'] = $imageClient;
+			if ($this->save($this->data)) {
+				if (!empty($sitesToDuplicateTo)) {
+					foreach ($sitesToDuplicateTo as $site) {
+						unset($imageClient['clientImageId']);
+						$dupeImage = $this->find('first', array(
+							'conditions' => array(
+								'ImageClient.imageId' => $imageClient['imageId'],
+								'ImageClient.clientId' => $clientId,
+								'ImageClient.imageTypeId' => $imageClient['imageTypeId'],
+								'ImageClient.siteId' => array_search($site, $sitesMap)
+							),
+							'fields' => array('ImageClient.clientImageId')
+						));
+						if (!empty($dupeImage)) {
+							$imageClient['clientImageId'] = $dupeImage['ImageClient']['clientImageId'];
+						} else {
+							$originalImage = $this->find('first', array(
+								'conditions' => array('ImageClient.clientImageId' => $imageClientId),
+								'fields' => array('ImageClient.caption')
+							));
+							$imageClient['caption'] = $originalImage['ImageClient']['caption'];
+						}
+						$imageClient['siteId'] = array_search($site, $sitesMap);
+						$this->create();
+						$this->data['ImageClient'] = $imageClient;
+						if ($this->save($this->data)) {
+							continue;
+						} else {
+							return false;
+						}
+					}
+				}
+			} else {
+				return false;
+			}
+		}
+		return true;
+	}
+
 }
 ?>
