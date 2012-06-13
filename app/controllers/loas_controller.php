7326f5f0d72434e5c10e3b657f5ab9b19f073d88
mbyrnes <mbyrnes@bd3a7434-8174-4811-bcf6-7b9fed35a952>
2012-06-13 23:00:18 +0000

    ticket3216
    git-svn-id: http://repos.luxurylink.com/svn/repos/toolbox/trunk@4280 bd3a7434-8174-4811-bcf6-7b9fed35a952

--- app/controllers/loas_controller.php
+++ app/controllers/loas_controller.php
@@ -2,9 +2,173 @@
 class LoasController extends AppController {
 
 	var $name = 'Loas';
-	var $helpers = array('Html', 'Form', 'Ajax', 'Text', 'Layout', 'Number');
-	var $uses = array('Loa', 'LoaItemRatePeriod');
+	var $helpers = array('Html', 'Form', 'Ajax', 'Text', 'Layout', 'Number', 'Paginator');
+	var $uses = array('Loa', 'LoaItemRatePeriod', 'SchedulingMasterTrackRel');
 	var $paginate;
+
+	function maintTracking($id = null) {
+
+		$this->Loa->recursive = 0;
+		$loa = $this->data = $this->Loa->read(null, $id);
+		$tracks = array();
+		$track_details = array();
+
+		$offerLiveFieldArr=array(
+			'offerId', 
+			'packageId', 
+			'offerTypeName', 
+			'offerSubtitle', 
+			'startDate', 
+			'endDate', 
+			'retailValue', 
+			'openingBid'
+		);
+
+		foreach($offerLiveFieldArr as $field){
+			$options['fields'][]='offerLive.'.$field;
+		}
+
+		$dir='';
+		$sort='';
+		if (isset($this->passedArgs['sort'])){
+			$sort=$this->passedArgs['sort'];
+			$dir=$this->passedArgs['direction'];
+		}else{
+			$sort='ticketId';
+			$dir='asc';
+		}
+		if ($sort!='' && in_array($sort,$offerLiveFieldArr)){
+			$options['order'] = array($sort => $dir);
+		}else{
+			$options['order'] = array('offerId' => 'ASC');
+		}
+
+		$tracks_result = $this->Loa->query('SELECT * FROM track WHERE loaId = ' . $loa['Loa']['loaId']);
+
+		foreach ($tracks_result as $track) {
+
+			$trackId=$track['track']['trackId'];
+			$tracks[$trackId] = $track['track'];
+
+			$offers = array();
+			foreach($loa['Loa']['sites'] as $site) {
+
+				$join_table = ($site == 'luxurylink') ? 'offerLuxuryLink' : 'offerFamily';
+
+				$options['conditions'] = array('SchedulingMasterTrackRel.trackId' => $track['track']['trackId']);
+
+				$options['joins'] = array(
+					array(
+						'table'=>'schedulingMaster',
+						'alias'=>'sm',
+						'type'=>'inner',
+						'conditions'=>'sm.schedulingMasterId = SchedulingMasterTrackRel.schedulingMasterId'
+					),
+					array(
+						'table'=>'schedulingInstance',
+						'type'=>'inner',
+						'alias'=>'si',
+						'conditions'=>'si.schedulingMasterId = SchedulingMasterTrackRel.schedulingMasterId'
+					),
+					array(
+						'table'=>'offer',
+						'alias'=>'o',
+						'type'=>'inner',
+						'conditions'=>'o.schedulingInstanceId = si.schedulingInstanceId'
+					),
+					array(
+						'table'=>$join_table,
+						'alias'=>'offerLive',
+						'type'=>'inner',
+						'conditions'=>'offerLive.offerId=o.offerId'
+					)
+				);
+
+	      $offer_result=$this->SchedulingMasterTrackRel->find('all', $options);
+
+				foreach ($offer_result as $offer) {
+					$oid=$offer['offerLive']['offerId'];
+					$offers[$oid] = $offer['offerLive'];	
+				}
+
+			}
+
+			$tracks[$trackId]['offers'] = $offers;
+
+		}
+
+		if (!empty($tracks)) {
+			$q='SELECT trackDetail.*, ticket.offerId, ticket.numNights as numNights ';
+			$q.="FROM trackDetail INNER JOIN ticket USING (ticketId) ";
+			$q.='WHERE trackId IN (' . implode(',', array_keys($tracks)) . ') ';
+			$q.='ORDER BY trackId ASC';
+			$track_details_result = $this->Loa->query($q);
+			foreach ($track_details_result as $track_detail) {
+				$oid=$track_detail['ticket']['offerId'];
+				$track_detail['trackDetail']['numNights']=$track_detail['ticket']['numNights'];
+				$track_details[$oid][] = $track_detail['trackDetail'];
+			}
+
+			//
+			// Restructure array - In order to be able to sort by ticketId, restructure array
+			//
+			foreach($tracks as $trackId=>$arr){
+				$tmpArr=array();
+				foreach($arr['offers'] as $oid=>$row){
+					if (isset($track_details[$oid])){
+						foreach($track_details[$oid] as $i=>$row){
+							$tracks[$trackId]['offers'][$oid]+=$row;
+						}
+					}else{
+						$tmpArr[$oid]=$row;
+						unset($tracks[$trackId]['offers'][$oid]);
+					}
+				}
+				if (count($tmpArr)>0){
+					$tracks[$trackId]['offers']+=$tmpArr;	
+				}
+			}
+
+			$new_arr=array();
+			if ($sort=='ticketId'){
+				//set key to use ticketId
+				foreach($tracks as $trackId=>$arr){
+					$counter=($dir=="asc")?10000000:0;
+					foreach($arr['offers'] as $oid=>$row){
+						$counter++;
+						if (isset($row['ticketId'])){
+							$tracks[$trackId]['offers'][$row['ticketId']]=$row;	
+						}else{
+							$tracks[$trackId]['offers'][$counter]=$row;	
+						}
+						unset($tracks[$trackId]['offers'][$oid]);
+					}
+					$arr=$tracks[$trackId]['offers'];
+					if ($dir=='asc'){
+						ksort($arr);
+					}else{
+						krsort($arr);
+					}
+					unset($tracks[$trackId['offers']]);
+					$tracks[$trackId]['offers']=$arr;
+				}
+			}
+			// 
+			// End restructuring
+			//
+
+			$trackWarning = false;
+		} else {
+			$trackWarning = '<h3 style="font-size:15px;">*** NO TRACK IS SETUP FOR THIS LOA ***</h3><br /><br />';	
+		}
+
+		$this->set('trackWarning', $trackWarning);
+		$this->set('loa', $loa);
+		$this->set('tracks', $tracks);
+		$this->set('track_details', $track_details);
+		$this->set('client', $this->Loa->Client->findByClientId($this->data['Loa']['clientId']));
+
+	}
 	
 	function beforeFilter() {
 		parent::beforeFilter();
@@ -60,8 +224,8 @@ class LoasController extends AppController {
 		$loaLevelIds = $this->Loa->LoaLevel->find('list', array('order' => array('LoaLevel.dropdownSortOrder')));
 		$loaMembershipTypeIds = $this->Loa->LoaMembershipType->find('list');
 		$accountTypeIds = $this->Loa->AccountType->find('list');
-    $this->Loa->LoaPublishingStatusRel->PublishingStatus->recursive = -1;
-    $publishingStatus = $this->Loa->LoaPublishingStatusRel->PublishingStatus->find('list');
+	$this->Loa->LoaPublishingStatusRel->PublishingStatus->recursive = -1;
+	$publishingStatus = $this->Loa->LoaPublishingStatusRel->PublishingStatus->find('list');
 		$this->set('clientName', $client['Client']['name']);
 		$this->set('client', $this->Loa->Client->findByClientId($clientId));
 		$this->set(compact('customerApprovalStatusIds', 'currencyIds', 'loaLevelIds', 'loaMembershipTypeIds', 'publishingStatus', 'accountTypeIds'));
@@ -120,11 +284,11 @@ class LoasController extends AppController {
 			$this->redirect(array('action'=>'index'));
 		}
 		if (!empty($this->data)) {
-      if (empty($this->data['Loa']['sites'])) {
-        $loa = $this->Loa->find($this->data['Loa']['loaId']);
+	  if (empty($this->data['Loa']['sites'])) {
+		$loa = $this->Loa->find($this->data['Loa']['loaId']);
 				$this->data['Client'] = $loa['Client'];
 				$this->Session->setFlash(__('You must select at least one site to save this LOA.', true));
-      } elseif ($this->Loa->save($this->data)) {
+	  } elseif ($this->Loa->save($this->data)) {
 				$this->Session->setFlash(__('The Loa has been saved', true));
 				$this->redirect(array('action'=>'edit', $this->data['Loa']['loaId']));
 			} else {
@@ -164,61 +328,12 @@ class LoasController extends AppController {
 	}
 	
 	function sortLoaItemsByType($a, $b) {
-	    if ($a['LoaItemType']['loaItemTypeName'] ==  $b['LoaItemType']['loaItemTypeName']) {
-            return 0;
-        }
-        return ($a['LoaItemType']['loaItemTypeName'] < $b['LoaItemType']['loaItemTypeName']) ? -1 : 1;
-	}
-	
-	function maintTracking($id = null) {
-		$this->Loa->recursive = 0;
-		$loa = $this->data = $this->Loa->read(null, $id);
-		
-		$tracks = array();
-		$track_details = array();
-
-		$tracks_result = $this->Loa->query('SELECT * FROM track WHERE loaId = ' . $loa['Loa']['loaId']);
-		foreach ($tracks_result as $track) {
-			$tracks[$track['track']['trackId']] = $track['track'];
-			
-			$offers = array();
-			foreach($loa['Loa']['sites'] as $site) {
-				$join_table = ($site == 'luxurylink') ? 'offerLuxuryLink' : 'offerFamily';
-			
-				$offer_result = $this->Loa->query('SELECT offerLive.offerId, offerLive.packageId, offerLive.offerTypeName, offerLive.offerSubtitle, 
-									  offerLive.startDate, offerLive.endDate, offerLive.retailValue, offerLive.openingBid 
-									  FROM schedulingMasterTrackRel smtr 
-									  INNER JOIN schedulingMaster sm USING (schedulingMasterId) 
-									  INNER JOIN schedulingInstance si USING (schedulingMasterId) 
-									  INNER JOIN offer o USING (schedulingInstanceId) 
-									  INNER JOIN ' . $join_table . ' as offerLive USING (offerId) 
-									  WHERE smtr.trackId = ' . $track['track']['trackId'] 						   
-								);
-
-				foreach ($offer_result as $offer) {
-					$offers[$offer['offerLive']['offerId']] = $offer['offerLive'];	
-				}
-			}
-			$tracks[$track['track']['trackId']]['offers'] = $offers;
+		if ($a['LoaItemType']['loaItemTypeName'] ==  $b['LoaItemType']['loaItemTypeName']) {
+			return 0;
 		}
-
-		if (!empty($tracks)) {
-			$track_details_result = $this->Loa->query('SELECT trackDetail.*, ticket.offerId, ticket.numNights FROM trackDetail 
-													INNER JOIN ticket USING (ticketId) 
-													WHERE trackId IN (' . implode(',', array_keys($tracks)) . ') ORDER BY trackId ASC');
-			foreach ($track_details_result as $track_detail) {
-				$track_details[$track_detail['ticket']['offerId']][] = $track_detail['trackDetail'];
-			}
-			$trackWarning = false;
-		} else {
-			$trackWarning = '<h3 style="font-size:15px;">*** NO TRACK IS SETUP FOR THIS LOA ***</h3><br /><br />';	
-		}
-		$this->set('trackWarning', $trackWarning);
-		$this->set('loa', $loa);
-		$this->set('tracks', $tracks);
-		$this->set('track_details', $track_details);
-		$this->set('client', $this->Loa->Client->findByClientId($this->data['Loa']['clientId']));
+		return ($a['LoaItemType']['loaItemTypeName'] < $b['LoaItemType']['loaItemTypeName']) ? -1 : 1;
 	}
+
 
 	function delete($id = null) {
 		if (!$id) {
@@ -254,12 +369,12 @@ class LoasController extends AppController {
 	}
 	
 	function inplace_notes_save() {
-	    $this->autoRender = false;
-	    
-	    $this->Loa->id = str_replace('notes-', "", $this->params['form']['editorId']);
-	    $this->Loa->saveField('notes', $this->params['form']['value']);
-	    $notesSaved = $this->Loa->read('notes');
-	    echo $notesSaved['Loa']['notes'];
+		$this->autoRender = false;
+		
+		$this->Loa->id = str_replace('notes-', "", $this->params['form']['editorId']);
+		$this->Loa->saveField('notes', $this->params['form']['value']);
+		$notesSaved = $this->Loa->read('notes');
+		echo $notesSaved['Loa']['notes'];
 	}
 }
 ?>
