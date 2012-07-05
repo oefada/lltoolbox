beace3b4dd7526f4bec8ced2c8c54f09a62ca7f2
mbyrnes <mbyrnes@bd3a7434-8174-4811-bcf6-7b9fed35a952>
2012-07-05 21:56:18 +0000

    added data by month
    git-svn-id: http://repos.luxurylink.com/svn/repos/toolbox/trunk@4358 bd3a7434-8174-4811-bcf6-7b9fed35a952

--- app/models/unsubscribe_log.php
+++ app/models/unsubscribe_log.php
@@ -5,5 +5,23 @@ class UnsubscribeLog extends AppModel {
 	//var $useDbConfig = 'shared';
 	var $useTable = 'unsubscribeLog';
 
+	public function getUnsubCountByMonth($nlDataArr){
+
+		$r=array();
+		foreach($nlDataArr as $siteId=>$arr){
+			foreach($arr as $nlId=>$row){
+				$q="select siteId, mailingId, count(*) as num, from_unixtime(unsubDate, '%Y-%m') as unsubDate_ym ";
+				$q.="FROM unsubscribeLog ";
+				$q.="WHERE mailingId=? and siteId=? ";
+				$q.="GROUP BY siteId, mailingId, unsubDate_ym ";
+				$q.="ORDER BY siteId, mailingId, unsubDate_ym DESC";
+				$r[]=$this->query($q,array($nlId, $siteId));
+			}
+		}
+ 
+		return $r;
+
+	} 
 }
+
 ?>
