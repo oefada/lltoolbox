0531551891ccc56d75bed82665767a4f952c808b
mbaek <mbaek@bd3a7434-8174-4811-bcf6-7b9fed35a952>
2012-07-05 17:44:21 +0000

    ticket3268 adding new client interview files
    git-svn-id: http://repos.luxurylink.com/svn/repos/toolbox/trunk@4355 bd3a7434-8174-4811-bcf6-7b9fed35a952

---
+++ app/controllers/client_interview_controller.php
@@ -0,0 +1,14 @@
+<?php
+class ClientInterviewsController extends AppController {
+
+	var $name = 'ClientInterviews';
+	var $helpers = array('Time','Html');
+	
+	function index() {
+
+	}
+}
+?>
+
+
+
---
+++ app/models/client_interview.php
@@ -0,0 +1,10 @@
+<?php
+class ClientInterview extends AppModel {
+
+	public $name = 'ClientInterview';
+	public $useTable = 'clientInterview';
+	public $primaryKey = 'clientInterviewId';
+}
+?>
+
+
