980e6c0b229fa32a31d88fb70e516efaf757625e
mbyrnes <mbyrnes@bd3a7434-8174-4811-bcf6-7b9fed35a952>
2012-07-25 18:51:57 +0000

    debug logger
    git-svn-id: http://repos.luxurylink.com/svn/repos/toolbox/trunk@4463 bd3a7434-8174-4811-bcf6-7b9fed35a952

--- app/app_model.php
+++ app/app_model.php
@@ -95,6 +95,7 @@ class AppModel extends LazyModel {
 			require_once('/usr/lib/php/FirePHPCore/fb.php'); 
 			FB::log($str,$num." log:");
 		}else{
+			Configure::write('debug',2);
 			CakeLog::write("debug",$str);
 		}
 
