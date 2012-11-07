f13f3b90efe4f4982f6dd8ff0f3e84dd8e62f2c4
mclifford <mclifford@bd3a7434-8174-4811-bcf6-7b9fed35a952>
2012-11-07 20:18:30 +0000

    Modifications for daily rollups
    git-svn-id: http://repos.luxurylink.com/svn/repos/toolbox/trunk@4793 bd3a7434-8174-4811-bcf6-7b9fed35a952

--- app/vendors/shells/skynet_rollup.php
+++ app/vendors/shells/skynet_rollup.php
@@ -23,6 +23,11 @@ class SkynetRollupShell extends Shell
 			$startDate = $this->params['year'] . '-' . $this->params['month'] . '-01 00:00:00';
 			$endDate = $this->params['year'] . '-' . $this->params['month'] . '-' . cal_days_in_month(CAL_GREGORIAN, $this->params['month'], $this->params['year']) . ' 23:59:59';
 			$siteId = (int) $this->params['siteId'];
+		} else if (isset($this->params['siteId'])) {
+			$yesterday = date('Y-m-d', strtotime("yesterday"));
+			$startDate = $yesterday . ' 00:00:00';
+			$endDate = $yesterday . ' 23:59:59';
+			$siteId = (int) $this->params['siteId'];
 		} else {
 			$this->log('siteId, month, and year are required parameters.');
 			exit;
