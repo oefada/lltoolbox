2d92b76b44ec4a52be0fdca1bef662d35dcf1c60
sknight <sknight@bd3a7434-8174-4811-bcf6-7b9fed35a952>
2012-11-13 00:06:28 +0000

    autoformat
    git-svn-id: http://repos.luxurylink.com/svn/repos/toolbox/trunk@4817 bd3a7434-8174-4811-bcf6-7b9fed35a952

--- app/controllers/room_grades_controller.php
+++ app/controllers/room_grades_controller.php
@@ -1,28 +1,32 @@
 <?php
-class RoomGradesController extends AppController {
 
-   var $name = 'RoomGrades';
-   
-   function beforeFilter() {
-	  parent::beforeFilter();
-	  $this->set('currentTab', 'property');
-	  if (isset($this->params['clientId']) && empty($clientId)) {
-		 $this->RoomGrade->clientId = $this->params['clientId'];
-	  } else {
-		 $this->params['clientId'] = $this->RoomGrade->clientId;
-	  }
-	  $this->RoomGrade->client = $this->RoomGrade->Client->findByClientId($this->RoomGrade->clientId);
-	  $this->set('clientId', $this->RoomGrade->clientId);
-	  $this->set('client', $this->RoomGrade->client);
-   }
-   
-   function index() {
-        if (!empty($this->data)) {
-            $this->data['RoomGrade']['clientId'] = $this->RoomGrade->clientId;
-            if (!isset($this->data['RoomGrade']['roomGradeId'])) {
-                $this->data['RoomGrade']['roomGradeId'] = null;
-            }
-			
+class RoomGradesController extends AppController
+{
+
+	var $name = 'RoomGrades';
+
+	function beforeFilter()
+	{
+		parent::beforeFilter();
+		$this->set('currentTab', 'property');
+		if (isset($this->params['clientId']) && empty($clientId)) {
+			$this->RoomGrade->clientId = $this->params['clientId'];
+		} else {
+			$this->params['clientId'] = $this->RoomGrade->clientId;
+		}
+		$this->RoomGrade->client = $this->RoomGrade->Client->findByClientId($this->RoomGrade->clientId);
+		$this->set('clientId', $this->RoomGrade->clientId);
+		$this->set('client', $this->RoomGrade->client);
+	}
+
+	function index()
+	{
+		if (!empty($this->data)) {
+			$this->data['RoomGrade']['clientId'] = $this->RoomGrade->clientId;
+			if (!isset($this->data['RoomGrade']['roomGradeId'])) {
+				$this->data['RoomGrade']['roomGradeId'] = null;
+			}
+
 			// check and make sure link has http://
 			if (isset($this->data['RoomGrade']['roomLink']) && !empty($this->data['RoomGrade']['roomLink'])) {
 				$link = $this->data['RoomGrade']['roomLink'];
@@ -31,24 +35,24 @@ class RoomGradesController extends AppController {
 					$this->data['RoomGrade']['roomLink'] = $link;
 				}
 			}
-			
-			
-            if ($this->RoomGrade->save($this->data)) {
-                $this->Session->setFlash('Room Grade has been saved.');
-            }
-            else {
-                $this->Session->setFlash('Room Grade could not be saved.');
-            }
-        }
-        $this->RoomGrade->recursive = 2;
-        $roomGrades = $this->RoomGrade->find('all', array('conditions' => array('RoomGrade.clientId' => $this->RoomGrade->clientId),
-                                                           'order' => array('RoomGrade.roomGradeName'))
-                                            );
-        $this->set('roomGrades', $roomGrades);
-   }
-   
-   function edit($clientId, $roomGradeId) {
-        if (!empty($this->data)) {
+
+			if ($this->RoomGrade->save($this->data)) {
+				$this->Session->setFlash('Room Grade has been saved.');
+			} else {
+				$this->Session->setFlash('Room Grade could not be saved.');
+			}
+		}
+		$this->RoomGrade->recursive = 2;
+		$roomGrades = $this->RoomGrade->find('all', array(
+			'conditions' => array('RoomGrade.clientId' => $this->RoomGrade->clientId),
+			'order' => array('RoomGrade.roomGradeName')
+		));
+		$this->set('roomGrades', $roomGrades);
+	}
+
+	function edit($clientId, $roomGradeId)
+	{
+		if (!empty($this->data)) {
 			print_r($this->data);
 			// check and make sure link has http://
 			if (isset($this->data['RoomGrade']['roomLink']) && !empty($this->data['RoomGrade']['roomLink'])) {
@@ -58,28 +62,26 @@ class RoomGradesController extends AppController {
 					$this->data['RoomGrade']['roomLink'] = $link;
 				}
 			}
-			
-            if ($this->RoomGrade->save($this->data)) {
-                $this->Session->setFlash('Room Grade has been saved.');
-            }
-            else {
-                $this->Session->setFlash('Room Grade could not be saved.');
-            }
-        }
-        $this->recursive = -1;
-        $roomGrade = $this->RoomGrade->findByRoomGradeId($roomGradeId);
-        $this->set('roomGrade', $roomGrade);
-   }
-   
-   function delete($clientId, $roomGradeId) {
-        if ($this->RoomGrade->delete($roomGradeId)) {
-            $this->Session->setFlash('Room Grade has been deleted');
-        }
-        else {
-            $this->Session->setFlash('Room Grade could not be deleted');
-        }
-        $this->redirect($this->referer());
-   }
-   
+
+			if ($this->RoomGrade->save($this->data)) {
+				$this->Session->setFlash('Room Grade has been saved.');
+			} else {
+				$this->Session->setFlash('Room Grade could not be saved.');
+			}
+		}
+		$this->recursive = -1;
+		$roomGrade = $this->RoomGrade->findByRoomGradeId($roomGradeId);
+		$this->set('roomGrade', $roomGrade);
+	}
+
+	function delete($clientId, $roomGradeId)
+	{
+		if ($this->RoomGrade->delete($roomGradeId)) {
+			$this->Session->setFlash('Room Grade has been deleted');
+		} else {
+			$this->Session->setFlash('Room Grade could not be deleted');
+		}
+		$this->redirect($this->referer());
+	}
+
 }
-?>
