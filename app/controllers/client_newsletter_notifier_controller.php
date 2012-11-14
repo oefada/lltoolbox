b4da27ab92669619588118ee78a65d1bbf63e24c
mbyrnes <mbyrnes@bd3a7434-8174-4811-bcf6-7b9fed35a952>
2012-11-14 22:12:25 +0000

    ticket3705
    git-svn-id: http://repos.luxurylink.com/svn/repos/toolbox/trunk@4827 bd3a7434-8174-4811-bcf6-7b9fed35a952

--- app/controllers/client_newsletter_notifier_controller.php
+++ app/controllers/client_newsletter_notifier_controller.php
@@ -7,47 +7,48 @@ class ClientNewsletterNotifierController extends AppController {
 	var $uses = array('ClientNewsletterNotifier', 'Reporting');
 	
 	function beforeFilter() {
-	    parent::beforeFilter();
-	    $this->set('currentTab', 'reports');
+		parent::beforeFilter();
+		$this->set('currentTab', 'reports');
 	}
 
 	function index() {
-	    if (!empty($this->data)) {
-	        $this->ClientNewsletterNotifier->data = $this->data;
-            $clients = $this->ClientNewsletterNotifier->prepareContactDetails();
-                        
-            if (@$this->data['ClientNewsletterNotifier']['approve']) {
-                $this->_send($clients, $this->data);
-                $this->set('emailSent', true);
 
-                // 11/16/11 jwoods - added email logging
-                $clientlist = 'clients were notified on ' . date('m/d/Y g:i:s a') . ":\n\n";
-                $clientlist .= 'user: ' . $this->user['LdapUser']['cn'] . "\n";
-                $clientlist .= 'site: ' . $this->data['ClientNewsletterNotifier']['site'] . "\n";
-                $clientlist .= 'themeName: ' . $this->data['ClientNewsletterNotifier']['themeName'] . "\n";
-                $clientlist .= 'url: ' . $this->data['ClientNewsletterNotifier']['url'] . "\n";
-                $clientlist .= 'subscribers: ' . $this->data['ClientNewsletterNotifier']['subscribers'] . "\n\n";
-                foreach ($clients as $c) {
-                	$clientlist .= $c['Client']['name'] . "\n";
-                }
-                $titleFrom = ($_SERVER['ENV'] == 'development' || $_SERVER['ENV'] == 'staging') ? 'Newsletter Notification Dev/Stage' : 'Newsletter Notification';
-                $header = "From: " . $titleFrom . " <devmail@luxurylink.com>\r\n";
-                @mail('NewsletterNotifications@luxurylink.com', 'client newsletter notifier :: ' . date('m/d/Y g:i:s a') , $clientlist, $header);
-            }
-            
-            $this->set('clients', $clients);
-        }
+		if (!empty($this->data)) {
+			$this->ClientNewsletterNotifier->data = $this->data;
+			$clients = $this->ClientNewsletterNotifier->prepareContactDetails();
+	
+			if (@$this->data['ClientNewsletterNotifier']['approve']) {
+				$this->_send($clients, $this->data);
+				$this->set('emailSent', true);
+
+				// 11/16/11 jwoods - added email logging
+				$clientlist = 'clients were notified on ' . date('m/d/Y g:i:s a') . ":\n\n";
+				$clientlist .= 'user: ' . $this->user['LdapUser']['cn'] . "\n";
+				$clientlist .= 'site: ' . $this->data['ClientNewsletterNotifier']['site'] . "\n";
+				$clientlist .= 'themeName: ' . $this->data['ClientNewsletterNotifier']['themeName'] . "\n";
+				$clientlist .= 'url: ' . $this->data['ClientNewsletterNotifier']['url'] . "\n";
+				$clientlist .= 'subscribers: ' . $this->data['ClientNewsletterNotifier']['subscribers'] . "\n\n";
+				foreach ($clients as $c) {
+					$clientlist .= $c['Client']['name'] . "\n";
+				}
+				$titleFrom = ($_SERVER['ENV'] == 'development' || $_SERVER['ENV'] == 'staging') ? 'Newsletter Notification Dev/Stage' : 'Newsletter Notification';
+				$header = "From: " . $titleFrom . " <devmail@luxurylink.com>\r\n";
+				@mail('NewsletterNotifications@luxurylink.com', 'client newsletter notifier :: ' . date('m/d/Y g:i:s a') , $clientlist, $header);
+			}
+			
+			$this->set('clients', $clients);
+		}
 	}
 	
-	function _send($clients, $data) {//TODO: turn off debug	    
-	    foreach ($clients as $client) {
+	function _send($clients, $data) {//TODO: turn off debug		
+		foreach ($clients as $client) {
 			$this->Email->reset();
 
-            if (!empty($client['Client']['managerUsername']) && empty($_SERVER['ENV'])) {
-               $this->Email->cc[] = $client['Client']['managerUsername'].' <'.$client['Client']['managerUsername'].'@luxurylink.com>';
-            }
-                
-            switch($data['ClientNewsletterNotifier']['site']) {
+			if (!empty($client['Client']['managerUsername']) && empty($_SERVER['ENV'])) {
+			   $this->Email->cc[] = $client['Client']['managerUsername'].' <'.$client['Client']['managerUsername'].'@luxurylink.com>';
+			}
+				
+			switch($data['ClientNewsletterNotifier']['site']) {
 				case 'luxurylink':
 					$this->Email->from = 'Client Marketing <clientmarketing@luxurylink.com>';
 					$this->Email->subject = "You are featured in this week's Luxury Link ";
@@ -65,42 +66,42 @@ class ClientNewsletterNotifierController extends AppController {
 					break;
 				default:
 					break;
-            }
-            
-            $this->Email->sendAs = 'both';
-                
-            $this->set('client', $client['Client']);
-            $this->set('clientContact', $client['ClientContact'][0]);
-            $this->set('theme', $data['ClientNewsletterNotifier']['themeName']);
-            $this->set('url', $data['ClientNewsletterNotifier']['url']);
-            $this->set('subscribers', $data['ClientNewsletterNotifier']['subscribers']);
-	        
-	        $mainContact = array_shift($client['ClientContact']);                       //first contact is primary, relies on order by clause in model
-            if ($_SERVER['ENV'] == 'development' || $_SERVER['ENV'] == 'staging') {
-                $this->Email->to = 'livedevmail@luxurylink.com';
-            }
-            else {
-                $this->Email->to = $mainContact['name'].' <'.$mainContact['emailAddress'].'>';
-            }
+			}
 			
-	        // iterate through all remaining contacts, array_shift takes care of the first one for us
-	        if (empty($_SERVER['ENV'])) {
-		        foreach ($client['ClientContact'] as $contact) { //$contact['emailAddress']
+			$this->Email->sendAs = 'both';
+				
+			$this->set('client', $client['Client']);
+			$this->set('clientContact', $client['ClientContact'][0]);
+			$this->set('theme', $data['ClientNewsletterNotifier']['themeName']);
+			$this->set('url', $data['ClientNewsletterNotifier']['url']);
+			$this->set('subscribers', $data['ClientNewsletterNotifier']['subscribers']);
+			
+			$mainContact = array_shift($client['ClientContact']);					   //first contact is primary, relies on order by clause in model
+			if ($_SERVER['ENV'] == 'development' || $_SERVER['ENV'] == 'staging') {
+				$this->Email->to = 'livedevmail@luxurylink.com';
+			}
+			else {
+				$this->Email->to = $mainContact['name'].' <'.$mainContact['emailAddress'].'>';
+			}
+			
+			// iterate through all remaining contacts, array_shift takes care of the first one for us
+			if (empty($_SERVER['ENV'])) {
+				foreach ($client['ClientContact'] as $contact) { //$contact['emailAddress']
 					$this->Email->cc[] = $contact['name'].' <'.$contact['emailAddress'].'>';
 				}
 				$mgr = $client['Client']['managerUsername'] . '@luxurylink.com';
 				$this->Email->cc[] = $mgr;
-	        }
+			}
 			// 12/8/11 jwoods - Michael asked to be copied on all of these
 			$this->Email->bcc[] = 'mchoe@luxurylink.com';
-	        $this->Email->send();
-	        
-	        // add records to carDataFeedEmail and carDataFeedEmailFg 
-	        $carTable = ($data['ClientNewsletterNotifier']['site'] == 'luxurylink') ? 'reporting.carDataFeedEmail' : 'reporting.carDataFeedEmailFg';
+			$this->Email->send();
+			
+			// add records to carDataFeedEmail and carDataFeedEmailFg 
+			$carTable = ($data['ClientNewsletterNotifier']['site'] == 'luxurylink') ? 'reporting.carDataFeedEmail' : 'reporting.carDataFeedEmailFg';
 			$sql = 'INSERT INTO ' . $carTable . ' (clientid, year2, month2, insertDateTime, newsletterDate, url, subscribers) VALUES (?, ?, ?, NOW(), ?, ?, ?)';
 			$params = array($client['Client']['clientId'], date('Y'), date('n'), '0000-00-00', $data['ClientNewsletterNotifier']['url'], str_replace(',', '', $data['ClientNewsletterNotifier']['subscribers']));
 			$results = $this->Reporting->query($sql, $params);
-	    }
+		}
 	}
 }
 ?>
