bfe7d4340c0e6f801ccfe773b6b965ea611da1e3
Michael Clifford <mclifford@luxurylink.com>
2012-11-15 16:12:18 -0800

    TICKET3504 Added tokenizer vendor file

---
+++ app/vendors/tokenizer.php
@@ -0,0 +1,5 @@
+<?php
+App::import("Vendor","TokenizerInterface",array('file' => "appshared/interfaces/TokenizerInterface.php"));
+App::import("Vendor","TokenizerHelper",array('file' => "appshared/helpers/TokenizerHelper.php"));
+App::import("Vendor","TokenExHelper",array('file' => "appshared/helpers/TokenExHelper.php"));
+App::import("Vendor","TokenizerFactoryHelper",array('file' => "appshared/helpers/TokenizerFactoryHelper.php"));
