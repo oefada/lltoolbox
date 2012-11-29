f69e80a835853647f31b7bdef98c71a1983da094
sknight <sknight@bd3a7434-8174-4811-bcf6-7b9fed35a952>
2012-11-29 22:01:31 +0000

    Ticket3481
    git-svn-id: http://repos.luxurylink.com/svn/repos/toolbox/trunk@4888 bd3a7434-8174-4811-bcf6-7b9fed35a952

--- app/controllers/blocks_controller.php
+++ app/controllers/blocks_controller.php
@@ -1,11 +1,32 @@
 <?php
 
+$modules = array(
+	'Markup',
+	'Block',
+	'Div',
+);
+
+foreach ($modules as $module) {
+	App::import('Vendor', $module . 'Module', array('file' => 'appshared' . DS . 'modules' . DS . $module . 'Module' . DS . $module . 'Module' . '.php'));
+}
+
 class BlocksController extends AppController
 {
 	var $name = 'Blocks';
 	var $helpers = array('Html');
 	var $uses = array();
 	function index()
+	{
+	}
+
+}
+
+/*
+ * Create this instead of loading the real 'Module'
+ */
+class Module
+{
+	function __construct()
 	{
 	}
 
