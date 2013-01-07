f0c3b737f4e5b6b082b9fc8ed04bb03ab16813eb
jwoods <jwoods@bd3a7434-8174-4811-bcf6-7b9fed35a952>
2013-01-07 19:14:13 +0000

    toolbox newsletter integration
    git-svn-id: http://repos.luxurylink.com/svn/repos/toolbox/trunk@4994 bd3a7434-8174-4811-bcf6-7b9fed35a952

---
+++ app/vendors/mailvendor.php
@@ -0,0 +1,5 @@
+<?php
+App::import("Vendor","MailVendorInterface",array('file' => "appshared/interfaces/MailVendorInterface.php"));
+App::import("Vendor","MailVendorHelper",array('file' => "appshared/helpers/MailVendorHelper.php"));
+App::import("Vendor","MailVendorBlueHornetHelper",array('file' => "appshared/helpers/MailVendorBlueHornetHelper.php"));
+App::import("Vendor","MailVendorFactoryHelper",array('file' => "appshared/helpers/MailVendorFactoryHelper.php"));
