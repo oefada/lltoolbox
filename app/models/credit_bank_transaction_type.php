8dcbd366efbd2e9b9b23a2dbcdaffb9daf2b68af
mbaek <mbaek@bd3a7434-8174-4811-bcf6-7b9fed35a952>
2013-01-09 01:12:14 +0000

    ticketEventRegistry added code to save creditBankItem amount changes to the creditTracking table.
    git-svn-id: http://repos.luxurylink.com/svn/repos/toolbox/trunk@4997 bd3a7434-8174-4811-bcf6-7b9fed35a952

---
+++ app/models/credit_bank_transaction_type.php
@@ -0,0 +1,8 @@
+<?php
+class CreditBankTransactionType extends AppModel {
+
+	public $name = 'CreditBankTransactionType';
+	public $useTable = 'creditBankTransactionType';
+	public $primaryKey = 'creditBankTransactionTypeId';
+	
+}
