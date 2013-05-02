b253e3cb1357d1a0ff0930e7966dea14fe25e98c
oefada <oefada@bd3a7434-8174-4811-bcf6-7b9fed35a952>
2013-05-02 00:18:19 +0000

    Ticket4085: Made Social Plugin Input Validation mandatory once each plugin enabled
    git-svn-id: http://repos.luxurylink.com/svn/repos/toolbox/trunk@5290 bd3a7434-8174-4811-bcf6-7b9fed35a952

--- app/controllers/components/http_request.php
+++ app/controllers/components/http_request.php
@@ -68,9 +68,7 @@ class HttpRequestComponent extends Object
             return false;
         }
         if (preg_match(
-            '/^(http|https):\/\/[a-z0-9_]+([\-\.]{1}[a-z_0-9]+)*\.[_a-z]{2,5}' . '((:[0-9]{1,5})?\/.*)?$/i',
-            $url
-        )
+            '/^(http|https):\/\/[a-z0-9_]+([\-\.]{1}[a-z_0-9]+)*\.[_a-z]{2,5}' . '((:[0-9]{1,5})?\/.*)?$/i',$url)
         ) {
             return true;
         } else {
