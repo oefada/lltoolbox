23952499f2fef6617ac5ba2d08c48d85cf501570
mclifford <mclifford@bd3a7434-8174-4811-bcf6-7b9fed35a952>
2013-05-06 21:45:18 +0000

    Formatting
    git-svn-id: http://repos.luxurylink.com/svn/repos/toolbox/trunk@5298 bd3a7434-8174-4811-bcf6-7b9fed35a952

--- app/models/client_phone_lead.php
+++ app/models/client_phone_lead.php
@@ -1,137 +1,156 @@
 <?php
 class ClientPhoneLead extends AppModel
 {
-	/**
-	 * Database table this model will use
-	 * 
-	 * @access	public
-	 * @param	string
-	 */
-	public $useTable = 'client_phone_leads';
+    /**
+     * Database table this model will use
+     *
+     * @access public
+     * @param string
+     */
+    public $useTable = 'client_phone_leads';
 
-	/**
-	 * Array of CSV File Headers mapped to a db table column
-	 * 
-	 * @access	public
-	 * @param	string
-	 */
-	public $tableColums = array(
-		'clientid' => 'client_id',
-		'site' => 'site',
-		'Datetime' => 'date',
-		'Duration' => 'duration',
-		'Tracking Number' => 'published_number',
-		'Target Number' => 'destination_number',
-		"Caller Number" => 'caller_number',
-		"Caller First Name" => 'caller_first_name',
-		"Last Name" => 'caller_last_name',
-		"City" => "city",
-		"State" => "state",
-		"Zip" => "zip",
-		'country' => 'country',
-		"caller location" => "caller_location"
-	);
+    /**
+     * Array of CSV File Headers mapped to a db table column
+     *
+     * @access public
+     * @param string
+     */
+    public $tableColums = array(
+        'clientid' => 'client_id',
+        'site' => 'site',
+        'Datetime' => 'date',
+        'Duration' => 'duration',
+        'Tracking Number' => 'published_number',
+        'Target Number' => 'destination_number',
+        "Caller Number" => 'caller_number',
+        "Caller First Name" => 'caller_first_name',
+        "Last Name" => 'caller_last_name',
+        "City" => "city",
+        "State" => "state",
+        "Zip" => "zip",
+        'country' => 'country',
+        "caller location" => "caller_location"
+    );
 
-	/**
-	 * Build an array to later be saved from CSV data
-	 * 
-	 * @access	public
-	 * @param	string csv data
-	 * @return	array
-	 */
-	public function buildArrayFromCSVData($data)
-	{
-		$columns = array_shift($data);
-		$client_phone_lead_record = array();
-		foreach($data as $row_number => $row) {
-			foreach($this->tableColums as $key => $value) {
-				$client_phone_lead_record[$row_number][$value] = $row[array_search($key, $columns)];
-			}
-			if (!$client_phone_lead_record[$row_number]['client_id']) {
-				unset($client_phone_lead_record[$row_number]);
-				continue;
-			}
+    /**
+     * Build an array to later be saved from CSV data
+     *
+     * @access public
+     * @param string csv data
+     * @return array
+     */
+    public function buildArrayFromCSVData($data)
+    {
+        $columns = array_shift($data);
+        $client_phone_lead_record = array();
+        foreach ($data as $row_number => $row) {
+            foreach ($this->tableColums as $key => $value) {
+                $client_phone_lead_record[$row_number][$value] = $row[array_search($key, $columns)];
+            }
+            if (!$client_phone_lead_record[$row_number]['client_id']) {
+                unset($client_phone_lead_record[$row_number]);
+                continue;
+            }
 
-			$client_phone_lead_record[$row_number]['site_id'] = self::getSiteID($client_phone_lead_record[$row_number]['site']);
-			$client_phone_lead_record[$row_number]['date'] = self::formatDate($client_phone_lead_record[$row_number]['date']);
-			$client_phone_lead_record[$row_number]['caller_name'] = trim(strtolower($client_phone_lead_record[$row_number]['caller_first_name'] . ' ' . $client_phone_lead_record[$row_number]['caller_last_name']));
-			$client_phone_lead_record[$row_number]['published_number'] = self::formatPhoneNumber($client_phone_lead_record[$row_number]['published_number']);
-			$client_phone_lead_record[$row_number]['destination_number'] = self::formatPhoneNumber($client_phone_lead_record[$row_number]['destination_number']);
-			$client_phone_lead_record[$row_number]['caller_number'] = self::formatPhoneNumber($client_phone_lead_record[$row_number]['caller_number']);
-		}
+            $client_phone_lead_record[$row_number]['site_id'] = self::getSiteID(
+                $client_phone_lead_record[$row_number]['site']
+            );
+            $client_phone_lead_record[$row_number]['date'] = self::formatDate(
+                $client_phone_lead_record[$row_number]['date']
+            );
+            $client_phone_lead_record[$row_number]['caller_name'] = trim(
+                strtolower(
+                    $client_phone_lead_record[$row_number]['caller_first_name'] . ' ' . $client_phone_lead_record[$row_number]['caller_last_name']
+                )
+            );
+            $client_phone_lead_record[$row_number]['published_number'] = self::formatPhoneNumber(
+                $client_phone_lead_record[$row_number]['published_number']
+            );
+            $client_phone_lead_record[$row_number]['destination_number'] = self::formatPhoneNumber(
+                $client_phone_lead_record[$row_number]['destination_number']
+            );
+            $client_phone_lead_record[$row_number]['caller_number'] = self::formatPhoneNumber(
+                $client_phone_lead_record[$row_number]['caller_number']
+            );
+        }
 
-		return $client_phone_lead_record;
-	}
-	
-	/**
-	 * Check that the upload data is valid
-	 * 
-	 * @access	public
-	 * @param	array upload_data
-	 * @return	boolean
-	 */
-	public function uploadIsValid($upload_data)
-	{
-		// Assume data is valid
-		$data_is_valid = true;
-		
-		// Check for data that makes the data invalid
-		if ($upload_data['type'] != 'text/csv') {
-			$data_is_invalid = false;
-		} else if ($upload_data['error'] != 0) {
-			$data_is_invalid = false;
-		} else if (!($upload_data['size'] > 0)) {
-			$data_is_invalid = false;
-		}
+        return $client_phone_lead_record;
+    }
 
-		return $data_is_valid;
-	}
+    /**
+     * Check that the upload data is valid
+     *
+     * @access public
+     * @param array upload_data
+     * @return boolean
+     */
+    public function uploadIsValid($upload_data)
+    {
+        // Assume data is valid
+        $data_is_valid = true;
 
-	/**
-	 * Get a site id from a string
-	 *
-	 * @param	string site_name
-	 * @return	int
-	 */
-	private static function getSiteId($site_name)
-	{
-		// default to luxurylink, siteId=1
-		$site_id = 1;
+        // Check for data that makes the data invalid
+        if ($upload_data['type'] != 'text/csv') {
+            $data_is_invalid = false;
+        } else {
+            if ($upload_data['error'] != 0) {
+                $data_is_invalid = false;
+            } else {
+                if (!($upload_data['size'] > 0)) {
+                    $data_is_invalid = false;
+                }
+            }
+        }
 
-		switch($site_name) {
-			case 'luxurylink':
-				$site_id = 1;
-				break;
-			case 'family':
-				$site_id = 2;
-				break;
-			default:
-				$site_id = 1;
-		}
+        return $data_is_valid;
+    }
 
-		return $site_id;
-	}
+    /**
+     * Get a site id from a string
+     *
+     * @param string site_name
+     * @return int
+     */
+    private static function getSiteId($site_name)
+    {
+        // default to luxurylink, siteId=1
+        $site_id = 1;
 
-	/**
-	 * Format a date
-	 *
-	 * @param	string date
-	 * @return	string
-	 */
-	private static function formatDate($date)
-	{
-		return date('Y-m-d H:i:s', strtotime($date));
-	}
+        switch ($site_name) {
+            case 'luxurylink':
+                $site_id = 1;
+                break;
+            case 'family':
+                $site_id = 2;
+                break;
+            default:
+                $site_id = 1;
+        }
 
-	/**
-	 * Remove non-numeric characters from a phone number
-	 * 
-	 * @param	string phone_number
-	 * @return	string
-	 */
-	private static function formatPhoneNumber($phone_number)
-	{
-		return ereg_replace("[^0-9]", '', $phone_number);
-	}
+        return $site_id;
+    }
+
+    /**
+     * Format a date
+     *
+     * @param string date
+     * @return string
+     */
+    private static function formatDate($date)
+    {
+        return date('Y-m-d H:i:s', strtotime($date));
+    }
+
+    /**
+     * Remove non-numeric characters from a phone number
+     *
+     * @param string phone_number
+     * @return string
+     */
+    private static function formatPhoneNumber($phone_number)
+    {
+        return ereg_replace("[^0-9]", '', $phone_number);
+    }
 }
+
 ?>
