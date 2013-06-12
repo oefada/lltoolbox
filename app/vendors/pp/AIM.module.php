ad693091d722203ca9f4e097c3b0f2d9d4fbfdff
mclifford <mclifford@bd3a7434-8174-4811-bcf6-7b9fed35a952>
2013-06-12 21:27:35 +0000

    PSR Formatting
    git-svn-id: http://repos.luxurylink.com/svn/repos/toolbox/trunk@5373 bd3a7434-8174-4811-bcf6-7b9fed35a952

--- app/vendors/pp/AIM.module.php
+++ app/vendors/pp/AIM.module.php
@@ -1,89 +1,94 @@
 <?php
-
-// AIM (AUTHORIZE.NET) PAYMENT MODULE
-// --------------------------------------------------------------------------
-// To be used with Processor.class.php only.  [alee@luxurylink.com]
-// Note:  See bottom of this page for sample responses
-
 class AIM
 {
-	var $url = 'https://secure.authorize.net/gateway/transact.dll';
-	var $map_params;
-	var $post_data;
+    var $url = 'https://secure.authorize.net/gateway/transact.dll';
+    var $map_params;
+    var $post_data;
 
-	function AIM($test_param = FALSE) {
-		
-		$this->post_data = array();
-		$this->post_data['x_version'] 			= '3.1';
-		$this->post_data['x_delim_data']		= 'TRUE';
-		$this->post_data['x_delim_char'] 		= '|';
-		$this->post_data['x_encap_char'] 		= '';
-		$this->post_data['x_relay_response'] 	= 'FALSE';
-		$this->post_data['x_login'] 			= 'LuxuryLink5200';
-		$this->post_data['x_tran_key'] 			= 'zWBPWcEWgz9HPQYP';
-		$this->post_data['x_test_request'] 		= $test_param ? 'TRUE' : 'FALSE';
-		$this->post_data['x_method'] 			= 'CC';
-		$this->post_data['x_type'] 				= 'AUTH_CAPTURE';
-		
-		$this->map_params = array();
-		$this->map_params['map_ticket_id'] 		= 'x_invoice_num';
-		$this->map_params['map_total_amount'] 	= 'x_amount';
-		$this->map_params['map_first_name'] 	= 'x_first_name';
-		$this->map_params['map_last_name'] 		= 'x_last_name';
-		$this->map_params['map_street'] 		= 'x_address';
-		$this->map_params['map_city'] 			= 'x_city';
-		$this->map_params['map_state'] 			= 'x_state';
-		$this->map_params['map_zip'] 			= 'x_zip';
-		$this->map_params['map_country'] 		= 'x_country';
-		$this->map_params['map_expiration'] 	= 'x_exp_date';
-		$this->map_params['map_card_num'] 		= 'x_card_num';
-	}
+    function AIM($test_param = false)
+    {
 
-	function ProcessResponse($raw_response) {
-		$tmp_array = split('[|]', strval($raw_response));
-		return $tmp_array;
-	}
+        $this->post_data = array();
+        $this->post_data['x_version'] = '3.1';
+        $this->post_data['x_delim_data'] = 'TRUE';
+        $this->post_data['x_delim_char'] = '|';
+        $this->post_data['x_encap_char'] = '';
+        $this->post_data['x_relay_response'] = 'FALSE';
+        $this->post_data['x_login'] = 'LuxuryLink5200';
+        $this->post_data['x_tran_key'] = 'zWBPWcEWgz9HPQYP';
+        $this->post_data['x_test_request'] = $test_param ? 'TRUE' : 'FALSE';
+        $this->post_data['x_method'] = 'CC';
+        $this->post_data['x_type'] = 'AUTH_CAPTURE';
 
-	function ChargeSuccess($response) {
-		if(isset($response[0])) {
-			if($response[0] == 1) {
-				return true;
-			}else return false;
-		}else return false;
-	}
+        $this->map_params = array();
+        $this->map_params['map_ticket_id'] = 'x_invoice_num';
+        $this->map_params['map_total_amount'] = 'x_amount';
+        $this->map_params['map_first_name'] = 'x_first_name';
+        $this->map_params['map_last_name'] = 'x_last_name';
+        $this->map_params['map_street'] = 'x_address';
+        $this->map_params['map_city'] = 'x_city';
+        $this->map_params['map_state'] = 'x_state';
+        $this->map_params['map_zip'] = 'x_zip';
+        $this->map_params['map_country'] = 'x_country';
+        $this->map_params['map_expiration'] = 'x_exp_date';
+        $this->map_params['map_card_num'] = 'x_card_num';
+    }
 
-	function GetMappedResponse($response) {
-		$paymentDetail = array();
-		
-		$paymentDetail['ppResponseDate']		= date('Y-m-d H:i:s', strtotime('now'));
-		$paymentDetail['ppTransactionId']		= $response[6];
-		$paymentDetail['ppApprovalText']		= $response[3];
-		$paymentDetail['ppApprovalCode']		= $response[0];
-		$paymentDetail['ppAvsCode']				= $response[5];
-		$paymentDetail['ppResponseText']		= $response[37];
-		$paymentDetail['ppResponseSubCode']		= $response[4];
-		$paymentDetail['ppReasonCode']			= $response[7];
-		$paymentDetail['isSuccessfulCharge']	= ($response[0] == 1) && (stristr($response[3], 'APPROVED')) ? 1 : 0;
-		
-		return $paymentDetail;
-	}
-	
-	function IsValidResponse($response, $valid_param) {
-		if(isset($response[7])) {
-			if(trim($response[7]) == trim($valid_param)) {
-				return true;
-			}else return false;
-		}else return false;
-	}
+    function ProcessResponse($raw_response)
+    {
+        $tmp_array = split('[|]', strval($raw_response));
+        return $tmp_array;
+    }
 
-	function GetResponseTxt($response) {
-		if(isset($response[3])) {
-			return $response[3];
-		}else {
-			return false;
-		}
-	}
+    function ChargeSuccess($response)
+    {
+        if (isset($response[0])) {
+            if ($response[0] == 1) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+
+    function GetMappedResponse($response)
+    {
+        $paymentDetail = array();
+
+        $paymentDetail['ppResponseDate'] = date('Y-m-d H:i:s', strtotime('now'));
+        $paymentDetail['ppTransactionId'] = $response[6];
+        $paymentDetail['ppApprovalText'] = $response[3];
+        $paymentDetail['ppApprovalCode'] = $response[0];
+        $paymentDetail['ppAvsCode'] = $response[5];
+        $paymentDetail['ppResponseText'] = $response[37];
+        $paymentDetail['ppResponseSubCode'] = $response[4];
+        $paymentDetail['ppReasonCode'] = $response[7];
+        $paymentDetail['isSuccessfulCharge'] = ($response[0] == 1) && (stristr($response[3], 'APPROVED')) ? 1 : 0;
+
+        return $paymentDetail;
+    }
+
+    function IsValidResponse($response, $valid_param)
+    {
+        if (isset($response[7])) {
+            if (trim($response[7]) == trim($valid_param)) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+
+    function GetResponseTxt($response)
+    {
+        if (isset($response[3])) {
+            return $response[3];
+        } else {
+            return false;
+        }
+    }
 }
-
-
-?>
--- app/vendors/pp/NOVA.module.php
+++ app/vendors/pp/NOVA.module.php
@@ -1,121 +1,122 @@
 <?php
-
-// NOVA (MYVIRTUAL MERCHANT) PAYMENT MODULE
-// --------------------------------------------------------------------------
-// To be used with Processor.class.php only.  [alee@luxurylink.com]
-// Note:  See bottom of this page for sample responses
-
 class NOVA
 {
-	var $url = 'https://www.myvirtualmerchant.com/VirtualMerchant/process.do';
-	var $map_params;
-	var $post_data;
-	private $valid_avs_codes = array("F","D","M","P","W","X","Y","Z");
-	
-	function NOVA($test_param = FALSE) {
-		$this->post_data = array();
-		$this->post_data['ssl_merchant_id'] 		= '506345';
-		$this->post_data['ssl_user_id'] 			= 'webpage';
-		$this->post_data['ssl_pin'] 				= '277516';
-		$this->post_data['ssl_transaction_type']	= 'ccavsonly';
-		$this->post_data['ssl_test_mode'] 			= $test_param ? 'TRUE' : 'FALSE';
-		$this->post_data['ssl_result_format'] 		= 'ASCII';
-		$this->post_data['ssl_show_form']	 		= 'false';
-		$this->post_data['ssl_cvv2cvc2_indicator'] 	= '0';
-		$this->post_data['ssl_salestax'] 			= '0';
+    var $url = 'https://www.myvirtualmerchant.com/VirtualMerchant/process.do';
+    var $map_params;
+    var $post_data;
+    private $valid_avs_codes = array("F", "D", "M", "P", "W", "X", "Y", "Z");
 
-		$this->map_params = array();   
-		$this->map_params['map_ticket_id'] 			= 'ssl_invoice_number'; // 25     
-		$this->map_params['map_total_amount'] 		= 'ssl_amount'; 	   // 13	 
-		$this->map_params['map_first_name'] 		= 'ssl_first_name';    // 20     25
-		$this->map_params['map_last_name'] 			= 'ssl_last_name';      // 30     25
-		$this->map_params['map_street'] 			= 'ssl_avs_address';       // 20     60
-		$this->map_params['map_street2'] 			= 'ssl_address2';         // 30     xx
-		$this->map_params['map_city'] 				= 'ssl_city';                // 30     30    
-		$this->map_params['map_state'] 				= 'ssl_state';              // 30     2
-		$this->map_params['map_zip'] 				= 'ssl_avs_zip';              // 9      10
-		$this->map_params['map_country'] 			= 'ssl_country';          // 50
-		$this->map_params['map_expiration'] 		= 'ssl_exp_date';      // 4      2
-		$this->map_params['map_card_num'] 			= 'ssl_card_number';     // 19    
-	}
+    function NOVA($test_param = false)
+    {
+        $this->post_data = array();
+        $this->post_data['ssl_merchant_id'] = '506345';
+        $this->post_data['ssl_user_id'] = 'webpage';
+        $this->post_data['ssl_pin'] = '277516';
+        $this->post_data['ssl_transaction_type'] = 'ccavsonly';
+        $this->post_data['ssl_test_mode'] = $test_param ? 'TRUE' : 'FALSE';
+        $this->post_data['ssl_result_format'] = 'ASCII';
+        $this->post_data['ssl_show_form'] = 'false';
+        $this->post_data['ssl_cvv2cvc2_indicator'] = '0';
+        $this->post_data['ssl_salestax'] = '0';
 
-	function ProcessResponse($raw_response) {
-		$processed = array();
-		$tmp_array = explode("\n", strval($raw_response));
-		foreach($tmp_array as $k=>$v) {
-			$tmp = explode('=', $v);
-			$processed[$tmp[0]] = (isset($tmp[1])) ? $tmp[1] : 0;
-		}
+        $this->map_params = array();
+        $this->map_params['map_ticket_id'] = 'ssl_invoice_number'; // 25
+        $this->map_params['map_total_amount'] = 'ssl_amount'; // 13
+        $this->map_params['map_first_name'] = 'ssl_first_name'; // 20     25
+        $this->map_params['map_last_name'] = 'ssl_last_name'; // 30     25
+        $this->map_params['map_street'] = 'ssl_avs_address'; // 20     60
+        $this->map_params['map_street2'] = 'ssl_address2'; // 30     xx
+        $this->map_params['map_city'] = 'ssl_city'; // 30     30
+        $this->map_params['map_state'] = 'ssl_state'; // 30     2
+        $this->map_params['map_zip'] = 'ssl_avs_zip'; // 9      10
+        $this->map_params['map_country'] = 'ssl_country'; // 50
+        $this->map_params['map_expiration'] = 'ssl_exp_date'; // 4      2
+        $this->map_params['map_card_num'] = 'ssl_card_number'; // 19
+    }
 
-		$processed['avs_only'] = false;
-				
-		if ($this->post_data['ssl_transaction_type'] == 'ccavsonly') {
-			$processed['avs_only'] = true;
-		}
+    function ProcessResponse($raw_response)
+    {
+        $processed = array();
+        $tmp_array = explode("\n", strval($raw_response));
+        foreach ($tmp_array as $k => $v) {
+            $tmp = explode('=', $v);
+            $processed[$tmp[0]] = (isset($tmp[1])) ? $tmp[1] : 0;
+        }
 
-		return $processed;
-	}
+        $processed['avs_only'] = false;
 
-	public function AddCvc($cvc) {
-		$this->post_data['ssl_cvv2cvc2'] 			= $cvc;
-		$this->post_data['ssl_cvv2cvc2_indicator'] 	= '1';
-	}
+        if ($this->post_data['ssl_transaction_type'] == 'ccavsonly') {
+            $processed['avs_only'] = true;
+        }
 
-	public function ChargeSuccess($response) {
-		if(isset($response['ssl_result'])) {
-			if($response['ssl_result'] == 0) {// DISABLE AVS FOR NOW - rvella 2-29-2012 && in_array($response['ssl_avs_response'],$this->valid_avs_codes)) {
-				return true;
-			}
-		}
-		
-		return false;
-	}
+        return $processed;
+    }
 
-	function GetMappedResponse($response) {
-		$paymentDetail = array();
-		
-		$paymentDetail['ppResponseDate']		= date('Y-m-d H:i:s', strtotime('now'));
-		$paymentDetail['ppTransactionId']		= (isset($response['ssl_txn_id'])) ? $response['ssl_txn_id'] : 0;
-		$paymentDetail['ppApprovalText']		= (isset($response['ssl_result_message'])) ? $response['ssl_result_message'] : 0;
-		$paymentDetail['ppApprovalCode']		= (isset($response['ssl_result'])) ? $response['ssl_result'] : 0;
-		$paymentDetail['ppAvsCode']				= (isset($response['ssl_avs_response'])) ? $response['ssl_avs_response'] : 0;
-		$paymentDetail['ppCvvCode']				= (isset($response['ssl_cvv2_response'])) ? $response['ssl_cvv2_response'] : 0;
-		$paymentDetail['ppResponseText']		= '';
-		$paymentDetail['ppResponseSubCode']		= '';
-		$paymentDetail['ppReasonCode']			= '';
-		$paymentDetail['isSuccessfulCharge']	= $this->ChargeSuccess($response) ? '1' : '0';
-		
-		return $paymentDetail;
-	}
-	
-	function IsValidResponse($response, $valid_param) {
-		if(isset($response['ssl_invoice_number'])) {
-			if(trim($response['ssl_invoice_number']) == trim($valid_param)) {
-				return true;
-			}
-		}
-		
-		return false;
-	}
+    public function AddCvc($cvc)
+    {
+        $this->post_data['ssl_cvv2cvc2'] = $cvc;
+        $this->post_data['ssl_cvv2cvc2_indicator'] = '1';
+    }
 
-	function GetResponseTxt($response) {
-		/*
-		if (isset($response['ssl_avs_response']) && !in_array($response['ssl_avs_response'],$this->valid_avs_codes)) {
-			return "NO_AVS";
-		} else
-		*/
-		if(isset($response['ssl_result_message'])) {
-			return $response['ssl_result_message'];
-		}else {
-			return false;
-		}
-	}
+    public function ChargeSuccess($response)
+    {
+        if (isset($response['ssl_result'])) {
+            if ($response['ssl_result'] == 0) { // DISABLE AVS FOR NOW - rvella 2-29-2012 && in_array($response['ssl_avs_response'],$this->valid_avs_codes)) {
+                return true;
+            }
+        }
 
-	public function getPostSale()
-	{
-		$this->post_data['ssl_transaction_type'] = 'ccsale';
-		return array('ssl_transaction_type' => 'ccsale');
-	}
+        return false;
+    }
+
+    function GetMappedResponse($response)
+    {
+        $paymentDetail = array();
+
+        $paymentDetail['ppResponseDate'] = date('Y-m-d H:i:s', strtotime('now'));
+        $paymentDetail['ppTransactionId'] = (isset($response['ssl_txn_id'])) ? $response['ssl_txn_id'] : 0;
+        $paymentDetail['ppApprovalText'] = (isset($response['ssl_result_message'])) ? $response['ssl_result_message'] : 0;
+        $paymentDetail['ppApprovalCode'] = (isset($response['ssl_result'])) ? $response['ssl_result'] : 0;
+        $paymentDetail['ppAvsCode'] = (isset($response['ssl_avs_response'])) ? $response['ssl_avs_response'] : 0;
+        $paymentDetail['ppCvvCode'] = (isset($response['ssl_cvv2_response'])) ? $response['ssl_cvv2_response'] : 0;
+        $paymentDetail['ppResponseText'] = '';
+        $paymentDetail['ppResponseSubCode'] = '';
+        $paymentDetail['ppReasonCode'] = '';
+        $paymentDetail['isSuccessfulCharge'] = $this->ChargeSuccess($response) ? '1' : '0';
+
+        return $paymentDetail;
+    }
+
+    function IsValidResponse($response, $valid_param)
+    {
+        if (isset($response['ssl_invoice_number'])) {
+            if (trim($response['ssl_invoice_number']) == trim($valid_param)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    function GetResponseTxt($response)
+    {
+        /*
+        if (isset($response['ssl_avs_response']) && !in_array($response['ssl_avs_response'],$this->valid_avs_codes)) {
+            return "NO_AVS";
+        } else
+        */
+        if (isset($response['ssl_result_message'])) {
+            return $response['ssl_result_message'];
+        } else {
+            return false;
+        }
+    }
+
+    public function getPostSale()
+    {
+        $this->post_data['ssl_transaction_type'] = 'ccsale';
+        return array('ssl_transaction_type' => 'ccsale');
+    }
 }
 
 /* 
