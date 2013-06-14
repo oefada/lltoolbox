d4ff7c045bfc991e8215ca34d3ed472dc3ffd2ca
mclifford <mclifford@bd3a7434-8174-4811-bcf6-7b9fed35a952>
2013-06-14 00:28:18 +0000

    SandboxController for testing
    git-svn-id: http://repos.luxurylink.com/svn/repos/toolbox/trunk@5381 bd3a7434-8174-4811-bcf6-7b9fed35a952

--- app/controllers/sandbox_controller.php
+++ app/controllers/sandbox_controller.php
@@ -1,19 +1,41 @@
 <?php
 class SandboxController extends AppController
 {
+    public $name = 'Sandbox';
+    public $uses = array();
 
-	var $name = 'Sandbox';
-	var $uses = array(
-		'ImageClient',
-		'Image',
-		'Ticket',
-		'ClientLoaPackageRel',
-	);
+    public function processorTest()
+    {
+        $paymentSettings = array(
+            'UserPaymentSetting' => array(
+                'nameOnCard' => 'Mort Wilson',
+                'expMonth' => date('m'),
+                'expYear' => date('y', strtotime('+1 year')),
+                'ccNumber' => '4111111111111111',
+                'address1' => '1234 Test Ave',
+                'address2' => '',
+                'city' => 'Los Angeles',
+                'state' => 'CA',
+                'postalCode' => '90046',
+                'country' => 'USA'
+            )
+        );
 
-	function index()
-	{
-		$z = $this->Ticket->field('packageId', array('Ticket.ticketId' => 196561));
-		$this->set('sandbox', $z);
-	}
+        $ticket = array(
+            'Ticket' => array(
+                'billingPrice' => 100,
+                'ticketId' => uniqid()
+            )
+        );
 
+        require_once(APP . '/vendors/pp/Processor.class.php');
+        $processor = new Processor('NOVA', true);
+        $processor->InitPayment($paymentSettings, $ticket);
+        //var_dump($processor); die;
+        $processor->SubmitPost();
+        var_dump($processor->GetMappedResponse());
+        var_dump($processor->GetResponseTxt());
+        var_dump($processor->getResponseData());
+        die;
+    }
 }
