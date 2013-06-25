89a94dd01a0d44acd83171e048bbbfff88b2f55f
mclifford <mclifford@bd3a7434-8174-4811-bcf6-7b9fed35a952>
2013-06-25 17:57:59 +0000

    PSR Formatting and CURL opt changes
    * CURLOPT_SSL_VERIFYPEER => false
    
    git-svn-id: http://repos.luxurylink.com/svn/repos/toolbox/trunk@5412 bd3a7434-8174-4811-bcf6-7b9fed35a952

--- app/controllers/components/http_request.php
+++ app/controllers/components/http_request.php
@@ -1,58 +1,52 @@
 <?php
-/**
- * User: oefada
- * Date: 4/9/13
- * Time: 6:11 PM
- */
-
 class HttpRequestComponent extends Object
 {
-
+    /**
+     * @param $controller
+     * @param array $settings
+     */
     public function initialize(&$controller, $settings = array())
     {
         // saving the controller reference for later use
         $this->controller =& $controller;
     }
-    /*
-    *
-    * Returns true if destination URL is reachable
-    * otherwise it returns false.
-    *
-    * $url is a string for the url including protocol e.g. http(s)
-    */
+
+    /**
+     *
+     * Returns true if destination URL is reachable
+     * otherwise it returns false.
+     *
+     * $url is a string for the url including protocol e.g. http(s)
+     */
     public function check_response($url, $timeout = 10)
     {
-        if (
-            empty($url)
-        ) {
+        if (empty($url)) {
             return false;
         }
 
         $ch = curl_init($url);
 
-        // Set request options
         curl_setopt_array(
             $ch,
             array(
                 CURLOPT_FOLLOWLOCATION => true,
                 CURLOPT_NOBODY => true,
                 CURLOPT_TIMEOUT => $timeout,
-                CURLOPT_USERAGENT => "page-check/1.0"
+                CURLOPT_USERAGENT => "page-check/1.0",
+                CURLOPT_SSL_VERIFYPEER => false
             )
         );
-        // Execute request
+
         curl_exec($ch);
-        // Check if an error occurred
+
         if (curl_errno($ch)) {
             curl_close($ch);
             return false;
         }
 
-        // Get HTTP response code
         $code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
         curl_close($ch);
 
-        // return response_code
         return $code;
     }
 
@@ -63,18 +57,17 @@ class HttpRequestComponent extends Object
      */
     public function isValidUrl($url)
     {
-
         if (empty($url)) {
             return false;
         }
         if (preg_match(
-            '/^(http|https):\/\/[a-z0-9_]+([\-\.]{1}[a-z_0-9]+)*\.[_a-z]{2,5}' . '((:[0-9]{1,5})?\/.*)?$/i',$url)
+            '/^(http|https):\/\/[a-z0-9_]+([\-\.]{1}[a-z_0-9]+)*\.[_a-z]{2,5}' . '((:[0-9]{1,5})?\/.*)?$/i',
+            $url
+        )
         ) {
             return true;
         } else {
             return false;
         }
-        //this way requires PHP > PHP 5.2
-        //return filter_var($url, FILTER_VALIDATE_URL);//if it's valid it return TRUE else FALSE
     }
 }
