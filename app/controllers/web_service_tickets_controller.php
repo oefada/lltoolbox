6a7f17a64ddd300b3a6832781e10fb21508ad0ae
oefada <oefada@bd3a7434-8174-4811-bcf6-7b9fed35a952>
2013-07-17 22:52:24 +0000

    Revert "TICKET4173: Family Getaway Client confirmation email PPV (23) populates wrong name in greeting"
    This reverts commit 9dc0040659131699368b3148ce4758721b6690bb.
    
    git-svn-id: http://repos.luxurylink.com/svn/repos/toolbox/trunk@5482 bd3a7434-8174-4811-bcf6-7b9fed35a952

--- app/controllers/web_service_tickets_controller.php
+++ app/controllers/web_service_tickets_controller.php
@@ -2401,7 +2401,7 @@ class WebServiceTicketsController extends WebServicesController
         }
 
         if (isset($templateFile) && $templateFile) {
-            if (($template = $this->newEmailTemplate($templateFile, $append, $specialException, $ppvNoticeTypeId)) !== false) {
+            if (($template = $this->newEmailTemplate($templateFile, $append, $specialException)) !== false) {
                 $rand = rand(100, 1000);
                 $file = "/tmp/template-" . $rand;
 
@@ -2520,7 +2520,7 @@ class WebServiceTicketsController extends WebServicesController
         }
     }
 
-    private function newEmailTemplate($templateFile, $append = "LL", $specialException = false, $ppvNoticeTypeId)
+    private function newEmailTemplate($templateFile, $append = "LL", $specialException = false)
     {
         // Add UTM links
         $template = file_get_contents("../vendors/email_msgs/includes/header_" . $append . ".html");
