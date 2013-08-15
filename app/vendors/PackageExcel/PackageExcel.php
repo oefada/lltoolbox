70364babb75e04158a541ad6b4e4b9c3a37edf7c
jwoods <jwoods@bd3a7434-8174-4811-bcf6-7b9fed35a952>
2013-08-15 18:36:17 +0000

    ticket 4336 - excel changes
    git-svn-id: http://repos.luxurylink.com/svn/repos/toolbox/trunk@5551 bd3a7434-8174-4811-bcf6-7b9fed35a952

--- app/vendors/PackageExcel/PackageExcel.php
+++ app/vendors/PackageExcel/PackageExcel.php
@@ -26,7 +26,7 @@ class PackageExcel
     public function __construct($viewVars = null)
     {
         $this->viewVars = $viewVars;
-        //die('viewvars<pre>' . htmlentities(print_r($viewVars, true)));
+        // die('viewvars<pre>' . htmlentities(print_r($viewVars, true)));
         $this->xls = ROOT . DS . APP_DIR . DS . 'views' . DS . 'packages' . DS . 'xls' . DS . 'Package.xlsx';
         $objReader = new PHPExcel_Reader_Excel2007();
         $this->sheet = $objReader->load($this->xls);
@@ -39,6 +39,8 @@ class PackageExcel
 
         // Modify Sheet
         $as = $this->sheet->getActiveSheet();
+        $as->getStyle('A1:C17')->getFill()->getStartColor()->setARGB('FFFFFFFF');
+        $as->getStyle('A19:C200')->getFill()->getStartColor()->setARGB('FFFFFFFF');
 
         switch ($package['siteId']) {
             case 1:
@@ -64,12 +66,35 @@ class PackageExcel
         $as->getCell('B9')->setValue($package['created']);
         $as->getCell('B10')->setValue($package['isBarter'] ? 'Barter' : 'Remit');
         $as->getCell('B11')->setValue($package['numNights']);
-        $as->getCell('B12')->setValue($package['numGuests']);
+        if ($package['isFlexPackage'] == 1) {
+        	$as->getCell('B12')->setValue($package['flexNumNightsMin'] . ' / ' . $package['flexNumNightsMax']);
+        } else {
+        	$as->getCell('B12')->setValue('');
+        }
+        $as->getCell('B13')->setValue($package['numGuests']);
+        if (in_array('family', $package['sites'])) {
+            $as->getCell('B14')->setValue($package['numChildren']);
+            $as->getCell('B15')->setValue($this->viewVars['package']['PackageAgeRange']['rangeLow'] . ' - ' . $this->viewVars['package']['PackageAgeRange']['rangeHigh']);
+        } else {
+            $as->getCell('B14')->setValue('');
+            $as->getCell('B15')->setValue('');
+        }
+        $as->getCell('B16')->setValue($this->viewVars['cc']);
 
 
-        $lowPrice = array_reverse(array_slice($this->viewVars['lowPrice'], 0, 9));
+        $lowPriceInfo = array_reverse(array_slice($this->viewVars['lowPrice'], 0, 9));
+        $lowPrice = array();
+        foreach ($lowPriceInfo as $lp) {
+        	if (array_key_exists($lp['PricePoint']['pricePointId'], $lowPrice)) {
+        		$lowPrice[$lp['PricePoint']['pricePointId']]['dateRanges'] .= '|' . $lp['dateRanges'];
+        	} else {
+        		$lowPrice[$lp['PricePoint']['pricePointId']] = $lp;
+        	}
+        }
+ 
+        
         $lp_row_height = 8;
-        $lp_row_offset = 28 + count($lowPrice) * $lp_row_height;
+        $lp_row_offset = 30 + count($lowPrice) * $lp_row_height;
         // Remove extra rows
         for ($i = 0; $i < 100; $i++) {
             $as->removeRow($lp_row_offset);
@@ -78,8 +103,12 @@ class PackageExcel
         // Low price
         $i = 0;
         while ($lp = array_pop($lowPrice)) {
-            $lp_row_offset = 29 + $i * $lp_row_height;
+            $lp_row_offset = 31 + $i * $lp_row_height;
             $as->getCell('A' . $lp_row_offset)->setValue(str_replace('<br/>',', ',$lp['dateRanges']));
+            $dateLineCount = substr_count($lp['dateRanges'], '|') + 1;
+            $as->getRowDimension($lp_row_offset)->setRowHeight($dateLineCount * 24);
+            
+            $as->getCell('A' . $lp_row_offset)->setValue(str_replace('|', "\n", $lp['dateRanges']));
             $as->getCell('B' . ($lp_row_offset + 1))->setValue($package['numNights']);
             $as->getCell('B' . ($lp_row_offset + 2))->setValue($lp['retailValue']);
             $as->getCell('C' . ($lp_row_offset + 3))->setValue(
@@ -92,14 +121,14 @@ class PackageExcel
         }
 
         // Booking conditions
-        $as->getCell('A27')->setValue($package['termsAndConditions']);
+        $as->getCell('A29')->setValue($package['termsAndConditions']);
 
         // Blackout Weekdays
-        $as->getCell('A24')->setValue($this->viewVars['bo_weekdays']);
+        $as->getCell('A26')->setValue($this->viewVars['bo_weekdays']);
 
         // Blackout Dates
-        $lp_row_offset = 21;
-        foreach ($this->viewVars['validity'] as $bod) {
+        $lp_row_offset = 23;
+        foreach ($this->viewVars['blackout'] as $bod) {
             $as->insertNewRowBefore($lp_row_offset + 1);
             $as->mergeCells('A' . $lp_row_offset . ':C' . $lp_row_offset);
             $as->getCell('A' . $lp_row_offset)->setValue($bod);
@@ -108,7 +137,7 @@ class PackageExcel
         $as->removeRow($lp_row_offset);
 
         // Inclusions
-        $lp_row_offset = 17;
+        $lp_row_offset = 19;
         $inclusions = $this->viewVars['package']['ClientLoaPackageRel'][0]['Inclusions'];
         $i = 0;
         foreach ($inclusions as $inclusion) {
