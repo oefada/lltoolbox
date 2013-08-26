7433e4f81809dd4e26030320272d1318d8745936
oefada <oefada@bd3a7434-8174-4811-bcf6-7b9fed35a952>
2013-08-26 18:28:44 +0000

    TICKET3768: Automated LOA- added installment types model
    git-svn-id: http://repos.luxurylink.com/svn/repos/toolbox/trunk@5565 bd3a7434-8174-4811-bcf6-7b9fed35a952

---
+++ app/models/loa_installment_type.php
@@ -0,0 +1,11 @@
+<?php
+class LoaInstallmentType extends AppModel
+{
+
+    var $name = 'LoaInstallmentType';
+//    var $useTable = 'loaInstallmentType';
+//    var $primaryKey = 'loaInstallmentTypeId';
+    var $displayField = 'name';
+    var $order = 'loaInstallmentTypeId';
+
+}
