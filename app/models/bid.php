4aa72c1282f34964962b291d9a31350d3157ed12
mclifford <mclifford@bd3a7434-8174-4811-bcf6-7b9fed35a952>
2013-11-14 03:20:06 +0000

    TICKET4522
    * Formatting
    
    git-svn-id: http://repos.luxurylink.com/svn/repos/toolbox/trunk@5808 bd3a7434-8174-4811-bcf6-7b9fed35a952

--- app/models/bid.php
+++ app/models/bid.php
@@ -1,51 +1,68 @@
 <?php
-class Bid extends AppModel {
+class Bid extends AppModel
+{
+    public $name = 'Bid';
+    public $useTable = 'bid';
+    public $primaryKey = 'bidId';
+    public $skipBeforeSaveFilter = false;
 
-	var $name = 'Bid';
-	var $useTable = 'bid';
-	var $primaryKey = 'bidId';
-	var $skipBeforeSaveFilter = false;
-	
-	var $belongsTo = array('User' => array('foreignKey' => 'userId'),
-						   'Offer' => array('foreignKey' => 'offerId')
-						   );
-	
-	function beforeSave() {
-		if($this->skipBeforeSaveFilter == false):
-		$bidData = $this->data['Bid'];
-		unset($this->data['Bid']);
-		
-		$this->data['Bid']['bidId'] = $bidData['bidId'];
-		$this->data['Bid']['inactive'] = $bidData['inactive'];
+    public $belongsTo = array('User' => array('foreignKey' => 'userId'),
+        'Offer' => array('foreignKey' => 'offerId')
+    );
 
-		endif;
-		return true;
-	}
-	
-	function beforeDelete() {
-		return false;
-	}
-    
-	function getBidStatsForOffer($offerId) {
-		$query = "SELECT COUNT(bidId) AS bidCount, MAX(bidAmount) AS winner
-							FROM bid 
-							WHERE offerId = " . $offerId;
-		if ($bidStats = $this->query($query)) {
-			return $bidStats;
-		}
-	}
+    /**
+     * @return bool
+     */
+    public function beforeSave()
+    {
+        if ($this->skipBeforeSaveFilter == false) {
+            $bidData = $this->data['Bid'];
+            unset($this->data['Bid']);
 
-	function getBidStatsForPackageId($packageId) {
+            $this->data['Bid']['bidId'] = $bidData['bidId'];
+            $this->data['Bid']['inactive'] = $bidData['inactive'];
+        }
 
-		$q="SELECT COUNT(bidId) AS bidCount ";
-		$q.="FROM bid ";
-		$q.="INNER JOIN offerLuxuryLink as ot on (ot.offerId=bid.offerId) ";
-		$q.="WHERE packageId= " . $packageId;
-		$bidStats = $this->query($q);
-		return $bidStats[0][0]['bidCount'];
+        return true;
+    }
 
-	}
+    /**
+     * @return bool
+     */
+    public function beforeDelete()
+    {
+        return false;
+    }
 
+    /**
+     * @param $offerId
+     * @return array
+     */
+    public function getBidStatsForOffer($offerId)
+    {
+        $query = "
+            SELECT COUNT(bidId) AS bidCount, MAX(bidAmount) AS winner
+            FROM bid
+            WHERE offerId = ?
+        ";
 
+        if ($bidStats = $this->query($query, array($offerId))) {
+            return $bidStats;
+        }
+    }
+
+    /**
+     * @param $packageId
+     * @return mixed
+     */
+    public function getBidStatsForPackageId($packageId)
+    {
+        $q = "SELECT COUNT(bidId) AS bidCount ";
+        $q .= "FROM bid ";
+        $q .= "INNER JOIN offerLuxuryLink as ot on (ot.offerId=bid.offerId) ";
+        $q .= "WHERE packageId= ?";
+        $bidStats = $this->query($q, array($packageId));
+
+        return $bidStats[0][0]['bidCount'];
+    }
 }
-?>
