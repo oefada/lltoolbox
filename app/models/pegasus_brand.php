9921e3bc0fd17b4515aa6ba3f1116282f0ea7c6b
jwoods <jwoods@bd3a7434-8174-4811-bcf6-7b9fed35a952>
2013-12-09 19:47:17 +0000

    add model
    git-svn-id: http://repos.luxurylink.com/svn/repos/toolbox/trunk@5841 bd3a7434-8174-4811-bcf6-7b9fed35a952

---
+++ app/models/pegasus_brand.php
@@ -0,0 +1,10 @@
+<?php
+class PegasusBrand extends AppModel {
+
+	var $name = 'PegasusBrand';
+	var $useTable = 'pegasusBrand';
+	var $primaryKey = 'pegasusBrandId';
+	var $displayField = 'description';
+    
+}
+
