e1d3d548da0c74bad9137d5f18f8496b4c6a88c3
oefada <oefada@bd3a7434-8174-4811-bcf6-7b9fed35a952>
2013-12-19 18:30:42 +0000

    Made access to database source conditional based on environment
    git-svn-id: http://repos.luxurylink.com/svn/repos/toolbox/trunk@5865 bd3a7434-8174-4811-bcf6-7b9fed35a952

--- app/models/reporting.php
+++ app/models/reporting.php
@@ -3,7 +3,15 @@
 class Reporting extends AppModel
 {
 	public $useTable = false;
-	public $useDbConfig = 'reporting';
+	//public $useDbConfig = 'reporting';
 
+    public function __construct(){
+        parent::__construct();
+        if (ISDEV == true ||ISSTAGE == true){
+            $this->useDbConfig = 'default';
+        }else{
+            $this->useDbConfig = 'reporting';
+        }
+    }
 }
 	
