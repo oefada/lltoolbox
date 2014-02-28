1152f35785949dc50ba4f5713e80b4ffa8f121f5
mclifford <mclifford@bd3a7434-8174-4811-bcf6-7b9fed35a952>
2014-02-28 23:38:51 +0000

    If it is no longer in use, remove the code.
    git-svn-id: http://repos.luxurylink.com/svn/repos/toolbox/trunk@5960 bd3a7434-8174-4811-bcf6-7b9fed35a952

--- app/controllers/web_service_bids_controller.php
+++ app/controllers/web_service_bids_controller.php
@@ -1,3 +1,2 @@
 <?php
-// THIS WEBSERVICE IS NO LONGER IN USE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 die('THIS WEBSERVICE IS NO LONGER IN USE!');
--- app/controllers/web_service_users_controller.php
+++ app/controllers/web_service_users_controller.php
@@ -1,28 +1,2 @@
 <?php
-
 die("NO LONGER IN USE");
-
-App::import('Vendor', 'nusoap/web_services_controller');
-Configure::write('debug', 0);
-
-class WebServiceUsersController extends WebServicesController
-{
-	var $name = 'WebServiceUsers';
-	var $uses = 'User';
-	var $serviceUrl;
-	var $errorResponse = array();
-	var $api = array(
-					'userProcessor1' => array(
-						'doc' => 'Any changes to user info from LIVE will be pushed here to update backend',
-						'input' => array('in0' => 'xsd:string'),
-						'output' => array('return' => 'xsd:string')
-						)
-					);
-
-	function __construct() {
-		parent::construct();
-		
-		$this->serviceUrl = Configure::read("Url.Ws");
-	}
-}
-?>
