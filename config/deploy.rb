e743a33403984b9bf6f1ebfc7756eae735276a12
mclifford <mclifford@bd3a7434-8174-4811-bcf6-7b9fed35a952>
2014-04-07 20:02:31 +0000

    Capistrano scripts
    git-svn-id: http://repos.luxurylink.com/svn/repos/toolbox/trunk@6045 bd3a7434-8174-4811-bcf6-7b9fed35a952

---
+++ config/deploy.rb
@@ -0,0 +1,23 @@
+# config valid only for Capistrano 3.1
+lock '3.1.0'
+
+set :application, 'toolbox.luxurylink.com'
+set :repo_url, 'ssh://git@git.luxurylink.com:7999/lltg/toolbox.git'
+
+# Default branch is :master
+ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
+
+set :deploy_to, "/var/www/deployments/toolbox.luxurylink.com"
+set :scm, :git
+set :format, :pretty
+set :log_level, :info
+set :pty, false
+set :keep_releases, 2
+
+task :post_deploy do
+  on roles(:app) do
+    execute "cd #{deploy_to}/current/app/config && ln -s #{fetch(:config_file)} database.php"
+    execute "cd #{deploy_to}/current/app/vendors && ln -s #{fetch(:appshared_location)} ."
+    execute "cd #{deploy_to}/current/app && chmod 777 tmp"
+  end
+end
