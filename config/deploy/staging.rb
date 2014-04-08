cd0494ad8f40d69a232489cfdc1d46c161119058
mclifford <mclifford@bd3a7434-8174-4811-bcf6-7b9fed35a952>
2014-04-08 18:25:29 +0000

    Updated server for staging
    git-svn-id: http://repos.luxurylink.com/svn/repos/toolbox/trunk@6059 bd3a7434-8174-4811-bcf6-7b9fed35a952

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,5 +1,4 @@
-server 'uat-internal-luxurylink', user: 'root', roles: %w{app}
+server 'uat-internal', user: 'root', roles: %w{app}
 
 set :config_file, 'database.uat.php'
-
 set :memcache_servers, %w[localhost]
