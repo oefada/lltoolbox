1a7411540d6dc5ce71d9afad048da4d939c71b77
mclifford <mclifford@bd3a7434-8174-4811-bcf6-7b9fed35a952>
2014-04-30 23:55:16 +0000

    Don't deploy to toolbox-old
    git-svn-id: http://repos.luxurylink.com/svn/repos/toolbox/trunk@6127 bd3a7434-8174-4811-bcf6-7b9fed35a952

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,5 @@
 server 'toolbox', user: 'root', roles: %w{app}
-server 'toolbox-new', user: 'root', roles: %w{app}
+# server 'toolbox-new', user: 'root', roles: %w{app}
 
 set :config_file, 'database.prod.php'
 set :memcache_servers, %w[localhost]
