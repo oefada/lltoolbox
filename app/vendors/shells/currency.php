a695781d3a1c232d031c66ecf3621c35964fdf17
Michael Clifford <mclifford@luxurylink.com>
2014-05-02 17:11:27 -0700

    PSR formatting

--- app/vendors/shells/currency.php
+++ app/vendors/shells/currency.php
@@ -1,12 +1,17 @@
 <?php
-class CurrencyShell extends Shell {
-    var $uses = array('Currency', 'CurrencyExchangeRate');
+class CurrencyShell extends Shell
+{
+    public $uses = array('Currency', 'CurrencyExchangeRate');
 
-    var $api_url = 'http://download.finance.yahoo.com/d/quotes.csv?e=.csv';
-    var $api_format = 'sl1d1t1';
-    var $currencyCodeList;
+    public $api_url = 'http://download.finance.yahoo.com/d/quotes.csv?e=.csv';
+    public $api_format = 'sl1d1t1';
+    public $currencyCodeList;
 
-    function main() {
+    /**
+     *
+     */
+    public function main()
+    {
         $url = $this->prepare_api_url();
 
         $result = $this->fetch_api_data($url);
@@ -14,28 +19,41 @@ class CurrencyShell extends Shell {
         $newRates = $this->update_exchange_rates($result);
         // email when no new entries made
         $dayOfWeek = date('w');
-        if ($newRates == 0 && $dayOfWeek != 0 && $dayOfWeek != 6) { $this->sendEmailNoEntries(); }
+        if ($newRates == 0 && $dayOfWeek != 0 && $dayOfWeek != 6) {
+            $this->sendEmailNoEntries();
+        }
     }
 
-    function prepare_api_url() {
+    /**
+     * @return string
+     */
+    public function prepare_api_url()
+    {
         $currencyList = $this->Currency->find('list', array('fields' => 'currencyCode'));
 
         $this->currencyCodeList = $currencyList;
 
         $currencyString = '';
         foreach ($currencyList as $currencyCode) {
-            if ($currencyCode == 'USD') { continue; }
-            $currencyString .= $currencyCode.'USD'.'=X+';
+            if ($currencyCode == 'USD') {
+                continue;
+            }
+            $currencyString .= $currencyCode . 'USD' . '=X+';
         }
 
         if (empty($currencyString)) {
             die('No currency codes found');
         }
 
-        return $this->api_url.'&f='.$this->api_format.'&'.'s='.$currencyString;
+        return $this->api_url . '&f=' . $this->api_format . '&' . 's=' . $currencyString;
     }
 
-    function fetch_api_data($url) {
+    /**
+     * @param $url
+     * @return array
+     */
+    public function fetch_api_data($url)
+    {
         $handle = @fopen($url, 'r');
 
         do {
@@ -47,12 +65,19 @@ class CurrencyShell extends Shell {
         return $result;
     }
 
-    function update_exchange_rates($result) {
+    /**
+     * @param $result
+     * @return int
+     */
+    public function update_exchange_rates($result)
+    {
 
         $dayOfWeek = date('w');
         $sqlCountToday = "SELECT COUNT(*) as nbr FROM currencyExchangeRate WHERE DATE_FORMAT(created, '%Y-%m-%d') = '" . date('Y-m-d') . "'";
-	$countToday = array_pop(array_pop(array_pop($this->CurrencyExchangeRate->query($sqlCountToday))));
-	if ($countToday == 0 && $dayOfWeek != 0 && $dayOfWeek != 6) { $this->sendEmailNoneToday($sqlCountToday); }
+        $countToday = array_pop(array_pop(array_pop($this->CurrencyExchangeRate->query($sqlCountToday))));
+        if ($countToday == 0 && $dayOfWeek != 0 && $dayOfWeek != 6) {
+            $this->sendEmailNoneToday($sqlCountToday);
+        }
 
         $recordCount = 0;
 
@@ -71,20 +96,20 @@ class CurrencyShell extends Shell {
             $date = $parts[2];
             $time = $parts[3];
 
-            $phpdate = strtotime( str_replace('"', '', $date).' '.str_replace('"', '', $time) );
-            $mysqldate = date( 'Y-m-d H:i:s', $phpdate);
-	    
-		
+            $phpdate = strtotime(str_replace('"', '', $date) . ' ' . str_replace('"', '', $time));
+            $mysqldate = date('Y-m-d H:i:s', $phpdate);
+
+
             $data = array('currencyId' => $currencyId,
-                                                'dailyExchangeRateToDollar' => $exchangeRate,
-                                                'asOfDateTime' => $mysqldate);
+                'dailyExchangeRateToDollar' => $exchangeRate,
+                'asOfDateTime' => $mysqldate);
 
             $existingRate = $this->CurrencyExchangeRate->find('all', array('conditions' => $data));
 
             //only insert this rate if it's different from an existing one
             if (empty($existingRate) && floatval($exchangeRate) > 0) {
-                $sqlAvgWeek = 'SELECT AVG(dailyExchangeRateToDollar) as avg FROM currencyExchangeRate WHERE currencyId = '.$currencyId.' AND asOfDateTime BETWEEN ("'.$mysqldate.'" - INTERVAL 1 WEEK) AND "'.$mysqldate.'"';
-                $sqlAvgMonth = 'SELECT AVG(dailyExchangeRateToDollar) as avg FROM currencyExchangeRate WHERE currencyId = '.$currencyId.' AND asOfDateTime BETWEEN ("'.$mysqldate.'" - INTERVAL 1 MONTH) AND "'.$mysqldate.'"';
+                $sqlAvgWeek = 'SELECT AVG(dailyExchangeRateToDollar) as avg FROM currencyExchangeRate WHERE currencyId = ' . $currencyId . ' AND asOfDateTime BETWEEN ("' . $mysqldate . '" - INTERVAL 1 WEEK) AND "' . $mysqldate . '"';
+                $sqlAvgMonth = 'SELECT AVG(dailyExchangeRateToDollar) as avg FROM currencyExchangeRate WHERE currencyId = ' . $currencyId . ' AND asOfDateTime BETWEEN ("' . $mysqldate . '" - INTERVAL 1 MONTH) AND "' . $mysqldate . '"';
                 $avgWeek = array_pop(array_pop(array_pop($this->CurrencyExchangeRate->query($sqlAvgWeek))));
                 $avgMonth = array_pop(array_pop(array_pop($this->CurrencyExchangeRate->query($sqlAvgMonth))));
 
@@ -104,22 +129,26 @@ class CurrencyShell extends Shell {
         return $recordCount;
     }
 
-	function sendEmailNoEntries() {
-		$emailTo = 'jwoods@luxurylink.com';
-		$emailSubject = "Toolbox CurrencyShell did not insert any records.";
-		$emailHeaders = "From: LuxuryLink.com DevMail<devmail@luxurylink.com>\r\n";
-		$emailBody = 'no records inserted: ' . date('m/d/Y g:i:s a');
-		@mail($emailTo, $emailSubject, $emailBody, $emailHeaders);
-	}
+    /**
+     *
+     */
+    public function sendEmailNoEntries()
+    {
+        $emailTo = 'jwoods@luxurylink.com';
+        $emailSubject = "Toolbox CurrencyShell did not insert any records.";
+        $emailHeaders = "From: LuxuryLink.com DevMail<devmail@luxurylink.com>\r\n";
+        $emailBody = 'no records inserted: ' . date('m/d/Y g:i:s a');
+        @mail($emailTo, $emailSubject, $emailBody, $emailHeaders);
+    }
 
-	function sendEmailNoneToday($sql) {
-		$emailTo = 'jwoods@luxurylink.com';
-		$emailSubject = "Toolbox CurrencyShell running for " . date('Y-m-d');
-		$emailHeaders = "From: LuxuryLink.com DevMail<devmail@luxurylink.com>\r\n";
-		@mail($emailTo, $emailSubject, $sql, $emailHeaders);
-	}
-
-
-
+    /**
+     * @param $sql
+     */
+    public function sendEmailNoneToday($sql)
+    {
+        $emailTo = 'jwoods@luxurylink.com';
+        $emailSubject = "Toolbox CurrencyShell running for " . date('Y-m-d');
+        $emailHeaders = "From: LuxuryLink.com DevMail<devmail@luxurylink.com>\r\n";
+        @mail($emailTo, $emailSubject, $sql, $emailHeaders);
+    }
 }
-?>
