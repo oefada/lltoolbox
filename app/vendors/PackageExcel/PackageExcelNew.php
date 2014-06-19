07d35adf1304e62ec17951ca14ad9f18ccfdb9cb
Onjefu Efada <oefada@luxurylink.com>
2014-06-18 17:09:27 -0700

    TICKET4769: recaclulated Flex data w/ new formula. added styling

--- app/vendors/PackageExcel/PackageExcelNew.php
+++ app/vendors/PackageExcel/PackageExcelNew.php
@@ -29,6 +29,7 @@ class PackageExcel
     public $current_row_offset = 0;
     private $validityPeriodArray = array();
     private $cellTotalInclusions, $rowTotalinclusions;
+    private $multiDayInclusionSingleDayPrice = 0;
 
     public function __construct($viewVars = null)
     {
@@ -66,6 +67,8 @@ class PackageExcel
         $as->getCell('B7')->setValue($package['isBarter'] ? 'Barter' : 'Remit');
         $as->getCell('B8')->setValue(date('M d, Y',strtotime($package['created'])));
         $as->getCell('B14')->setValue($package['packageName']);
+        $as->getCell('B15')->setValue($package['roomGrade']);
+
 
         $as->getCell('B19')->setValue($package['numNights']);
 //        if ($package['isFlexPackage'] == 1) {
@@ -108,6 +111,24 @@ class PackageExcel
                 'color' => array(
                     'rgb' => 'EEEEEE',
                 ),
+            )
+        );
+
+        $redFontStyleArray = array(
+            'font' => array(
+                'color' => array(
+                    'rgb' => 'FF0000',
+                ),
+                'bold' => true,
+            )
+        );
+
+        $blueFontStyleArray = array(
+            'font' => array(
+                'color' => array(
+                    'rgb' => '0000FF',
+                ),
+            'italic'=>true,
             )
         );
         $boldStyleArray = array(
@@ -159,6 +180,10 @@ class PackageExcel
         $percentageStyle =  array(
             'code' => PHPExcel_Style_NumberFormat::FORMAT_PERCENTAGE_00
         );
+
+        $currencyStyle = array('numberformat' => array('code' => PHPExcel_Style_NumberFormat::FORMAT_CURRENCY_USD_SIMPLE));
+        $currencyStyleNoCents = array('numberformat' => array('code' => PHPExcel_Style_NumberFormat::FORMAT_CURRENCY_USD));
+
         $as->getStyle('A1:G100')->getAlignment()->setVertical(PHPExcel_Style_Alignment::VERTICAL_TOP);
 
         $validity_data_row_start = $this->getDefaultOffset() + 1;
@@ -197,7 +222,8 @@ class PackageExcel
                         //$this->cellColor($labelColumn . $lp_row_offset, 'FFFDA9');
                         $as->getStyle($labelColumn . $lp_row_offset)->applyFromArray($ratePerNightStyle);
 
-                        $as->getCell($feeColumn . $lp_row_offset)->setValue($lp['flexPricePerNight']);
+                        $as->getCell($feeColumn . $lp_row_offset)->setValue($lp['LoaItemRate']['price']);
+
                         $as->getStyle($feeColumn . ($lp_row_offset))->getAlignment()->setHorizontal(PHPExcel_Style_Alignment::HORIZONTAL_LEFT);
 
                         //multiply rate per night with number of nights
@@ -244,6 +270,11 @@ class PackageExcel
                         $as->getCell('D'.$rowTotalAccom)->setValue('=ROUND(SUM(D'.$lp_row_offset.':D'.($lastLineIndex).'),2)');
                         $as->getStyle('D'.$lp_row_offset.':D'.($lastLineIndex))->applyFromArray($invisibleFontStyleArray);
                         $as->getStyle('D'.$rowTotalAccom)->applyFromArray($boldStyleArray);
+
+                        $as->getRowDimension($rowTotalAccom)->setRowHeight(18);
+                        $as->getStyle('D'.$rowTotalAccom)->applyFromArray($currencyStyle);
+
+
                         //store position of total accommodations for index $i
 
                         //use index to draw a border around validity date groups
@@ -286,6 +317,10 @@ class PackageExcel
             $as->getCell($totalsColumn. ($inclusions_row_offset + $key))->setValue(
                 '='.$feeColumn. ($inclusions_row_offset + $key) . '*' . $inclusion['PackageLoaItemRel']['quantity']
             );
+            $inclusionsQty  =  floatval($inclusion['PackageLoaItemRel']['quantity']);
+            if($inclusionsQty > 1){
+                $this->addMultiDayInclusionSingleDayPrice($inclusion['LoaItem']['itemBasePrice']);
+            }
             if(($key+1)==$numberInclusions){
                 //this is the last inclusion
                 $lastInclusionIndex = $key+1;
@@ -294,6 +329,7 @@ class PackageExcel
                 $as->getStyle($feeColumn . ($inclusions_row_offset + $lastInclusionIndex))->getAlignment()->setHorizontal(PHPExcel_Style_Alignment::HORIZONTAL_RIGHT);
                 /***/
                 $as->getCell($totalsColumn . ($inclusions_row_offset + $lastInclusionIndex))->setValue('=ROUND(SUM('.$totalsColumn.$inclusions_row_offset.':'.$totalsColumn.(($inclusions_row_offset + $lastInclusionIndex)-1).'),2)');
+                $as->getStyle($totalsColumn . ($inclusions_row_offset + $lastInclusionIndex))->applyFromArray($currencyStyle);
 
                 $as->getStyle($totalsColumn . ($inclusions_row_offset + $lastInclusionIndex))->applyFromArray($boldStyleArray);
                 $this->rowTotalinclusions = $inclusions_row_offset + $lastInclusionIndex;
@@ -341,7 +377,7 @@ class PackageExcel
             $as->getCell('B' . ($lp_row_offset))->setValue('PACKAGE RETAIL VALUE IN LOCAL CURRENCY:');
             $as->getCell('B' . ($lp_row_offset +2))->setValue('Percentage of Retail:');
             $as->getCell('B' . ($lp_row_offset +4))->setValue('Percentage of Retail:');
-            $as->getCell('B' . ($lp_row_offset +6))->setValue('Percentage of Retail:');
+            $as->getCell('B' . ($lp_row_offset +6))->setValue('Percentage of Daily Retail:');
 
 
              //put all in border
@@ -371,40 +407,56 @@ class PackageExcel
              $as->getStyle('C' . $lp_row_offset)->applyFromArray($boldStyleArray);
 
              $retailValue = floatval($lp['retailValue']);
+             $validityTotalCell = 'D'.$this->getValidityTotalRowByKey($i);
 
              $as->getCell('C'. ($lp_row_offset+1))->setValue('AUCTION STARTING PRICE: ');
+             $as->getStyle('C'. ($lp_row_offset+1))->applyFromArray($redFontStyleArray);
 
              $aucPercentOfRetail =  '=('.floatval($lp['auctionPrice']).'/'.floatval($retailValue).')';
              $as->getCell('C'. ($lp_row_offset+2))->setValue($aucPercentOfRetail);
              $as->getStyle('C' .($lp_row_offset+2))->getNumberFormat()->applyFromArray($percentageStyle);
+             $as->getStyle('C' .($lp_row_offset+2))->applyFromArray($blueFontStyleArray);
              $as->getCell('C'. ($lp_row_offset+3))->setValue('BUY NOW PRICE: ');
+             $as->getStyle('C'. ($lp_row_offset+3))->applyFromArray($redFontStyleArray);
 
              $buyNowPercentOfRetail = '=('.floatval($lp['buyNowPrice']).'/'.$retailValue.')';
              $as->getCell('C'. ($lp_row_offset+4))->setValue($buyNowPercentOfRetail);
              $as->getStyle('C' .($lp_row_offset+4))->getNumberFormat()->applyFromArray($percentageStyle);
+             $as->getStyle('C' .($lp_row_offset+4))->applyFromArray($blueFontStyleArray);
 
              $as->getCell('C'. ($lp_row_offset+5))->setValue('FLEX NIGHT PRICE (+/-): ');
-             $flexPercentOfRetail = '=('.floatval($lp['flexPricePerNight']).'/'.$retailValue.')';
+             $as->getStyle('C'. ($lp_row_offset+5))->applyFromArray($redFontStyleArray);
+             $flexDenominator = '(('.$validityTotalCell.'/B19)+'.$this->multiDayInclusionSingleDayPrice.')';
+             $flexPercentOfRetail = '=('.floatval($lp['flexPricePerNight']).'/'.$flexDenominator.')';
              $as->getCell('C'. ($lp_row_offset+6))->setValue($flexPercentOfRetail);
              $as->getStyle('C' .($lp_row_offset+6))->getNumberFormat()->applyFromArray($percentageStyle);
+             $as->getStyle('C' .($lp_row_offset+6))->applyFromArray($blueFontStyleArray);
 
              //Column D
-             $validityTotalCell = 'D'.$this->getValidityTotalRowByKey($i);
-             $totalWithInclusions = '=('.$validityTotalCell.'+ '.$this->cellTotalInclusions.')';
+             //Order is important- values must already exist.
+             $totalWithInclusions = '=ROUND(('.$validityTotalCell.'+ '.$this->cellTotalInclusions.'),0)';
              $as->getCell('D'. ($lp_row_offset))->setValue($totalWithInclusions);
+             $as->getStyle('D'. ($lp_row_offset))->applyFromArray($currencyStyleNoCents);
+             $as->getStyle('D'. ($lp_row_offset))->applyFromArray($boldStyleArray);
 
              $cellTotalWithInclusions = 'D'.$lp_row_offset;
              $cellAucPercentOfRetail = 'C'.($lp_row_offset+2);
-             $totalAucPercentOfRetail = '=ROUND(('.$cellAucPercentOfRetail.'* '.$cellTotalWithInclusions.'),2)';
+             $totalAucPercentOfRetail = '=ROUND(('.$cellAucPercentOfRetail.'* '.$cellTotalWithInclusions.'),0)';
              $as->getCell('D'. ($lp_row_offset+1))->setValue($totalAucPercentOfRetail);
+             $as->getStyle('D'. ($lp_row_offset+1))->applyFromArray($currencyStyleNoCents);
+             $as->getStyle('D'. ($lp_row_offset+1))->applyFromArray($redFontStyleArray);
 
              $cellBuyNowPercentOfRetail = 'C'.($lp_row_offset+4);
-             $totalBuyNowPercentOfRetail = '=ROUND(('.$cellBuyNowPercentOfRetail.'* '.$cellTotalWithInclusions.'),2)';
+             $totalBuyNowPercentOfRetail = '=ROUND(('.$cellBuyNowPercentOfRetail.'* '.$cellTotalWithInclusions.'),0)';
              $as->getCell('D'. ($lp_row_offset+3))->setValue($totalBuyNowPercentOfRetail);
+             $as->getStyle('D'. ($lp_row_offset+3))->applyFromArray($currencyStyleNoCents);
+             $as->getStyle('D'. ($lp_row_offset+3))->applyFromArray($redFontStyleArray);
 
              $cellFlexPercentOfRetail = 'C'.($lp_row_offset+6);
-             $totalFlexPercentOfRetail = '=ROUND(('.$cellFlexPercentOfRetail.'* '.$cellTotalWithInclusions.'),2)';
+             $totalFlexPercentOfRetail = '=ROUND(('.$cellFlexPercentOfRetail.'* '.$flexDenominator.'),0)';
              $as->getCell('D'. ($lp_row_offset+5))->setValue($totalFlexPercentOfRetail);
+             $as->getStyle('D'. ($lp_row_offset+5))->applyFromArray($currencyStyleNoCents);
+             $as->getStyle('D'. ($lp_row_offset+5))->applyFromArray($redFontStyleArray);
 
             $i++;
         }
@@ -490,12 +542,10 @@ class PackageExcel
 
     public function addToCurrentOffset($n)
     {
-        var_dump('add to current offset: '.$n);
 
         if (isset($n)) {
             $this->current_row_offset = $this->current_row_offset + $n;
         }
-        var_dump('current offset: '.$this->current_row_offset);
     }
 
     public function getCurrentRow()
@@ -567,5 +617,12 @@ class PackageExcel
             return $this->validityPeriodArray[$key]['lastRow'];
         }
         return null;
+    }
+
+    private function addMultiDayInclusionSingleDayPrice($inclusionValue)
+    {
+        $inclusionValue = floatval($inclusionValue);
+        $this->multiDayInclusionSingleDayPrice = $this->multiDayInclusionSingleDayPrice + $inclusionValue;
+        return true;
     }
 }
